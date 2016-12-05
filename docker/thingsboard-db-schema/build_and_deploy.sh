4b1e0df43584acde509fb4d735f54e40a709a828
volodymyr-babak <volodymyr.babak@gmail.com>
2016-12-05 21:07:35 +0200

    0.1 -> 1.0

--- docker/thingsboard-db-schema/build_and_deploy.sh
+++ docker/thingsboard-db-schema/build_and_deploy.sh
@@ -4,8 +4,8 @@ cp ../../dao/src/main/resources/schema.cql schema.cql
 cp ../../dao/src/main/resources/demo-data.cql demo-data.cql
 cp ../../dao/src/main/resources/system-data.cql system-data.cql
 
-docker build -t thingsboard/thingsboard-db-schema:0.1 .
+docker build -t thingsboard/thingsboard-db-schema:1.0 .
 
 docker login
 
-docker push thingsboard/thingsboard-db-schema:0.1
+docker push thingsboard/thingsboard-db-schema:1.0
