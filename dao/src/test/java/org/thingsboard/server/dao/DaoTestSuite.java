96dad362c3dbff77dc3c6c8df87d6f19d5eb51bf
Andrew Shvayka <ashvayka@thingsboard.io>
2017-05-18 13:13:36 +0300

    Few minor fixes to license files

--- dao/src/test/java/org/thingsboard/server/dao/DaoTestSuite.java
+++ dao/src/test/java/org/thingsboard/server/dao/DaoTestSuite.java
@@ -25,13 +25,12 @@ import java.util.Arrays;
 
 @RunWith(ClasspathSuite.class)
 @ClassnameFilters({
-        "org.thingsboard.server.dao.service.AlarmServiceTest",
-//        "org.thingsboard.server.dao.service.*Test",
-//        "org.thingsboard.server.dao.kv.*Test",
-//        "org.thingsboard.server.dao.plugin.*Test",
-//        "org.thingsboard.server.dao.rule.*Test",
-//        "org.thingsboard.server.dao.attributes.*Test",
-//        "org.thingsboard.server.dao.timeseries.*Test"
+        "org.thingsboard.server.dao.service.*Test",
+        "org.thingsboard.server.dao.kv.*Test",
+        "org.thingsboard.server.dao.plugin.*Test",
+        "org.thingsboard.server.dao.rule.*Test",
+        "org.thingsboard.server.dao.attributes.*Test",
+        "org.thingsboard.server.dao.timeseries.*Test"
 })
 public class DaoTestSuite {
 
