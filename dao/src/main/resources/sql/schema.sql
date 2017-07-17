5e5790a9d22052a9bf5c2891a044f8b0dcd68896
Andrew Shvayka <ashvayka@thingsboard.io>
2017-07-17 17:32:32 +0300

    DAO layer fixes

--- dao/src/main/resources/sql/schema.sql
+++ dao/src/main/resources/sql/schema.sql
@@ -16,44 +16,44 @@
 
 
 CREATE TABLE IF NOT EXISTS admin_settings (
-    id character varying(31) NOT NULL CONSTRAINT admin_settings_pkey PRIMARY KEY,
+    id varchar(31) NOT NULL CONSTRAINT admin_settings_pkey PRIMARY KEY,
     json_value varchar,
-    key character varying(255)
+    key varchar(255)
 );
 
 CREATE TABLE IF NOT EXISTS alarm (
-    id character varying(31) NOT NULL CONSTRAINT alarm_pkey PRIMARY KEY,
+    id varchar(31) NOT NULL CONSTRAINT alarm_pkey PRIMARY KEY,
     ack_ts bigint,
     clear_ts bigint,
     additional_info varchar,
     end_ts bigint,
-    originator_id character varying(31),
+    originator_id varchar(31),
     originator_type integer,
     propagate boolean,
-    severity character varying(255),
+    severity varchar(255),
     start_ts bigint,
-    status character varying(255),
-    tenant_id character varying(31),
-    type character varying(255)
+    status varchar(255),
+    tenant_id varchar(31),
+    type varchar(255)
 );
 
 CREATE TABLE IF NOT EXISTS asset (
-    id character varying(31) NOT NULL CONSTRAINT asset_pkey PRIMARY KEY,
+    id varchar(31) NOT NULL CONSTRAINT asset_pkey PRIMARY KEY,
     additional_info varchar,
-    customer_id character varying(31),
-    name character varying(255),
-    search_text character varying(255),
-    tenant_id character varying(31),
-    type character varying(255)
+    customer_id varchar(31),
+    name varchar(255),
+    search_text varchar(255),
+    tenant_id varchar(31),
+    type varchar(255)
 );
 
 CREATE TABLE IF NOT EXISTS attribute_kv (
-  entity_type character varying(255),
-  entity_id character varying(31),
-  attribute_type character varying(255),
-  attribute_key character varying(255),
+  entity_type varchar(255),
+  entity_id varchar(31),
+  attribute_type varchar(255),
+  attribute_key varchar(255),
   bool_v boolean,
-  str_v character varying(255),
+  str_v varchar,
   long_v bigint,
   dbl_v double precision,
   last_update_ts bigint,
@@ -61,182 +61,182 @@ CREATE TABLE IF NOT EXISTS attribute_kv (
 );
 
 CREATE TABLE IF NOT EXISTS component_descriptor (
-    id character varying(31) NOT NULL CONSTRAINT component_descriptor_pkey PRIMARY KEY,
-    actions character varying(255),
-    clazz character varying(255),
+    id varchar(31) NOT NULL CONSTRAINT component_descriptor_pkey PRIMARY KEY,
+    actions varchar(255),
+    clazz varchar,
     configuration_descriptor varchar,
-    name character varying(255),
-    scope character varying(255),
-    search_text character varying(255),
-    type character varying(255)
+    name varchar(255),
+    scope varchar(255),
+    search_text varchar(255),
+    type varchar(255)
 );
 
 CREATE TABLE IF NOT EXISTS customer (
-    id character varying(31) NOT NULL CONSTRAINT customer_pkey PRIMARY KEY,
+    id varchar(31) NOT NULL CONSTRAINT customer_pkey PRIMARY KEY,
     additional_info varchar,
-    address character varying(255),
-    address2 character varying(255),
-    city character varying(255),
-    country character varying(255),
-    email character varying(255),
-    phone character varying(255),
-    search_text character varying(255),
-    state character varying(255),
-    tenant_id character varying(31),
-    title character varying(255),
-    zip character varying(255)
+    address varchar,
+    address2 varchar,
+    city varchar(255),
+    country varchar(255),
+    email varchar(255),
+    phone varchar(255),
+    search_text varchar(255),
+    state varchar(255),
+    tenant_id varchar(31),
+    title varchar(255),
+    zip varchar(255)
 );
 
 CREATE TABLE IF NOT EXISTS dashboard (
-    id character varying(31) NOT NULL CONSTRAINT dashboard_pkey PRIMARY KEY,
+    id varchar(31) NOT NULL CONSTRAINT dashboard_pkey PRIMARY KEY,
     configuration varchar,
-    customer_id character varying(31),
-    search_text character varying(255),
-    tenant_id character varying(31),
-    title character varying(255)
+    customer_id varchar(31),
+    search_text varchar(255),
+    tenant_id varchar(31),
+    title varchar(255)
 );
 
 CREATE TABLE IF NOT EXISTS device (
-    id character varying(31) NOT NULL CONSTRAINT device_pkey PRIMARY KEY,
+    id varchar(31) NOT NULL CONSTRAINT device_pkey PRIMARY KEY,
     additional_info varchar,
-    customer_id character varying(31),
-    type character varying(255),
-    name character varying(255),
-    search_text character varying(255),
-    tenant_id character varying(31)
+    customer_id varchar(31),
+    type varchar(255),
+    name varchar(255),
+    search_text varchar(255),
+    tenant_id varchar(31)
 );
 
 CREATE TABLE IF NOT EXISTS device_credentials (
-    id character varying(31) NOT NULL CONSTRAINT device_credentials_pkey PRIMARY KEY,
-    credentials_id character varying(255),
-    credentials_type character varying(255),
-    credentials_value character varying(255),
-    device_id character varying(31)
+    id varchar(31) NOT NULL CONSTRAINT device_credentials_pkey PRIMARY KEY,
+    credentials_id varchar,
+    credentials_type varchar(255),
+    credentials_value varchar),
+    device_id varchar(31)
 );
 
 CREATE TABLE IF NOT EXISTS event (
-    id character varying(31) NOT NULL CONSTRAINT event_pkey PRIMARY KEY,
+    id varchar(31) NOT NULL CONSTRAINT event_pkey PRIMARY KEY,
     body varchar,
-    entity_id character varying(31),
-    entity_type character varying(255),
-    event_type character varying(255),
-    event_uid character varying(255),
-    tenant_id character varying(31),
+    entity_id varchar(31),
+    entity_type varchar(255),
+    event_type varchar(255),
+    event_uid varchar(255),
+    tenant_id varchar(31),
     CONSTRAINT event_unq_key UNIQUE (tenant_id, entity_type, entity_id, event_type, event_uid)
 );
 
 CREATE TABLE IF NOT EXISTS plugin (
-    id character varying(31) NOT NULL CONSTRAINT plugin_pkey PRIMARY KEY,
+    id varchar(31) NOT NULL CONSTRAINT plugin_pkey PRIMARY KEY,
     additional_info varchar,
-    api_token character varying(255),
-    plugin_class character varying(255),
+    api_token varchar(255),
+    plugin_class varchar(255),
     configuration varchar,
-    name character varying(255),
+    name varchar(255),
     public_access boolean,
-    search_text character varying(255),
-    state character varying(255),
-    tenant_id character varying(31)
+    search_text varchar(255),
+    state varchar(255),
+    tenant_id varchar(31)
 );
 
 CREATE TABLE IF NOT EXISTS relation (
-    from_id character varying(31),
-    from_type character varying(255),
-    to_id character varying(31),
-    to_type character varying(255),
-    relation_type_group character varying(255),
-    relation_type character varying(255),
+    from_id varchar(31),
+    from_type varchar(255),
+    to_id varchar(31),
+    to_type varchar(255),
+    relation_type_group varchar(255),
+    relation_type varchar(255),
     additional_info varchar,
     CONSTRAINT relation_unq_key UNIQUE (from_id, from_type, relation_type_group, relation_type, to_id, to_type)
 );
 
 CREATE TABLE IF NOT EXISTS rule (
-    id character varying(31) NOT NULL CONSTRAINT rule_pkey PRIMARY KEY,
+    id varchar(31) NOT NULL CONSTRAINT rule_pkey PRIMARY KEY,
     action varchar,
     additional_info varchar,
     filters varchar,
-    name character varying(255),
-    plugin_token character varying(255),
+    name varchar(255),
+    plugin_token varchar(255),
     processor varchar,
-    search_text character varying(255),
-    state character varying(255),
-    tenant_id character varying(31),
+    search_text varchar(255),
+    state varchar(255),
+    tenant_id varchar(31),
     weight integer
 );
 
 CREATE TABLE IF NOT EXISTS tb_user (
-    id character varying(31) NOT NULL CONSTRAINT tb_user_pkey PRIMARY KEY,
+    id varchar(31) NOT NULL CONSTRAINT tb_user_pkey PRIMARY KEY,
     additional_info varchar,
-    authority character varying(255),
-    customer_id character varying(31),
-    email character varying(255) UNIQUE,
-    first_name character varying(255),
-    last_name character varying(255),
-    search_text character varying(255),
-    tenant_id character varying(31)
+    authority varchar(255),
+    customer_id varchar(31),
+    email varchar(255) UNIQUE,
+    first_name varchar(255),
+    last_name varchar(255),
+    search_text varchar(255),
+    tenant_id varchar(31)
 );
 
 CREATE TABLE IF NOT EXISTS tenant (
-    id character varying(31) NOT NULL CONSTRAINT tenant_pkey PRIMARY KEY,
+    id varchar(31) NOT NULL CONSTRAINT tenant_pkey PRIMARY KEY,
     additional_info varchar,
-    address character varying(255),
-    address2 character varying(255),
-    city character varying(255),
-    country character varying(255),
-    email character varying(255),
-    phone character varying(255),
-    region character varying(255),
-    search_text character varying(255),
-    state character varying(255),
-    title character varying(255),
-    zip character varying(255)
+    address varchar,
+    address2 varchar,
+    city varchar(255),
+    country varchar(255),
+    email varchar(255),
+    phone varchar(255),
+    region varchar(255),
+    search_text varchar(255),
+    state varchar(255),
+    title varchar(255),
+    zip varchar(255)
 );
 
 CREATE TABLE IF NOT EXISTS ts_kv (
-    entity_type character varying(255) NOT NULL,
-    entity_id character varying(31) NOT NULL,
-    key character varying(255) NOT NULL,
+    entity_type varchar(255) NOT NULL,
+    entity_id varchar(31) NOT NULL,
+    key varchar(255) NOT NULL,
     ts bigint NOT NULL,
     bool_v boolean,
-    str_v character varying(255),
+    str_v varchar,
     long_v bigint,
     dbl_v double precision,
     CONSTRAINT ts_kv_unq_key UNIQUE (entity_type, entity_id, key, ts)
 );
 
 CREATE TABLE IF NOT EXISTS ts_kv_latest (
-    entity_type character varying(255) NOT NULL,
-    entity_id character varying(31) NOT NULL,
-    key character varying(255) NOT NULL,
+    entity_type varchar(255) NOT NULL,
+    entity_id varchar(31) NOT NULL,
+    key varchar(255) NOT NULL,
     ts bigint NOT NULL,
     bool_v boolean,
-    str_v character varying(255),
+    str_v varchar,
     long_v bigint,
     dbl_v double precision,
     CONSTRAINT ts_kv_latest_unq_key UNIQUE (entity_type, entity_id, key)
 );
 
 CREATE TABLE IF NOT EXISTS user_credentials (
-    id character varying(31) NOT NULL CONSTRAINT user_credentials_pkey PRIMARY KEY,
-    activate_token character varying(255) UNIQUE,
+    id varchar(31) NOT NULL CONSTRAINT user_credentials_pkey PRIMARY KEY,
+    activate_token varchar(255) UNIQUE,
     enabled boolean,
-    password character varying(255),
-    reset_token character varying(255) UNIQUE,
-    user_id character varying(31) UNIQUE
+    password varchar(255),
+    reset_token varchar(255) UNIQUE,
+    user_id varchar(31) UNIQUE
 );
 
 CREATE TABLE IF NOT EXISTS widget_type (
-    id character varying(31) NOT NULL CONSTRAINT widget_type_pkey PRIMARY KEY,
-    alias character varying(255),
-    bundle_alias character varying(255),
+    id varchar(31) NOT NULL CONSTRAINT widget_type_pkey PRIMARY KEY,
+    alias varchar(255),
+    bundle_alias varchar(255),
     descriptor varchar(1000000),
-    name character varying(255),
-    tenant_id character varying(31)
+    name varchar(255),
+    tenant_id varchar(31)
 );
 
 CREATE TABLE IF NOT EXISTS widgets_bundle (
-    id character varying(31) NOT NULL CONSTRAINT widgets_bundle_pkey PRIMARY KEY,
-    alias character varying(255),
-    search_text character varying(255),
-    tenant_id character varying(31),
-    title character varying(255)
+    id varchar(31) NOT NULL CONSTRAINT widgets_bundle_pkey PRIMARY KEY,
+    alias varchar(255),
+    search_text varchar(255),
+    tenant_id varchar(31),
+    title varchar(255)
 );
