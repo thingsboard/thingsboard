ceb8b39c165776f1429fc3583454b306c6dda589
Igor Kulikov <ikulikov@thingsboard.io>
2017-10-31 14:58:36 +0200

    UI: Fix alarms widget.

--- ui/src/app/widget/lib/alarms-table-widget.js
+++ ui/src/app/widget/lib/alarms-table-widget.js
@@ -61,7 +61,7 @@ function AlarmsTableWidgetController($element, $scope, $filter, $mdMedia, $mdDia
     vm.selectedAlarms = []
 
     vm.alarmSource = null;
-    vm.allAlarms = null;
+    vm.allAlarms = [];
 
     vm.currentAlarm = null;
 
