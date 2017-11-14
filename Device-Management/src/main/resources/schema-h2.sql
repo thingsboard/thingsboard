800c778e015cfcb7edb03291da59e590634b014a
Himanshu <hmdcisin@gmail.com>
2017-11-14 15:36:11 +0530

    Removed device-management from the branch.

--- Device-Management/src/main/resources/schema-h2.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS DEVICE_SHADOW (
-  deviceName varchar,
-  availableTags varchar,
-  reportedTags varchar,
-  desiredTags varchar,
-  CONSTRAINT PK_DEVICE_SHADOW PRIMARY KEY (deviceName)
- );
