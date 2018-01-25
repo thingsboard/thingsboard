fd4da012e12b623e877e52825ab994c8e284a30f
Dima Landiak <n0fear2110@gmail.com>
2018-01-25 10:58:05 +0200

    fix coap tests

--- transport/coap/src/main/java/org/thingsboard/server/transport/coap/CoapTransportService.java
+++ transport/coap/src/main/java/org/thingsboard/server/transport/coap/CoapTransportService.java
@@ -30,15 +30,13 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.thingsboard.server.common.transport.SessionMsgProcessor;
 import org.thingsboard.server.common.transport.auth.DeviceAuthService;
 import org.thingsboard.server.transport.coap.adaptors.CoapTransportAdaptor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Service;
 
 @Service("CoapTransportService")
-@ConditionalOnProperty(prefix = "coap", value = "enabled", havingValue = "true", matchIfMissing = false)
+@ConditionalOnProperty(prefix = "coap", value = "enabled", havingValue = "true", matchIfMissing = true)
 @Slf4j
 public class CoapTransportService {
 
