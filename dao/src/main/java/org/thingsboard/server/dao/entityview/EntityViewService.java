14f658c92a166e9747942f0eb199dd123cf781c4
viktorbasanets <vbasanec@gmail.com>
2018-08-29 10:48:11 +0300

    Was created EntityViewService interface

--- dao/src/main/java/org/thingsboard/server/dao/entityview/EntityViewService.java
+++ dao/src/main/java/org/thingsboard/server/dao/entityview/EntityViewService.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright © 2016-2018 The Thingsboard Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.thingsboard.server.dao.entityview;
 
 import org.thingsboard.server.common.data.EntityView;
@@ -7,9 +22,12 @@ import org.thingsboard.server.common.data.id.TenantId;
 import org.thingsboard.server.common.data.page.TextPageData;
 import org.thingsboard.server.common.data.page.TextPageLink;
 
+/**
+ * Created by Victor Basanets on 8/27/2017.
+ */
 public interface EntityViewService {
-    EntityView findEntityViewById(EntityViewId entityViewId);
 
+    EntityView findEntityViewById(EntityViewId entityViewId);
 
     EntityView findEntityViewByTenantIdAndName(TenantId tenantId, String name);
 
