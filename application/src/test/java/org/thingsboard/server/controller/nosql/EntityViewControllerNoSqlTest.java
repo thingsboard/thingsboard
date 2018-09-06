e132d702a2ec05e688eddacad5d5188ec8ca3450
viktorbasanets <vbasanec@gmail.com>
2018-09-06 13:10:48 +0300

    Was changed path to all sql and nosql tests

--- application/src/test/java/org/thingsboard/server/controller/nosql/EntityViewControllerNoSqlTest.java
+++ application/src/test/java/org/thingsboard/server/controller/nosql/EntityViewControllerNoSqlTest.java
@@ -16,9 +16,11 @@
 package org.thingsboard.server.controller.nosql;
 
 import org.thingsboard.server.controller.BaseEntityViewControllerTest;
+import org.thingsboard.server.dao.service.DaoNoSqlTest;
 
 /**
  * Created by Victor Basanets on 8/27/2017.
  */
+@DaoNoSqlTest
 public class EntityViewControllerNoSqlTest extends BaseEntityViewControllerTest {
 }
