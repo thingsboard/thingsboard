457b5d1250d2b71f49cb07ad49a3c2f13d369457
viktorbasanets <vbasanec@gmail.com>
2018-09-07 17:58:09 +0300

    Created entity-view dao for nosql db

--- dao/src/main/java/org/thingsboard/server/dao/entityview/CassandraEntityViewDao.java
+++ dao/src/main/java/org/thingsboard/server/dao/entityview/CassandraEntityViewDao.java
@@ -16,22 +16,24 @@
 package org.thingsboard.server.dao.entityview;
 
 import com.datastax.driver.core.Statement;
+import com.datastax.driver.core.querybuilder.Select;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.thingsboard.server.common.data.EntitySubtype;
 import org.thingsboard.server.common.data.EntityType;
 import org.thingsboard.server.common.data.EntityView;
 import org.thingsboard.server.common.data.page.TextPageLink;
+import org.thingsboard.server.dao.DaoUtil;
 import org.thingsboard.server.dao.model.EntitySubtypeEntity;
 import org.thingsboard.server.dao.model.nosql.EntityViewEntity;
 import org.thingsboard.server.dao.nosql.CassandraAbstractSearchTextDao;
 import org.thingsboard.server.dao.util.NoSqlDao;
 
-import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
+import java.util.*;
 
-import static org.thingsboard.server.dao.model.ModelConstants.ENTITY_VIEW_TABLE_FAMILY_NAME;
+import static com.datastax.driver.core.querybuilder.QueryBuilder.eq;
+import static com.datastax.driver.core.querybuilder.QueryBuilder.select;
+import static org.thingsboard.server.dao.model.ModelConstants.*;
 
 /**
  * Created by Victor Basanets on 9/06/2017.
@@ -62,20 +64,36 @@ public class CassandraEntityViewDao extends CassandraAbstractSearchTextDao<Entit
         return savedEntityView;
     }
 
-    /*Wasn't done!!!*/
     @Override
     public List<EntityView> findEntityViewByTenantId(UUID tenantId, TextPageLink pageLink) {
+        log.debug("Try to find entity-views by tenantId [{}] and pageLink [{}]", tenantId, pageLink);
+        List<EntityViewEntity> entityViewEntities =
+                findPageWithTextSearch(ENTITY_VIEW_BY_TENANT_AND_SEARCH_TEXT_COLUMN_FAMILY_NAME,
+                Collections.singletonList(eq(ENTITY_VIEW_TENANT_ID_PROPERTY, tenantId)), pageLink);
 
+        log.trace("Found entity-views [{}] by tenantId [{}] and pageLink [{}]", entityViewEntities, tenantId, pageLink);
+        return DaoUtil.convertDataList(entityViewEntities);
     }
 
     @Override
-    public Optional<EntityView> findEntityViewByTenantIdAndName(UUID tenantId, String name) {
-        return Optional.empty();
+    public Optional<EntityView> findEntityViewByTenantIdAndName(UUID tenantId, String entityViewName) {
+        return Optional.ofNullable(DaoUtil.getData(
+                findOneByStatement(select().from(ENTITY_VIEW_TENANT_AND_NAME_VIEW_NAME).where()
+                        .and(eq(ENTITY_VIEW_TENANT_ID_PROPERTY, tenantId))
+                        .and(eq(ENTITY_VIEW_NAME_PROPERTY, entityViewName))))
+        );
     }
 
     @Override
     public List<EntityView> findEntityViewByTenantIdAndEntityId(UUID tenantId, UUID entityId, TextPageLink pageLink) {
-        return null;
+        log.debug("Try to find entity-views by tenantId [{}], entityId[{}] and pageLink [{}]", tenantId, entityId, pageLink);
+        List<EntityViewEntity> entityViewEntities = findPageWithTextSearch(DEVICE_BY_CUSTOMER_AND_SEARCH_TEXT_COLUMN_FAMILY_NAME,
+                Arrays.asList(eq(DEVICE_CUSTOMER_ID_PROPERTY, entityId),
+                        eq(DEVICE_TENANT_ID_PROPERTY, tenantId)),
+                pageLink);
+
+        log.trace("Found entity-views [{}] by tenantId [{}], entityId [{}] and pageLink [{}]", entityViewEntities, tenantId, entityId, pageLink);
+        return DaoUtil.convertDataList(entityViewEntities);
     }
 
     @Override
