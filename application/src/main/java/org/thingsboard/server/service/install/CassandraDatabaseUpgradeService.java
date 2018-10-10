b8c8f57a82d3de7685fc5c889c0943b2e65bf280
Volodymyr Babak <volodymyr.babak@gmail.com>
2018-10-10 11:21:49 +0300

    Fixed dump for bigint

--- application/src/main/java/org/thingsboard/server/service/install/CassandraDatabaseUpgradeService.java
+++ application/src/main/java/org/thingsboard/server/service/install/CassandraDatabaseUpgradeService.java
@@ -224,7 +224,7 @@ public class CassandraDatabaseUpgradeService implements DatabaseUpgradeService {
 
             case "2.1.1":
 
-                log.info("Upgrading Cassandara DataBase from version {} to 2.1.2 ...", fromVersion);
+                log.info("Upgrading Cassandra DataBase from version {} to 2.1.2 ...", fromVersion);
 
                 cluster.getSession();
 
