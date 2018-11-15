4f7ce892ecb8da7862838aca610caa3a4871cd66
Igor Kulikov <ikulikov82@gmail.com>
2018-11-15 20:42:10 +0200

    Update login.routes.js

--- ui/src/app/login/login.routes.js
+++ ui/src/app/login/login.routes.js
@@ -27,10 +27,6 @@ export default function LoginRoutes($stateProvider) {
     $stateProvider.state('login', {
         url: '/login',
         module: 'public',
-        params: {
-            params: null,
-            toName: null
-        },
         views: {
             "@": {
                 controller: 'LoginController',
