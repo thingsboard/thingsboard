eb33ffcc4a4bb2731f394a5696fd9fe6c1501acb
Igor Kulikov <ikulikov@thingsboard.io>
2019-01-28 16:22:40 +0200

    Improve Data Update Service.

--- application/src/main/java/org/thingsboard/server/install/ThingsboardInstallService.java
+++ application/src/main/java/org/thingsboard/server/install/ThingsboardInstallService.java
@@ -23,7 +23,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 import org.thingsboard.server.service.component.ComponentDiscoveryService;
-import org.thingsboard.server.service.install.DataUpdateService;
+import org.thingsboard.server.service.install.update.DataUpdateService;
 import org.thingsboard.server.service.install.DatabaseUpgradeService;
 import org.thingsboard.server.service.install.EntityDatabaseSchemaService;
 import org.thingsboard.server.service.install.SystemDataLoaderService;
--- application/src/main/java/org/thingsboard/server/service/install/update/DataUpdateService.java
+++ application/src/main/java/org/thingsboard/server/service/install/update/DataUpdateService.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.thingsboard.server.service.install;
+package org.thingsboard.server.service.install.update;
 
 public interface DataUpdateService {
 
