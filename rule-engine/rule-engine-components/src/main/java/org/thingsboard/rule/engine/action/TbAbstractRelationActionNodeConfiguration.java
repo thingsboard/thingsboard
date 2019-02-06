75b1f848056ef53010965da701c5570813667349
ShvaykaD <dshvaika@thingsboard.io>
2019-02-06 12:54:55 +0200

    Feature/relation nodes (#1452)
    * modified relation-action-nodes
    
    * add rule node description
    
    * fix rule node description
    
    * fix typo
    
    * updated relation-action-nodes

--- rule-engine/rule-engine-components/src/main/java/org/thingsboard/rule/engine/action/TbAbstractRelationActionNodeConfiguration.java
+++ rule-engine/rule-engine-components/src/main/java/org/thingsboard/rule/engine/action/TbAbstractRelationActionNodeConfiguration.java
@@ -21,7 +21,7 @@ import lombok.Data;
 public abstract class TbAbstractRelationActionNodeConfiguration {
 
     private String direction;
-    private String relationType;
+    private String relationTypePattern;
 
     private String entityType;
     private String entityNamePattern;
