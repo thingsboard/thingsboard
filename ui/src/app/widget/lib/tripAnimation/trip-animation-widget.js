5f8547d6351a67287d546e675169add620a49730
Maksym Dudnik <dudnikmaksim@gmail.com>
2019-03-01 10:08:00 +0200

    Licence Update fro trip animation

--- ui/src/app/widget/lib/tripAnimation/trip-animation-widget.js
+++ ui/src/app/widget/lib/tripAnimation/trip-animation-widget.js
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2016-2019 The Thingsboard Authors
+ * Copyright © 2016-2018 The Thingsboard Authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 import './trip-animation-widget.scss';
 import template from "./trip-animation-widget.tpl.html";
 import TbOpenStreetMap from '../openstreet-map';
