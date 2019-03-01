3ffc455e6d3c9209785e3bedf38fdbe25311e6b4
Maksym Dudnik <dudnikmaksim@gmail.com>
2019-03-01 10:31:00 +0200

    Licence Update for trip animation"

--- ui/src/app/widget/lib/tripAnimation/trip-animation-widget.js
+++ ui/src/app/widget/lib/tripAnimation/trip-animation-widget.js
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 import './trip-animation-widget.scss';
 import template from "./trip-animation-widget.tpl.html";
 import TbOpenStreetMap from '../openstreet-map';
