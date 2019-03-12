282b16b1af6e1602de19fa66fa93a1bea54b3930
Igor Kulikov <ikulikov@thingsboard.io>
2019-03-12 16:14:46 +0200

    Update missing translation handler.

--- ui/src/app/locale/translate-handler.js
+++ ui/src/app/locale/translate-handler.js
@@ -21,9 +21,9 @@
 function ThingsboardMissingTranslateHandler($log, types) {
 
     return function (translationId) {
-        if (translationId && !translationId.startsWith(types.translate.customTranslationsPrefix)) {
+        /*if (translationId && !translationId.startsWith(types.translate.customTranslationsPrefix)) {
             $log.warn('Translation for ' + translationId + ' doesn\'t exist');
-        }
+        }*/
     };
 
 }
