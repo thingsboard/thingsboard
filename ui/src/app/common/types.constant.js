1a1c507785ae27b90f1faf5765a4c50d7b4a77cb
Chantsova Ekaterina <chantsova.ekaterina@gmail.com>
2019-07-17 14:35:26 +0300

    Custom action with additional resources

--- ui/src/app/common/types.constant.js
+++ ui/src/app/common/types.constant.js
@@ -821,6 +821,10 @@ export default angular.module('thingsboard.types', [])
                 custom: {
                     name: 'widget-action.custom',
                     value: 'custom'
+                },
+                customPretty: {
+                    name: 'widget-action.custom-pretty',
+                    value: 'customPretty'
                 }
             },
             systemBundleAlias: {
