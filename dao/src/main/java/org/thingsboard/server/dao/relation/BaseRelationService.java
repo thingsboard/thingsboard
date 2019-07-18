1d421879b5489d2b4fc5ae2f9718076e00f76387
nordmif <dronfim@mail.ru>
2019-07-18 13:08:04 +0300

    cleanup and formatting

--- dao/src/main/java/org/thingsboard/server/dao/relation/BaseRelationService.java
+++ dao/src/main/java/org/thingsboard/server/dao/relation/BaseRelationService.java
@@ -1,12 +1,12 @@
 /**
  * Copyright © 2016-2019 The Thingsboard Authors
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -307,9 +307,11 @@ public class BaseRelationService implements RelationService {
                         public void onSuccess(@Nullable List<EntityRelation> result) {
                             cache.putIfAbsent(fromAndTypeGroup, result);
                         }
+
                         @Override
-                        public void onFailure(Throwable t) {}
-             });
+                        public void onFailure(Throwable t) {
+                        }
+                    });
             return relationsFuture;
         }
     }
@@ -386,8 +388,10 @@ public class BaseRelationService implements RelationService {
                         public void onSuccess(@Nullable List<EntityRelation> result) {
                             cache.putIfAbsent(toAndTypeGroup, result);
                         }
+
                         @Override
-                        public void onFailure(Throwable t) {}
+                        public void onFailure(Throwable t) {
+                        }
                     });
             return relationsFuture;
         }
