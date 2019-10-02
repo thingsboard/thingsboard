2817cb6361c1b6320aabcd6c6105a46265af5a65
ShvaykaD <mitiabmx@gmail.com>
2019-10-02 10:52:03 +0300

    add annotation to repository

--- dao/src/main/java/org/thingsboard/server/dao/sqlts/psql/PsqlPartitioningRepository.java
+++ dao/src/main/java/org/thingsboard/server/dao/sqlts/psql/PsqlPartitioningRepository.java
@@ -19,17 +19,19 @@ import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import org.thingsboard.server.dao.timeseries.PsqlPartition;
 import org.thingsboard.server.dao.util.PsqlDao;
+import org.thingsboard.server.dao.util.SqlTsDao;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+@SqlTsDao
 @PsqlDao
 @Repository
 @Transactional
 public class PsqlPartitioningRepository {
 
     @PersistenceContext
-    public EntityManager entityManager;
+    private EntityManager entityManager;
 
     public void save(PsqlPartition partition) {
         entityManager.createNativeQuery(partition.getQuery())
