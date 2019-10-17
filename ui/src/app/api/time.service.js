467d7b5c3c5d9dce13b82e3c2da4dc15fcc0999f
Chantsova Ekaterina <chantsova.ekaterina@gmail.com>
2019-10-17 15:25:24 +0300

    Flots comparison option (#2079)
    * Add new hidden widget cards for latest and timeseries value
    
    * Revert "Add new hidden widget cards for latest and timeseries value"
    
    This reverts commit 09b73d5afcc66baf942a776f83a0295700f83d22.
    
    * Allow hiding of zero/false dataKey values from tbFlot widgets tooltips
    
    * comparison option draft
    
    * Added dataKey setting for excluding from Stacking mode in chart widgets.
    
    * Flot comparison option (draft)
    
    * Flot comparison option (draft)
    
    * Fix color generation for additional keys
    
    * Add new time options for comparison
    
    * Add ability to define points symbol and line width in 'line' flot charts
    
    * Change history timeinterval calculation, translations definition

--- ui/src/app/api/time.service.js
+++ ui/src/app/api/time.service.js
@@ -47,6 +47,7 @@ function TimeService($translate, $http, $q, types) {
         defaultTimewindow: defaultTimewindow,
         toHistoryTimewindow: toHistoryTimewindow,
         createSubscriptionTimewindow: createSubscriptionTimewindow,
+        createTimewindowForComparison: createTimewindowForComparison,
         getMaxDatapointsLimit: function () {
             return maxDatapointsLimit;
         },
@@ -383,5 +384,27 @@ function TimeService($translate, $http, $q, types) {
         }
     }
 
+    function createTimewindowForComparison(subscriptionTimewindow, timeUnit) {
+        var timewindowForComparison = {
+            fixedWindow: null,
+            realtimeWindowMs: null,
+            aggregation: subscriptionTimewindow.aggregation
+        };
 
+        if (subscriptionTimewindow.realtimeWindowMs) {
+            timewindowForComparison.startTs = moment(subscriptionTimewindow.startTs).subtract(1, timeUnit).valueOf(); //eslint-disable-line
+            timewindowForComparison.realtimeWindowMs = subscriptionTimewindow.realtimeWindowMs;
+        } else if (subscriptionTimewindow.fixedWindow) {
+            var timeInterval = subscriptionTimewindow.fixedWindow.endTimeMs - subscriptionTimewindow.fixedWindow.startTimeMs;
+            var endTimeMs = moment(subscriptionTimewindow.fixedWindow.endTimeMs).subtract(1, timeUnit).valueOf(); //eslint-disable-line
+
+            timewindowForComparison.startTs = endTimeMs - timeInterval;
+            timewindowForComparison.fixedWindow = {
+                startTimeMs: timewindowForComparison.startTs,
+                endTimeMs: endTimeMs
+            };
+        }
+
+        return timewindowForComparison;
+    }
 }
