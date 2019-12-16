735bba10536d32b4a06560997c053b7ad8902880
Mirco Pizzichini <mirco.pizzichini@isemar.biz>
2019-12-16 08:55:20 +0100

    Fix timewindow parameters when zooming flot widget chart

--- ui/src/app/api/time.service.js
+++ ui/src/app/api/time.service.js
@@ -280,6 +280,9 @@ function TimeService($translate, $http, $q, types) {
 
 
         var historyTimewindow = {
+            hideInterval: timewindow.hideInterval || false,
+            hideAggregation: timewindow.hideAggregation || false,
+            hideAggInterval: timewindow.hideAggInterval || false,
             history: {
                 fixedTimewindow: {
                     startTimeMs: startTimeMs,
