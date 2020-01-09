c6fab3e0099b71bea2619dd63c354f59436c78d6
Mirco Pizzichini <mirco.pizzichini@isemar.biz>
2020-01-09 09:29:29 +0100

    Add possibility to select a preferred entity for alias not resolved as multiple entities

--- ui/src/app/api/entity.service.js
+++ ui/src/app/api/entity.service.js
@@ -414,6 +414,15 @@ function EntityService($http, $q, $filter, $translate, $log, userService, device
                 aliasInfo.currentEntity = null;
                 if (aliasInfo.resolvedEntities.length) {
                     aliasInfo.currentEntity = aliasInfo.resolvedEntities[0];
+                    var defaultEntity = [];
+                    if (!filter.resolveMultiple) {
+                        if (stateParams && stateParams.targetEntityParamName === entityAlias.alias) {
+                            defaultEntity = $filter('filter')(aliasInfo.resolvedEntities, {id: stateParams[entityAlias.alias].entityId.id});
+                        } else if (filter.defaultEntity) {
+                            defaultEntity = $filter('filter')(aliasInfo.resolvedEntities, {id: filter.defaultEntity.id});
+                        }
+                    }
+                    aliasInfo.currentEntity = defaultEntity.length ? defaultEntity[0] : aliasInfo.resolvedEntities[0];        
                 }
                 deferred.resolve(aliasInfo);
             },
