e25595fabc5bf6fd69f84a143336e5402cbbcf33
YevhenBondarenko <ybondarenko@thingsboard.io>
2020-02-03 15:52:44 +0200

    added json support to attributes from telemetry controller

--- application/src/main/java/org/thingsboard/server/controller/TelemetryController.java
+++ application/src/main/java/org/thingsboard/server/controller/TelemetryController.java
@@ -57,6 +57,7 @@ import org.thingsboard.server.common.data.kv.BasicTsKvEntry;
 import org.thingsboard.server.common.data.kv.BooleanDataEntry;
 import org.thingsboard.server.common.data.kv.DeleteTsKvQuery;
 import org.thingsboard.server.common.data.kv.DoubleDataEntry;
+import org.thingsboard.server.common.data.kv.JsonDataEntry;
 import org.thingsboard.server.common.data.kv.KvEntry;
 import org.thingsboard.server.common.data.kv.LongDataEntry;
 import org.thingsboard.server.common.data.kv.ReadTsKvQuery;
@@ -283,8 +284,7 @@ public class TelemetryController extends BaseController {
             if (startTs == null || endTs == null) {
                 deleteToTs = endTs;
                 return getImmediateDeferredResult("When deleteAllDataForKeys is false, start and end timestamp values shouldn't be empty", HttpStatus.BAD_REQUEST);
-            }
-            else{
+            } else {
                 deleteFromTs = startTs;
                 deleteToTs = endTs;
             }
@@ -638,7 +638,9 @@ public class TelemetryController extends BaseController {
         jsonNode.fields().forEachRemaining(entry -> {
             String key = entry.getKey();
             JsonNode value = entry.getValue();
-            if (entry.getValue().isTextual()) {
+            if (entry.getValue().isObject()) {
+                attributes.add(new BaseAttributeKvEntry(new JsonDataEntry(key, value.asText()), ts));
+            } else if (entry.getValue().isTextual()) {
                 if (maxStringValueLength > 0 && entry.getValue().textValue().length() > maxStringValueLength) {
                     String message = String.format("String value length [%d] for key [%s] is greater than maximum allowed [%d]", entry.getValue().textValue().length(), key, maxStringValueLength);
                     throw new UncheckedApiException(new InvalidParametersException(message));
