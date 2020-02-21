3f0cd0d5556af5bf0f7f5376a52807f260c2303b
Andrii Shvaika <ashvayka@thingsboard.io>
2020-02-21 13:26:03 +0200

    Reverted change of the dashboard configuration size

--- application/src/main/java/org/thingsboard/server/service/install/SqlDatabaseUpgradeService.java
+++ application/src/main/java/org/thingsboard/server/service/install/SqlDatabaseUpgradeService.java
@@ -221,10 +221,6 @@ public class SqlDatabaseUpgradeService implements DatabaseEntitiesUpgradeService
                             }
                         }
                     }
-                    try {
-                        conn.createStatement().execute("ALTER TABLE dashboard ALTER COLUMN configuration SET DATA TYPE varchar(100000000);"); //NOSONAR, ignoring because method used to execute thingsboard database upgrade script
-                    } catch (Exception e) {
-                    }
                     log.info("Schema updated.");
                 }
                 break;
