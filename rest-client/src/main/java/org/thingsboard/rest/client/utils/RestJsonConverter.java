1cd4710db8e31bba6844e30bfc23b03babb74c14
YevhenBondarenko <ybondarenko@thingsboard.io>
2020-02-24 18:20:05 +0200

    Refactored RestJsonConverter

--- rest-client/src/main/java/org/thingsboard/rest/client/utils/RestJsonConverter.java
+++ rest-client/src/main/java/org/thingsboard/rest/client/utils/RestJsonConverter.java
@@ -19,15 +19,19 @@ import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.util.CollectionUtils;
 import org.thingsboard.server.common.data.kv.AttributeKvEntry;
 import org.thingsboard.server.common.data.kv.BaseAttributeKvEntry;
+import org.thingsboard.server.common.data.kv.BasicTsKvEntry;
 import org.thingsboard.server.common.data.kv.BooleanDataEntry;
 import org.thingsboard.server.common.data.kv.DoubleDataEntry;
 import org.thingsboard.server.common.data.kv.JsonDataEntry;
 import org.thingsboard.server.common.data.kv.KvEntry;
 import org.thingsboard.server.common.data.kv.LongDataEntry;
 import org.thingsboard.server.common.data.kv.StringDataEntry;
+import org.thingsboard.server.common.data.kv.TsKvEntry;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class RestJsonConverter {
@@ -45,6 +49,22 @@ public class RestJsonConverter {
                         return new BaseAttributeKvEntry(entry, attr.get(LAST_UPDATE_TS).asLong());
                     }
             ).collect(Collectors.toList());
+        } else {
+            return Collections.emptyList();
+        }
+    }
+
+    public static List<TsKvEntry> toTimeseries(Map<String, List<JsonNode>> timeseries) {
+        if (!CollectionUtils.isEmpty(timeseries)) {
+            List<TsKvEntry> result = new ArrayList<>();
+            timeseries.forEach((key, values) ->
+                    result.addAll(values.stream().map(ts -> {
+                                KvEntry entry = parseValue(key, ts.get(VALUE));
+                                return new BasicTsKvEntry(ts.get(TS).asLong(), entry);
+                            }
+                    ).collect(Collectors.toList()))
+            );
+            return result;
         } else {
             return Collections.emptyList();
         }
