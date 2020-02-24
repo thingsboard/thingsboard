58c9821742c791010545ecd4557b499bd57e3285
YevhenBondarenko <ybondarenko@thingsboard.io>
2020-02-24 18:09:14 +0200

    Improvements RestJsonConverter

--- rest-client/src/main/java/org/thingsboard/rest/client/utils/RestJsonConverter.java
+++ rest-client/src/main/java/org/thingsboard/rest/client/utils/RestJsonConverter.java
@@ -19,18 +19,15 @@ import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.util.CollectionUtils;
 import org.thingsboard.server.common.data.kv.AttributeKvEntry;
 import org.thingsboard.server.common.data.kv.BaseAttributeKvEntry;
-import org.thingsboard.server.common.data.kv.BasicTsKvEntry;
 import org.thingsboard.server.common.data.kv.BooleanDataEntry;
 import org.thingsboard.server.common.data.kv.DoubleDataEntry;
+import org.thingsboard.server.common.data.kv.JsonDataEntry;
 import org.thingsboard.server.common.data.kv.KvEntry;
 import org.thingsboard.server.common.data.kv.LongDataEntry;
 import org.thingsboard.server.common.data.kv.StringDataEntry;
-import org.thingsboard.server.common.data.kv.TsKvEntry;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 public class RestJsonConverter {
@@ -53,35 +50,32 @@ public class RestJsonConverter {
         }
     }
 
-    public static List<TsKvEntry> toTimeseries(Map<String, List<JsonNode>> timeseries) {
-        if (!CollectionUtils.isEmpty(timeseries)) {
-            List<TsKvEntry> result = new ArrayList<>();
-            timeseries.forEach((key, values) ->
-                    result.addAll(values.stream().map(ts -> {
-                                KvEntry entry = parseValue(key, ts.get(VALUE));
-                                return new BasicTsKvEntry(ts.get(TS).asLong(), entry);
-                            }
-                    ).collect(Collectors.toList()))
-            );
-            return result;
-        } else {
-            return Collections.emptyList();
-        }
-    }
-
     private static KvEntry parseValue(String key, JsonNode value) {
         if (!value.isObject()) {
             if (value.isBoolean()) {
                 return new BooleanDataEntry(key, value.asBoolean());
-            } else if (value.isDouble()) {
-                return new DoubleDataEntry(key, value.asDouble());
-            } else if (value.isLong()) {
-                return new LongDataEntry(key, value.asLong());
-            } else {
+            } else if (value.isNumber()) {
+                return parseNumericValue(key, value);
+            } else if (value.isTextual()) {
                 return new StringDataEntry(key, value.asText());
+            } else {
+                throw new RuntimeException(CAN_T_PARSE_VALUE + value);
             }
         } else {
-            throw new RuntimeException(CAN_T_PARSE_VALUE + value);
+            return new JsonDataEntry(key, value.toString());
+        }
+    }
+
+    private static KvEntry parseNumericValue(String key, JsonNode value) {
+        if (value.isFloatingPointNumber()) {
+            return new DoubleDataEntry(key, value.asDouble());
+        } else {
+            try {
+                long longValue = Long.parseLong(value.toString());
+                return new LongDataEntry(key, longValue);
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException("Big integer values are not supported!");
+            }
         }
     }
 }
