83dd1d5e1a2c4b3e092fd5dabb95dd2a80dc8962
Volodymyr Babak <volodymyr.babak@gmail.com>
2020-02-25 16:23:31 +0200

    Fixes for pvc removal

--- k8s/k8s-delete-all.sh
+++ k8s/k8s-delete-all.sh
@@ -17,6 +17,4 @@
 
 kubectl -n thingsboard delete svc,sts,deploy,cm,po,ing --all
 
-kubectl -n thingsboard delete pvc postgres-pv-claim
-
-kubectl -n thingsboard delete pv cassandra
+kubectl -n thingsboard get pvc --no-headers=true | awk '//{print $1}' | xargs kubectl -n thingsboard delete --ignore-not-found=true pvc
