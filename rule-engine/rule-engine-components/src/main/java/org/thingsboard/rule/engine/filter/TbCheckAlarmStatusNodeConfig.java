a4ae57eb86a329a3afe97c7658968ca2e450022d
YevhenBondarenko <ybondarenko@thingsboard.io>
2020-03-05 16:02:42 +0200

    improvements

--- rule-engine/rule-engine-components/src/main/java/org/thingsboard/rule/engine/filter/TbCheckAlarmStatusNodeConfig.java
+++ rule-engine/rule-engine-components/src/main/java/org/thingsboard/rule/engine/filter/TbCheckAlarmStatusNodeConfig.java
@@ -17,18 +17,19 @@ package org.thingsboard.rule.engine.filter;
 
 import lombok.Data;
 import org.thingsboard.rule.engine.api.NodeConfiguration;
+import org.thingsboard.server.common.data.alarm.AlarmStatus;
 
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
 
 @Data
 public class TbCheckAlarmStatusNodeConfig implements NodeConfiguration<TbCheckAlarmStatusNodeConfig> {
-    private List<String> alarmStatusList;
+    private List<AlarmStatus> alarmStatusList;
 
     @Override
     public TbCheckAlarmStatusNodeConfig defaultConfiguration() {
         TbCheckAlarmStatusNodeConfig config = new TbCheckAlarmStatusNodeConfig();
-        config.setAlarmStatusList(Collections.emptyList());
+        config.setAlarmStatusList(Arrays.asList(AlarmStatus.ACTIVE_ACK, AlarmStatus.ACTIVE_UNACK));
         return config;
     }
 }
