2ca4b78cf9db67888bdcfd3ecb25286c4248ef67
Vladyslav_Prykhodko <vprykhodko@thingsboard.io>
2020-04-06 11:04:03 +0300

    Improvement view on mobile device

--- ui-ngx/src/app/modules/home/pages/asset/asset.module.ts
+++ ui-ngx/src/app/modules/home/pages/asset/asset.module.ts
@@ -14,14 +14,14 @@
 /// limitations under the License.
 ///
 
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {SharedModule} from '@shared/shared.module';
-import {HomeDialogsModule} from '../../dialogs/home-dialogs.module';
-import {AssetComponent} from './asset.component';
-import {AssetTableHeaderComponent} from './asset-table-header.component';
-import {AssetRoutingModule} from './asset-routing.module';
-import {HomeComponentsModule} from '@modules/home/components/home-components.module';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SharedModule } from '@shared/shared.module';
+import { HomeDialogsModule } from '../../dialogs/home-dialogs.module';
+import { AssetComponent } from './asset.component';
+import { AssetTableHeaderComponent } from './asset-table-header.component';
+import { AssetRoutingModule } from './asset-routing.module';
+import { HomeComponentsModule } from '@modules/home/components/home-components.module';
 import { AssetTabsComponent } from '@home/pages/asset/asset-tabs.component';
 
 @NgModule({
