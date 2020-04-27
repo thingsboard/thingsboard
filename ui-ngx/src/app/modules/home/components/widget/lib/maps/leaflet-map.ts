2fc25ad33d870e01c8f9a0310c103fd40c161444
Igor Kulikov <ikulikov@thingsboard.io>
2020-04-27 18:37:26 +0300

    Remove redudant css import

--- ui-ngx/src/app/modules/home/components/widget/lib/maps/leaflet-map.ts
+++ ui-ngx/src/app/modules/home/components/widget/lib/maps/leaflet-map.ts
@@ -17,8 +17,6 @@
 import L, { LatLngBounds, LatLngTuple } from 'leaflet';
 
 import 'leaflet-providers';
-import 'leaflet.markercluster/dist/MarkerCluster.css';
-import 'leaflet.markercluster/dist/MarkerCluster.Default.css';
 import LM from 'leaflet.markercluster/dist/leaflet.markercluster';
 
 import {
