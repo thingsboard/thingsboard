069a2939a69e247ba8e2900eb1c7ddf9e6d022cd
viktor <vzikratyi@thingsboard.io>
2020-05-13 13:54:10 +0300

    start tb-cassandra with 'thingsboard' user (with postgresql server)

--- msa/tb/docker-cassandra/stop-db.sh
+++ msa/tb/docker-cassandra/stop-db.sh
@@ -21,6 +21,10 @@ CASSANDRA_PID=$(ps aux | grep '[c]assandra' | awk '{print $2}')
 echo "Stopping cassandra (pid ${CASSANDRA_PID})."
 kill -SIGTERM ${CASSANDRA_PID}
 
+PG_CTL=$(find /usr/lib/postgresql/ -name pg_ctl)
+echo "Stopping postgres."
+${PG_CTL} stop
+
 while [ -e /proc/${CASSANDRA_PID} ]
 do
     echo "Waiting for cassandra to stop."
