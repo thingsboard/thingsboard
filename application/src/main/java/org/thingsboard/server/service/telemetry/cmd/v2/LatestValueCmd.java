2aaa51fe8670284e7e7c30e574e76a4c11a2d854
Igor Kulikov <ikulikov@thingsboard.io>
2020-06-19 20:40:41 +0300

    Data query serialization improvements

--- application/src/main/java/org/thingsboard/server/service/telemetry/cmd/v2/LatestValueCmd.java
+++ application/src/main/java/org/thingsboard/server/service/telemetry/cmd/v2/LatestValueCmd.java
@@ -15,10 +15,12 @@
  */
 package org.thingsboard.server.service.telemetry.cmd.v2;
 
+import lombok.Data;
 import org.thingsboard.server.common.data.query.EntityKey;
 
 import java.util.List;
 
+@Data
 public class LatestValueCmd {
 
     private List<EntityKey> keys;
--- common/data/src/main/java/org/thingsboard/server/common/data/query/EntityFilter.java
+++ common/data/src/main/java/org/thingsboard/server/common/data/query/EntityFilter.java
@@ -16,9 +16,11 @@
 package org.thingsboard.server.common.data.query;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 @JsonTypeInfo(
         use = JsonTypeInfo.Id.NAME,
         include = JsonTypeInfo.As.PROPERTY,
