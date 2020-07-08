09bdfebab3825bf006bd477dd5f4e10c6a7bfef8
vzikratyi <vzikratyi@thingsboard.io>
2020-07-08 15:22:46 +0300

    Added producer stats to 'transport' services

--- common/queue/src/main/java/org/thingsboard/server/queue/TbQueueRequestTemplate.java
+++ common/queue/src/main/java/org/thingsboard/server/queue/TbQueueRequestTemplate.java
@@ -16,6 +16,7 @@
 package org.thingsboard.server.queue;
 
 import com.google.common.util.concurrent.ListenableFuture;
+import org.thingsboard.server.queue.stats.MessagesStats;
 
 public interface TbQueueRequestTemplate<Request extends TbQueueMsg, Response extends TbQueueMsg> {
 
@@ -25,4 +26,5 @@ public interface TbQueueRequestTemplate<Request extends TbQueueMsg, Response ext
 
     void stop();
 
+    void setMessagesStats(MessagesStats messagesStats);
 }
