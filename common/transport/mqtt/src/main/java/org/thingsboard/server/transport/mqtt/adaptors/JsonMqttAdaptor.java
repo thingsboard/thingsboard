52bed1748958ecf97bd33470875dc444cbcadb6d
nordmif <dronfim@mail.ru>
2020-07-21 15:03:45 +0300

    changed logging level to WARN

--- common/transport/mqtt/src/main/java/org/thingsboard/server/transport/mqtt/adaptors/JsonMqttAdaptor.java
+++ common/transport/mqtt/src/main/java/org/thingsboard/server/transport/mqtt/adaptors/JsonMqttAdaptor.java
@@ -207,7 +207,7 @@ public class JsonMqttAdaptor implements MqttTransportAdaptor {
         try {
             return new JsonParser().parse(payload);
         } catch (JsonSyntaxException ex) {
-            log.error("Payload is in incorrect format: {}", payload);
+            log.warn("Payload is in incorrect format: {}", payload);
             throw new AdaptorException(ex);
         }
     }
