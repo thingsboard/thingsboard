a37aad777949a93282a4fca9bafd85c50ee4a1a1
Oscar Aguilera <oscar@aguilera.xyz>
2020-08-05 22:37:00 -0400

    modified:   msa/js-executor/package-lock.json 	modified:   msa/web-ui/package-lock.json 	modified:   ui-ngx/angular.json 	modified:   ui-ngx/pom.xml 	modified:   ui-ngx/src/app/app.component.ts 	modified:   ui-ngx/src/app/modules/home/pages/dashboard/dashboard-page.component.html 	modified:   ui-ngx/src/app/shared/components/footer.component.html 	modified:   ui-ngx/src/assets/locale/locale.constant-cs_CZ.json 	modified:   ui-ngx/src/assets/locale/locale.constant-de_DE.json 	modified:   ui-ngx/src/assets/locale/locale.constant-en_US.json 	modified:   ui-ngx/src/assets/locale/locale.constant-es_ES.json 	modified:   ui-ngx/src/assets/locale/locale.constant-fa_IR.json 	modified:   ui-ngx/src/assets/locale/locale.constant-fr_FR.json 	modified:   ui-ngx/src/assets/locale/locale.constant-it_IT.json 	modified:   ui-ngx/src/assets/locale/locale.constant-ja_JA.json 	modified:   ui-ngx/src/assets/locale/locale.constant-ro_RO.json 	modified:   ui-ngx/src/assets/locale/locale.constant-ru_RU.json 	modified:   ui-ngx/src/assets/locale/locale.constant-uk_UA.json 	modified:   ui-ngx/src/assets/logo_title_white.svg 	modified:   ui-ngx/src/assets/logo_white.svg 	modified:   ui-ngx/src/environments/environment.prod.ts 	modified:   ui-ngx/src/environments/environment.ts 	modified:   ui-ngx/src/index.html 	modified:   ui-ngx/src/theme.scss 	deleted:    ui-ngx/src/thingsboard.ico

--- ui-ngx/src/environments/environment.prod.ts
+++ ui-ngx/src/environments/environment.prod.ts
@@ -15,7 +15,7 @@
 ///
 
 export const environment = {
-  appTitle: 'ThingsBoard',
+  appTitle: 'iGromi',
   production: true,
 // @ts-ignore
   tbVersion: TB_VERSION,
--- ui-ngx/src/environments/environment.ts
+++ ui-ngx/src/environments/environment.ts
@@ -19,7 +19,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  appTitle: 'ThingsBoard',
+  appTitle: 'iGromi',
   production: false,
 // @ts-ignore
   tbVersion: TB_VERSION,
