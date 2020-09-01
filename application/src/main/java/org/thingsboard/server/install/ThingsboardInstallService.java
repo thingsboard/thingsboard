f647c69f509db991039f7b0dd7a990c7fa860f2c
Dmytro Shvaika <dshvaika@thingsboard.io>
2020-09-01 19:57:57 +0300

    cherry-pick bfdd52c from fix/ttlCleanUpServices and added the upgrade from 3.1.1

--- application/src/main/java/org/thingsboard/server/install/ThingsboardInstallService.java
+++ application/src/main/java/org/thingsboard/server/install/ThingsboardInstallService.java
@@ -175,6 +175,11 @@ public class ThingsboardInstallService {
                         case "3.1.0":
                             log.info("Upgrading ThingsBoard from version 3.1.0 to 3.1.1 ...");
                             databaseEntitiesUpgradeService.upgradeDatabase("3.1.0");
+                        case "3.1.1":
+                            log.info("Upgrading ThingsBoard from version 3.1.1 to 3.1.2 ...");
+                            if (databaseTsUpgradeService != null) {
+                                databaseTsUpgradeService.upgradeDatabase("3.1.1");
+                            }
                             log.info("Updating system data...");
                             systemDataLoaderService.updateSystemWidgets();
                             break;
