f6bd8ac1f66fffedd09efa141531d2f350028fc0
Kalutka Zhenya <ykalytka@thingsboard.io>
2020-09-28 14:48:47 +0300

    Fix marker move and marker point

--- ui-ngx/src/app/modules/home/components/widget/lib/maps/leaflet-map.ts
+++ ui-ngx/src/app/modules/home/components/widget/lib/maps/leaflet-map.ts
@@ -131,10 +131,13 @@ export default abstract class LeafletMap {
                       tooltipAnchor: [16, -28],
                       shadowSize: [41, 41]
                     });
+                    const customLatLng = this.convertToCustomFormat(mousePositionOnMap);
+                      mousePositionOnMap.lat = customLatLng[this.options.latKeyName];
+                      mousePositionOnMap.lng = customLatLng[this.options.lngKeyName];
+
                     const newMarker = L.marker(mousePositionOnMap, { icon }).addTo(this.map);
                     this.addMarkers.push(newMarker);
                     const datasourcesList = document.createElement('div');
-                    const customLatLng = this.convertToCustomFormat(mousePositionOnMap);
                     const header = document.createElement('p');
                     header.appendChild(document.createTextNode('Select entity:'));
                     header.setAttribute('style', 'font-size: 14px; margin: 8px 0');
@@ -415,7 +418,6 @@ export default abstract class LeafletMap {
       }else if(position.lng < -180){
         position.lng = -180;
       };
-
       return {
         [this.options.latKeyName]: position.lat,
         [this.options.lngKeyName]: position.lng
