4f30678554671117705fe53ff2a63112ecf0ab2f
Vladyslav_Prykhodko <vprykhodko@thingsboard.io>
2020-09-29 10:33:29 +0300

    UI Moved device credentials for new component

--- ui-ngx/src/app/modules/home/components/home-components.module.ts
+++ ui-ngx/src/app/modules/home/components/home-components.module.ts
@@ -107,6 +107,7 @@ import { AlarmRuleKeyFiltersDialogComponent } from './profile/alarm/alarm-rule-k
 import { FilterTextComponent } from './filter/filter-text.component';
 import { AddDeviceProfileDialogComponent } from './profile/add-device-profile-dialog.component';
 import { RuleChainAutocompleteComponent } from './rule-chain/rule-chain-autocomplete.component';
+import { DeviceCredentialsComponent } from './device/device-credentials.component';
 
 @NgModule({
   declarations:
@@ -196,7 +197,8 @@ import { RuleChainAutocompleteComponent } from './rule-chain/rule-chain-autocomp
       DeviceProfileComponent,
       DeviceProfileDialogComponent,
       AddDeviceProfileDialogComponent,
-      RuleChainAutocompleteComponent
+      RuleChainAutocompleteComponent,
+      DeviceCredentialsComponent
     ],
   imports: [
     CommonModule,
@@ -275,7 +277,8 @@ import { RuleChainAutocompleteComponent } from './rule-chain/rule-chain-autocomp
     DeviceProfileComponent,
     DeviceProfileDialogComponent,
     AddDeviceProfileDialogComponent,
-    RuleChainAutocompleteComponent
+    RuleChainAutocompleteComponent,
+    DeviceCredentialsComponent
   ],
   providers: [
     WidgetComponentService,
