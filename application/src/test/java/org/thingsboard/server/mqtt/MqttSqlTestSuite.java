9fef6b02a3a3c94066e41ec10ee3b57da85b6f28
ShvaykaD <dshvaika@thingsboard.io>
2020-10-02 11:31:27 +0300

    [3.2] Feature/Proto Converter (#3423)
    * device post-telemetry & post-attributes & claim
    
    * device rpc to/from server & attributes request, attributes updates
    
    * refactoring & added implementation for gateway protos api
    
    * added timeseries/attributes mqtt tests
    
    * fix MqttTimseriesIntegrationTest values asserts
    
    * mqtt attributes tests improvements
    
    * optimized time for telemetry & attributes tests
    
    * update proto files, refactoring converter, attribute requests tests
    
    * added claim tests, attribute request test
    
    * added deleted keys to gateway response on attributes request & refactored tests
    
    * added attribute updates test & refactored attribute requests tests
    
    * added attribute updates tests for gateways
    
    * added tests for RPC
    
    * fix tests & cleanup code
    
    * fix typo & cleanup transport.proto file
    
    * added more timeouts
    
    * revert handleGetAttributesRequest method
    
    * revert package-locks
    
    * fix getJsonObjectForGateway method
    
    * fix validateSharedResponseGateway method in AbstractMqttAttributesRequestIntegrationTest
    
    * fix mqtt topics
    
    * fix license headers
    
    * refactor tests
    
    * remove todo and lck files from pull
    
    * improvements for claiming tests
    
    * update device creation logic from gateway request
    
    * refactoring
    
    * extract TransportService process calls to private methods
    
    * fix duplicates & removed empty lines

--- application/src/test/java/org/thingsboard/server/mqtt/MqttSqlTestSuite.java
+++ application/src/test/java/org/thingsboard/server/mqtt/MqttSqlTestSuite.java
@@ -27,7 +27,11 @@ import java.util.Arrays;
 @RunWith(ClasspathSuite.class)
 @ClasspathSuite.ClassnameFilters({
         "org.thingsboard.server.mqtt.rpc.sql.*Test",
-        "org.thingsboard.server.mqtt.telemetry.sql.*Test"
+        "org.thingsboard.server.mqtt.telemetry.timeseries.sql.*Test",
+        "org.thingsboard.server.mqtt.telemetry.attributes.sql.*Test",
+        "org.thingsboard.server.mqtt.attributes.updates.sql.*Test",
+        "org.thingsboard.server.mqtt.attributes.request.sql.*Test",
+        "org.thingsboard.server.mqtt.claim.sql.*Test"
 })
 public class MqttSqlTestSuite {
 
