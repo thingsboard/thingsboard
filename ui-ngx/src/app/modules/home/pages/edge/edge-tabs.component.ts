2309193d83956ce170e00775a0841855c8ae6075
Artem Babak <artemio.babak@gmail.com>
2020-11-05 15:56:58 +0200

    added buttons Copy Edge Key and Secret instead of old icon buttons

--- ui-ngx/src/app/modules/home/pages/edge/edge-tabs.component.ts
+++ ui-ngx/src/app/modules/home/pages/edge/edge-tabs.component.ts
@@ -14,11 +14,11 @@
 /// limitations under the License.
 ///
 
-import {Component} from '@angular/core';
-import {Store} from "@ngrx/store";
-import {AppState} from "@core/core.state";
-import {EdgeInfo} from "@shared/models/edge.models";
-import {EntityTabsComponent} from "@home/components/entity/entity-tabs.component";
+import { Component } from '@angular/core';
+import { Store } from "@ngrx/store";
+import { AppState } from "@core/core.state";
+import { EdgeInfo } from "@shared/models/edge.models";
+import { EntityTabsComponent } from "@home/components/entity/entity-tabs.component";
 
 @Component({
   selector: 'tb-edge-tabs',
