dcc9023349c59931eb9314a92ce8d2c4d4fa87b2
Sanad Amin <sanad@altshiftcreative.com>
2020-12-12 19:17:01 +0200

    Some Design Modifications

--- ui-ngx/src/app/shared/components/logo.component.ts
+++ ui-ngx/src/app/shared/components/logo.component.ts
@@ -23,7 +23,7 @@ import { Component } from '@angular/core';
 })
 export class LogoComponent {
 
-  logo = 'assets/logo_title_white.svg';
+  logo = 'assets/logo_title_white1.svg';
 
   gotoThingsboard(): void {
     window.open('https://thingsboard.io', '_blank');
--- ui-ngx/src/environments/environment.prod.ts
+++ ui-ngx/src/environments/environment.prod.ts
@@ -15,7 +15,7 @@
 ///
 
 export const environment = {
-  appTitle: 'ThingsBoard',
+  appTitle: 'BlueWaves',
   production: true,
 // @ts-ignore
   tbVersion: TB_VERSION,
