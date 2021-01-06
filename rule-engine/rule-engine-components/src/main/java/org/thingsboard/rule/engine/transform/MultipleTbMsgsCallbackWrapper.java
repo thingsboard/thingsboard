4aa55bccf84519fd7923ffd38cb5eed8541dda0c
Illia Barkov <ibarkov@thingsboard.io>
2021-01-06 14:41:01 +0200

    Added ability to return arrays in transformation script node (#3910)
    * Added ability to return arrays in transformation script node
    
    * fix typo
    
    * Refactoring
    
    * Improvements
    
    * Improvements

---
+++ rule-engine/rule-engine-components/src/main/java/org/thingsboard/rule/engine/transform/MultipleTbMsgsCallbackWrapper.java
@@ -0,0 +1,45 @@
+/**
+ * Copyright © 2016-2020 The Thingsboard Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.thingsboard.rule.engine.transform;
+
+import org.thingsboard.server.common.msg.queue.RuleEngineException;
+import org.thingsboard.server.common.msg.queue.TbMsgCallback;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class MultipleTbMsgsCallbackWrapper implements TbMsgCallbackWrapper {
+
+    private final AtomicInteger tbMsgsCallbackCount;
+    private final TbMsgCallback callback;
+
+    public MultipleTbMsgsCallbackWrapper(int tbMsgsCallbackCount, TbMsgCallback callback) {
+        this.tbMsgsCallbackCount = new AtomicInteger(tbMsgsCallbackCount);
+        this.callback = callback;
+    }
+
+    @Override
+    public void onSuccess() {
+        if (tbMsgsCallbackCount.decrementAndGet() <= 0) {
+            callback.onSuccess();
+        }
+    }
+
+    @Override
+    public void onFailure(Throwable t) {
+        callback.onFailure(new RuleEngineException(t.getMessage()));
+    }
+}
+
