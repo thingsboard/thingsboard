bff7297bf285ab4bdef8bbd3ab39eda6a0d7b8da
Valerii Sosliuk <valerii@thingsboard.io>
2021-01-20 10:45:57 +0200

    Removed unused imports

--- application/src/main/java/org/thingsboard/server/controller/RuleChainController.java
+++ application/src/main/java/org/thingsboard/server/controller/RuleChainController.java
@@ -20,7 +20,6 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import lombok.extern.slf4j.Slf4j;
-import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
