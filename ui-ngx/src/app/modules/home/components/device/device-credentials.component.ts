e0591bc6d0c2d611f4098c39084959fe6681baf6
nickAS21 <nick@avalr.com.ua>
2021-02-20 09:50:29 +0200

    Lwm2m:  front add validator json for security config value delete debug

--- ui-ngx/src/app/modules/home/components/device/device-credentials.component.ts
+++ ui-ngx/src/app/modules/home/components/device/device-credentials.component.ts
@@ -286,7 +286,6 @@ export class DeviceCredentialsComponent implements ControlValueAccessor, OnInit,
 
   private jsonValidator(control: FormControl) {
     try {
-      debugger;
       JSON.parse(control.value);
       return null
     } catch (e) {
