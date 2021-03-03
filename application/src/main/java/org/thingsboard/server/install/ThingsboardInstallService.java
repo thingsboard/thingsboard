3255eb9027792880c063047ae8d51bbbec79d5c2
Igor Kulikov <ikulikov@thingsboard.io>
2021-03-03 12:02:06 +0200

    Version set to 3.2.2-SNAPSHOT

--- application/src/main/java/org/thingsboard/server/install/ThingsboardInstallService.java
+++ application/src/main/java/org/thingsboard/server/install/ThingsboardInstallService.java
@@ -186,7 +186,7 @@ public class ThingsboardInstallService {
                             log.info("Upgrading ThingsBoard from version 3.2.0 to 3.2.1 ...");
                             databaseEntitiesUpgradeService.upgradeDatabase("3.2.0");
                         case "3.2.1":
-                            log.info("Upgrading ThingsBoard from version 3.2.1 to 3.3.0 ...");
+                            log.info("Upgrading ThingsBoard from version 3.2.1 to 3.2.2 ...");
                             if (databaseTsUpgradeService != null) {
                                 databaseTsUpgradeService.upgradeDatabase("3.2.1");
                             }
