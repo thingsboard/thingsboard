805cc8d8ea653e37362bb14177ab0ca4a55ba72f
Volodymyr Babak <volodymyr.babak@gmail.com>
2021-03-05 16:57:58 +0200

    eval is not thread safe. Added lock to avoid 'No such function invokeInternalXXX' exception

--- application/src/main/java/org/thingsboard/server/service/script/AbstractNashornJsInvokeService.java
+++ application/src/main/java/org/thingsboard/server/service/script/AbstractNashornJsInvokeService.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -40,6 +40,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.ReentrantLock;
 
 @Slf4j
 public abstract class AbstractNashornJsInvokeService extends AbstractJsInvokeService {
@@ -55,6 +56,8 @@ public abstract class AbstractNashornJsInvokeService extends AbstractJsInvokeSer
     private final AtomicInteger jsTimeoutMsgs = new AtomicInteger(0);
     private final FutureCallback<UUID> evalCallback = new JsStatCallback<>(jsEvalMsgs, jsTimeoutMsgs, jsFailedMsgs);
     private final FutureCallback<Object> invokeCallback = new JsStatCallback<>(jsInvokeMsgs, jsTimeoutMsgs, jsFailedMsgs);
+
+    private final ReentrantLock evalLock = new ReentrantLock();
 
     @Getter
     private final JsExecutorService jsExecutor;
@@ -121,10 +124,15 @@ public abstract class AbstractNashornJsInvokeService extends AbstractJsInvokeSer
         jsPushedMsgs.incrementAndGet();
         ListenableFuture<UUID> result = jsExecutor.executeAsync(() -> {
             try {
-                if (useJsSandbox()) {
-                    sandbox.eval(jsScript);
-                } else {
-                    engine.eval(jsScript);
+                evalLock.lock();
+                try {
+                    if (useJsSandbox()) {
+                        sandbox.eval(jsScript);
+                    } else {
+                        engine.eval(jsScript);
+                    }
+                } finally {
+                    evalLock.unlock();
                 }
                 scriptIdToNameMap.put(scriptId, functionName);
                 return scriptId;
