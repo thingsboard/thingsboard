92d360e14b61115f5a62ef7a43de2502fd87640c
Andrew Volostnykh <avolostnykh@thingsboard.io>
2021-03-10 19:31:01 +0200

    Refactoring of migration tool for new Thingsboard DB structure

---
+++ tools/src/main/java/org/thingsboard/client/tools/migrator/RelatedEntitiesParser.java
@@ -0,0 +1,76 @@
+package org.thingsboard.client.tools.migrator;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.LineIterator;
+import org.apache.commons.lang3.StringUtils;
+import org.thingsboard.server.common.data.EntityType;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class RelatedEntitiesParser {
+    private final Map<String, String> allEntityIdsAndTypes = new HashMap<>();
+
+    public RelatedEntitiesParser(File source) throws IOException {
+        processAllTables(FileUtils.lineIterator(source));
+    }
+
+    public String getEntityType(String uuid) {
+        return this.allEntityIdsAndTypes.get(uuid);
+    }
+
+    private boolean isBlockFinished(String line) {
+        return StringUtils.isBlank(line) || line.equals("\\.");
+    }
+
+    private void processAllTables(LineIterator lineIterator) {
+        String currentLine;
+        while(lineIterator.hasNext()) {
+            currentLine = lineIterator.nextLine();
+            if(currentLine.startsWith("COPY public.alarm")) {
+                processBlock(lineIterator, EntityType.ALARM);
+            } else if (currentLine.startsWith("COPY public.asset")) {
+                processBlock(lineIterator, EntityType.ASSET);
+            } else if (currentLine.startsWith("COPY public.customer")) {
+                processBlock(lineIterator, EntityType.CUSTOMER);
+            } else if (currentLine.startsWith("COPY public.dashboard")) {
+                processBlock(lineIterator, EntityType.DASHBOARD);
+            } else if (currentLine.startsWith("COPY public.device")) {
+                processBlock(lineIterator, EntityType.DEVICE);
+            } else if (currentLine.startsWith("COPY public.rule_chain")) {
+                processBlock(lineIterator, EntityType.RULE_CHAIN);
+            } else if (currentLine.startsWith("COPY public.rule_node")) {
+                processBlock(lineIterator, EntityType.RULE_NODE);
+            } else if (currentLine.startsWith("COPY public.tenant")) {
+                processBlock(lineIterator, EntityType.TENANT);
+            } else if (currentLine.startsWith("COPY public.tb_user")) {
+                processBlock(lineIterator, EntityType.USER);
+            } else if (currentLine.startsWith("COPY public.entity_view")) {
+                processBlock(lineIterator, EntityType.ENTITY_VIEW);
+            } else if (currentLine.startsWith("COPY public.widgets_bundle")) {
+                processBlock(lineIterator, EntityType.WIDGETS_BUNDLE);
+            } else if (currentLine.startsWith("COPY public.widget_type")) {
+                processBlock(lineIterator, EntityType.WIDGET_TYPE);
+            } else if (currentLine.startsWith("COPY public.tenant_profile")) {
+                processBlock(lineIterator, EntityType.TENANT_PROFILE);
+            } else if (currentLine.startsWith("COPY public.device_profile")) {
+                processBlock(lineIterator, EntityType.DEVICE_PROFILE);
+            } else if (currentLine.startsWith("COPY public.api_usage_state")) {
+                processBlock(lineIterator, EntityType.API_USAGE_STATE);
+            }
+        }
+    }
+
+    private void processBlock(LineIterator lineIterator, EntityType entityType) {
+        String currentLine;
+        while(lineIterator.hasNext()) {
+            currentLine = lineIterator.nextLine();
+            if(isBlockFinished(currentLine)) {
+                return;
+            }
+            allEntityIdsAndTypes.put(currentLine.split("\t")[0], entityType.name());
+        }
+    }
+}
