b8cc971e7248c66095ae958f6cf8742134f7e523
Vladyslav_Prykhodko <vprykhodko@thingsboard.io>
2021-03-12 13:07:19 +0200

    UI: Added validate EntityId before update dashboard state

--- ui-ngx/src/app/core/utils.ts
+++ ui-ngx/src/app/core/utils.ts
@@ -18,6 +18,8 @@ import _ from 'lodash';
 import { Observable, Subject } from 'rxjs';
 import { finalize, share } from 'rxjs/operators';
 import { Datasource } from '@app/shared/models/widget.models';
+import { EntityId } from '@shared/models/id/entity-id';
+import { NULL_UUID } from '@shared/models/id/has-uuid';
 
 const varsRegex = /\${([^}]*)}/g;
 
@@ -430,4 +432,8 @@ export function generateSecret(length?: number): string {
     return str;
   }
   return str.concat(generateSecret(length - str.length));
+}
+
+export function validateEntityId(entityId: EntityId): boolean {
+  return isDefinedAndNotNull(entityId.id) && entityId.id !== NULL_UUID && isDefinedAndNotNull(entityId.entityType);
 }
