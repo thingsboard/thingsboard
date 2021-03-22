1f6a5b7a2114b58492bcaeebe89160751939c8be
Igor Kulikov <ikulikov@thingsboard.io>
2021-03-22 12:42:58 +0200

    Fix timeseries tests

--- dao/src/test/java/org/thingsboard/server/dao/service/timeseries/BaseTimeseriesServiceTest.java
+++ dao/src/test/java/org/thingsboard/server/dao/service/timeseries/BaseTimeseriesServiceTest.java
@@ -143,52 +143,52 @@ public abstract class BaseTimeseriesServiceTest extends AbstractServiceTest {
     public void testFindByQueryAscOrder() throws Exception {
         DeviceId deviceId = new DeviceId(Uuids.timeBased());
 
+        saveEntries(deviceId, TS - 3);
         saveEntries(deviceId, TS - 2);
         saveEntries(deviceId, TS - 1);
-        saveEntries(deviceId, TS);
 
         List<ReadTsKvQuery> queries = new ArrayList<>();
         queries.add(new BaseReadTsKvQuery(STRING_KEY, TS - 3, TS, 0, 1000, Aggregation.NONE, "ASC"));
 
         List<TsKvEntry> entries = tsService.findAll(tenantId, deviceId, queries).get();
         Assert.assertEquals(3, entries.size());
-        Assert.assertEquals(toTsEntry(TS - 2, stringKvEntry), entries.get(0));
-        Assert.assertEquals(toTsEntry(TS - 1, stringKvEntry), entries.get(1));
-        Assert.assertEquals(toTsEntry(TS, stringKvEntry), entries.get(2));
+        Assert.assertEquals(toTsEntry(TS - 3, stringKvEntry), entries.get(0));
+        Assert.assertEquals(toTsEntry(TS - 2, stringKvEntry), entries.get(1));
+        Assert.assertEquals(toTsEntry(TS - 1, stringKvEntry), entries.get(2));
 
         EntityView entityView = saveAndCreateEntityView(deviceId, Arrays.asList(STRING_KEY));
 
         entries = tsService.findAll(tenantId, entityView.getId(), queries).get();
         Assert.assertEquals(3, entries.size());
-        Assert.assertEquals(toTsEntry(TS - 2, stringKvEntry), entries.get(0));
-        Assert.assertEquals(toTsEntry(TS - 1, stringKvEntry), entries.get(1));
-        Assert.assertEquals(toTsEntry(TS, stringKvEntry), entries.get(2));
+        Assert.assertEquals(toTsEntry(TS - 3, stringKvEntry), entries.get(0));
+        Assert.assertEquals(toTsEntry(TS - 2, stringKvEntry), entries.get(1));
+        Assert.assertEquals(toTsEntry(TS - 1, stringKvEntry), entries.get(2));
     }
 
     @Test
     public void testFindByQueryDescOrder() throws Exception {
         DeviceId deviceId = new DeviceId(Uuids.timeBased());
 
+        saveEntries(deviceId, TS - 3);
         saveEntries(deviceId, TS - 2);
         saveEntries(deviceId, TS - 1);
-        saveEntries(deviceId, TS);
 
         List<ReadTsKvQuery> queries = new ArrayList<>();
         queries.add(new BaseReadTsKvQuery(STRING_KEY, TS - 3, TS, 0, 1000, Aggregation.NONE, "DESC"));
 
         List<TsKvEntry> entries = tsService.findAll(tenantId, deviceId, queries).get();
         Assert.assertEquals(3, entries.size());
-        Assert.assertEquals(toTsEntry(TS, stringKvEntry), entries.get(0));
-        Assert.assertEquals(toTsEntry(TS - 1, stringKvEntry), entries.get(1));
-        Assert.assertEquals(toTsEntry(TS - 2, stringKvEntry), entries.get(2));
+        Assert.assertEquals(toTsEntry(TS - 1, stringKvEntry), entries.get(0));
+        Assert.assertEquals(toTsEntry(TS - 2, stringKvEntry), entries.get(1));
+        Assert.assertEquals(toTsEntry(TS - 3, stringKvEntry), entries.get(2));
 
         EntityView entityView = saveAndCreateEntityView(deviceId, Arrays.asList(STRING_KEY));
 
         entries = tsService.findAll(tenantId, entityView.getId(), queries).get();
         Assert.assertEquals(3, entries.size());
-        Assert.assertEquals(toTsEntry(TS, stringKvEntry), entries.get(0));
-        Assert.assertEquals(toTsEntry(TS - 1, stringKvEntry), entries.get(1));
-        Assert.assertEquals(toTsEntry(TS - 2, stringKvEntry), entries.get(2));
+        Assert.assertEquals(toTsEntry(TS - 1, stringKvEntry), entries.get(0));
+        Assert.assertEquals(toTsEntry(TS - 2, stringKvEntry), entries.get(1));
+        Assert.assertEquals(toTsEntry(TS - 3, stringKvEntry), entries.get(2));
     }
 
     @Test
