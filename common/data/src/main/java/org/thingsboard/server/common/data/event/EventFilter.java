e08d1b2c0734596fa6e01f5dac3f70daf1c43112
Andrew Shvayka <ashvayka@thingsboard.io>
2021-04-15 12:05:04 +0300

    Feature/event filters
    * Event filter implementation
    
    * Removed redundant imports
    
    * convert createdTime to column name
    
    * UI: Added ui to filter events parameters
    
    * Case sensitivity fixed, boolean Is Error added
    
    * UI: Added support event filter for error
    
    * Improvement for implementation of Event Filter
    
    * UI: Refactoring event filter
    
    * UI: Add trim
    
    * Filter added for all event types
    
    * UI: Updated filter events for new API
    
    * Refactoring
    
    * Code cleaning. Debug Rule Chain added
    
    * Removed redundant casts in JpaBaseEventDao, correct created_time check
    
    * Removed redundant abstract method in DebugEvent
    
    * refactoring of the filter queries
    
    * Filters for debug events
    
    * UI: Refactoring event table panel
    
    Co-authored-by: AndrewVolosytnykhThingsboard <avolostnykh@thingsboard.io>
    Co-authored-by: Vladyslav_Prykhodko <vprykhodko@thingsboard.io>
    Co-authored-by: AndrewVolosytnykhThingsboard <77969531+AndrewVolosytnykhThingsboard@users.noreply.github.com>

---
+++ common/data/src/main/java/org/thingsboard/server/common/data/event/EventFilter.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright © 2016-2021 The Thingsboard Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.thingsboard.server.common.data.event;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
+@JsonTypeInfo(
+        use = JsonTypeInfo.Id.NAME,
+        include = JsonTypeInfo.As.PROPERTY,
+        property = "eventType")
+@JsonSubTypes({
+        @JsonSubTypes.Type(value = DebugRuleNodeEventFilter.class, name = "DEBUG_RULE_NODE"),
+        @JsonSubTypes.Type(value = DebugRuleChainEventFilter.class, name = "DEBUG_RULE_CHAIN"),
+        @JsonSubTypes.Type(value = ErrorEventFilter.class, name = "ERROR"),
+        @JsonSubTypes.Type(value = LifeCycleEventFilter.class, name = "LC_EVENT"),
+        @JsonSubTypes.Type(value = StatisticsEventFilter.class, name = "STATS")
+})
+public interface EventFilter {
+    @JsonIgnore
+    EventType getEventType();
+
+    boolean hasFilterForJsonBody();
+
+}
