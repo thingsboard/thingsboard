6888a1b349de234db1f6f3a8b81326fb34f438b1
Sergey Matvienko <smatvienko@thingsboard.io>
2021-05-05 08:22:38 +0300

    new utility class tbStopWatch added to improve performance measurement experience

---
+++ common/util/src/main/java/org/thingsboard/common/util/TbStopWatch.java
@@ -0,0 +1,62 @@
+/**
+ * ThingsBoard, Inc. ("COMPANY") CONFIDENTIAL
+ *
+ * Copyright © 2016-2021 ThingsBoard, Inc. All Rights Reserved.
+ *
+ * NOTICE: All information contained herein is, and remains
+ * the property of ThingsBoard, Inc. and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to ThingsBoard, Inc.
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ *
+ * Dissemination of this information or reproduction of this material is strictly forbidden
+ * unless prior written permission is obtained from COMPANY.
+ *
+ * Access to the source code contained herein is hereby forbidden to anyone except current COMPANY employees,
+ * managers or contractors who have executed Confidentiality and Non-disclosure agreements
+ * explicitly covering such access.
+ *
+ * The copyright notice above does not evidence any actual or intended publication
+ * or disclosure  of  this source code, which includes
+ * information that is confidential and/or proprietary, and is a trade secret, of  COMPANY.
+ * ANY REPRODUCTION, MODIFICATION, DISTRIBUTION, PUBLIC  PERFORMANCE,
+ * OR PUBLIC DISPLAY OF OR THROUGH USE  OF THIS  SOURCE CODE  WITHOUT
+ * THE EXPRESS WRITTEN CONSENT OF COMPANY IS STRICTLY PROHIBITED,
+ * AND IN VIOLATION OF APPLICABLE LAWS AND INTERNATIONAL TREATIES.
+ * THE RECEIPT OR POSSESSION OF THIS SOURCE CODE AND/OR RELATED INFORMATION
+ * DOES NOT CONVEY OR IMPLY ANY RIGHTS TO REPRODUCE, DISCLOSE OR DISTRIBUTE ITS CONTENTS,
+ * OR TO MANUFACTURE, USE, OR SELL ANYTHING THAT IT  MAY DESCRIBE, IN WHOLE OR IN PART.
+ */
+package org.thingsboard.common.util;
+
+import org.springframework.util.StopWatch;
+
+/**
+ * Utility method that extends Spring Framework StopWatch
+ * It is a MONOTONIC time stopwatch.
+ * It is a replacement for any measurements with a wall-clock like System.currentTimeMillis()
+ * It is not affected by leap second, day-light saving and wall-clock adjustments by manual or network time synchronization
+ * The main features is a single call for common use cases:
+ *  - create and start: TbStopWatch sw = TbStopWatch.startNew()
+ *  - stop and get: sw.stopAndGetTotalTimeMillis() or sw.stopAndGetLastTaskTimeMillis()
+ * */
+public class TbStopWatch extends StopWatch {
+
+    public static TbStopWatch startNew(){
+        TbStopWatch stopWatch = new TbStopWatch();
+        stopWatch.start();
+        return stopWatch;
+    }
+
+    public long stopAndGetTotalTimeMillis(){
+        stop();
+        return getTotalTimeMillis();
+    }
+
+    public long stopAndGetLastTaskTimeMillis(){
+        stop();
+        return getLastTaskTimeMillis();
+    }
+
+}
