eae41d112f7cef82c4cc07dce4c4188f0c4f2e35
Volodymyr Babak <volodymyr.babak@gmail.com>
2021-05-06 18:10:51 +0300

    Refactored

--- rule-engine/rule-engine-components/src/main/java/org/thingsboard/rule/engine/kafka/TbKafkaNode.java
+++ rule-engine/rule-engine-components/src/main/java/org/thingsboard/rule/engine/kafka/TbKafkaNode.java
@@ -71,10 +71,6 @@ public class TbKafkaNode implements TbNode {
 
     @Override
     public void init(TbContext ctx, TbNodeConfiguration configuration) throws TbNodeException {
-        // Ugly workaround to fix org.apache.kafka.common.KafkaException: javax.security.auth.login.LoginException: unable to find LoginModule class
-        // details: https://stackoverflow.com/questions/57574901/kafka-java-client-classloader-doesnt-find-sasl-scram-login-class
-        Thread.currentThread().setContextClassLoader(this.getClass().getClassLoader());
-
         this.config = TbNodeUtils.convert(configuration, TbKafkaNodeConfiguration.class);
         Properties properties = new Properties();
         properties.put(ProducerConfig.CLIENT_ID_CONFIG, "producer-tb-kafka-node-" + ctx.getSelfId().getId().toString() + "-" + ctx.getServiceId());
@@ -92,6 +88,9 @@ public class TbKafkaNode implements TbNode {
         addMetadataKeyValuesAsKafkaHeaders = BooleanUtils.toBooleanDefaultIfNull(config.isAddMetadataKeyValuesAsKafkaHeaders(), false);
         toBytesCharset = config.getKafkaHeadersCharset() != null ? Charset.forName(config.getKafkaHeadersCharset()) : StandardCharsets.UTF_8;
         try {
+            // Ugly workaround to fix org.apache.kafka.common.KafkaException: javax.security.auth.login.LoginException: unable to find LoginModule class
+            // details: https://stackoverflow.com/questions/57574901/kafka-java-client-classloader-doesnt-find-sasl-scram-login-class
+            Thread.currentThread().setContextClassLoader(this.getClass().getClassLoader());
             this.producer = new KafkaProducer<>(properties);
         } catch (Exception e) {
             throw new TbNodeException(e);
