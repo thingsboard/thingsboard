ff364f77b583c170ab36839cae258a1b5ac717f1
nickAS21 <nick@thingsboard.io>
2021-05-27 10:31:56 +0300

    RPC: optimized handleDeviceRPCRequest

--- application/src/main/java/org/thingsboard/server/controller/RpcController.java
+++ application/src/main/java/org/thingsboard/server/controller/RpcController.java
@@ -40,7 +40,6 @@ import org.thingsboard.server.common.data.id.DeviceId;
 import org.thingsboard.server.common.data.id.EntityId;
 import org.thingsboard.server.common.data.id.TenantId;
 import org.thingsboard.server.common.data.id.UUIDBased;
-import org.thingsboard.server.common.data.rpc.RpcRequest;
 import org.thingsboard.server.common.data.rpc.ToDeviceRpcRequestBody;
 import org.thingsboard.server.common.msg.rpc.ToDeviceRpcRequest;
 import org.thingsboard.server.queue.util.TbCoreComponent;
@@ -97,23 +96,13 @@ public class RpcController extends BaseController {
     private DeferredResult<ResponseEntity> handleDeviceRPCRequest(boolean oneWay, DeviceId deviceId, String requestBody) throws ThingsboardException {
         try {
             JsonNode rpcRequestBody = jsonMapper.readTree(requestBody);
-            RpcRequest cmd = new RpcRequest(rpcRequestBody.get("method").asText(),
-                    jsonMapper.writeValueAsString(rpcRequestBody.get("params")));
-
-            if (rpcRequestBody.has("timeout")) {
-                cmd.setTimeout(rpcRequestBody.get("timeout").asLong());
-            }
-            if (rpcRequestBody.has("requestUUID")) {
-                UUID requestUUID = UUID.fromString(rpcRequestBody.get("requestUUID").asText());
-                cmd.setRequestUUID(requestUUID);
-            }
+            ToDeviceRpcRequestBody body = new ToDeviceRpcRequestBody(rpcRequestBody.get("method").asText(), jsonMapper.writeValueAsString(rpcRequestBody.get("params")));
             SecurityUser currentUser = getCurrentUser();
             TenantId tenantId = currentUser.getTenantId();
             final DeferredResult<ResponseEntity> response = new DeferredResult<>();
-            long timeout = cmd.getTimeout() != null ? cmd.getTimeout() : defaultTimeout;
+            long timeout = rpcRequestBody.has("timeout") ? rpcRequestBody.get("timeout").asLong() : defaultTimeout;
             long expTime = System.currentTimeMillis() + Math.max(minTimeout, timeout);
-            UUID rpcRequestUUID = cmd.getRequestUUID() != null ? cmd.getRequestUUID() : UUID.randomUUID();
-            ToDeviceRpcRequestBody body = new ToDeviceRpcRequestBody(cmd.getMethodName(), cmd.getRequestData());
+            UUID rpcRequestUUID = rpcRequestBody.has("requestUUID") ? UUID.fromString(rpcRequestBody.get("requestUUID").asText()) : UUID.randomUUID();
             accessValidator.validate(currentUser, Operation.RPC_CALL, deviceId, new HttpValidationCallback(response, new FutureCallback<DeferredResult<ResponseEntity>>() {
                 @Override
                 public void onSuccess(@Nullable DeferredResult<ResponseEntity> result) {
