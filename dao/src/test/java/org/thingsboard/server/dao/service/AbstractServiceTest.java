1c22a4b35af2a556018eb21f78e7566357802e9c
YevhenBondarenko <ybondarenko@thingsboard.io>
2021-06-04 10:38:49 +0300

    fixed tests

--- dao/src/test/java/org/thingsboard/server/dao/service/AbstractServiceTest.java
+++ dao/src/test/java/org/thingsboard/server/dao/service/AbstractServiceTest.java
@@ -59,7 +59,6 @@ import org.thingsboard.server.dao.relation.RelationService;
 import org.thingsboard.server.dao.resource.ResourceService;
 import org.thingsboard.server.dao.rule.RuleChainService;
 import org.thingsboard.server.dao.settings.AdminSettingsService;
-import org.thingsboard.server.dao.tenant.DefaultTbTenantProfileCache;
 import org.thingsboard.server.dao.tenant.TenantProfileService;
 import org.thingsboard.server.dao.tenant.TenantService;
 import org.thingsboard.server.dao.timeseries.TimeseriesService;
@@ -161,9 +160,6 @@ public abstract class AbstractServiceTest {
 
     @Autowired
     protected OtaPackageService otaPackageService;
-
-    @Autowired
-    protected DefaultTbTenantProfileCache tenantProfileCache;
 
     public class IdComparator<D extends HasId> implements Comparator<D> {
         @Override
--- dao/src/test/java/org/thingsboard/server/dao/service/BaseDeviceProfileServiceTest.java
+++ dao/src/test/java/org/thingsboard/server/dao/service/BaseDeviceProfileServiceTest.java
@@ -109,6 +109,7 @@ public class BaseDeviceProfileServiceTest extends AbstractServiceTest {
         firmware.setChecksumAlgorithm(ChecksumAlgorithm.SHA256);
         firmware.setChecksum("4bf5122f344554c53bde2ebb8cd2b7e3d1600ad631c385a5d7cce23c7785459a");
         firmware.setData(ByteBuffer.wrap(new byte[]{1}));
+        firmware.setDataSize(1L);
         OtaPackage savedFirmware = otaPackageService.saveOtaPackage(firmware);
 
         deviceProfile.setFirmwareId(savedFirmware.getId());
