10a55ea6d768f284d40162d7a318a83ab03e6796
Vladyslav_Prykhodko <vprykhodko@thingsboard.io>
2021-06-14 11:46:54 +0300

    UI: Refactoring call ngZone in table widgets

--- ui-ngx/src/app/modules/home/components/widget/lib/timeseries-table-widget.component.ts
+++ ui-ngx/src/app/modules/home/components/widget/lib/timeseries-table-widget.component.ts
@@ -193,9 +193,7 @@ export class TimeseriesTableWidgetComponent extends PageComponent implements OnI
   }
 
   public onDataUpdated() {
-    this.ngZone.run(() => {
-      this.updateCurrentSourceData();
-    });
+    this.updateCurrentSourceData();
   }
 
   private initialize() {
@@ -288,7 +286,7 @@ export class TimeseriesTableWidgetComponent extends PageComponent implements OnI
         if (this.actionCellDescriptors.length) {
           source.displayedColumns.push('actions');
         }
-        const tsDatasource = new TimeseriesDatasource(source, this.hideEmptyLines, this.dateFormatFilter, this.datePipe);
+        const tsDatasource = new TimeseriesDatasource(source, this.hideEmptyLines, this.dateFormatFilter, this.datePipe, this.ngZone);
         tsDatasource.dataUpdated(this.data);
         this.sources.push(source);
       }
@@ -547,7 +545,8 @@ class TimeseriesDatasource implements DataSource<TimeseriesRow> {
     private source: TimeseriesTableSource,
     private hideEmptyLines: boolean,
     private dateFormatFilter: string,
-    private datePipe: DatePipe
+    private datePipe: DatePipe,
+    private ngZone: NgZone
   ) {
     this.source.timeseriesDatasource = this;
   }
@@ -570,8 +569,10 @@ class TimeseriesDatasource implements DataSource<TimeseriesRow> {
       catchError(() => of(emptyPageData<TimeseriesRow>())),
     ).subscribe(
       (pageData) => {
-        this.rowsSubject.next(pageData.data);
-        this.pageDataSubject.next(pageData);
+        this.ngZone.run(() => {
+          this.rowsSubject.next(pageData.data);
+          this.pageDataSubject.next(pageData);
+        });
       }
     );
   }
