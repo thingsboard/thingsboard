d65b02df19da10c97ca4c11aea1910e5721e89e5
Sergey Matvienko <smatvienko@thingsboard.io>
2021-09-06 11:18:06 +0300

    test: count entities by query adjusted due to the loop relations in hierarchy

--- dao/src/test/java/org/thingsboard/server/dao/service/BaseEntityServiceTest.java
+++ dao/src/test/java/org/thingsboard/server/dao/service/BaseEntityServiceTest.java
@@ -170,7 +170,7 @@ public abstract class BaseEntityServiceTest extends AbstractServiceTest {
         EntityCountQuery countQuery = new EntityCountQuery(filter);
 
         long count = entityService.countEntitiesByQuery(tenantId, new CustomerId(CustomerId.NULL_UUID), countQuery);
-        Assert.assertEquals(30, count);
+        Assert.assertEquals(31, count); //due to the loop relations in hierarchy, the TenantId included in total count (1*Tenant + 5*Asset + 5*5*Devices = 31)
 
         filter.setFilters(Collections.singletonList(new RelationEntityTypeFilter("Contains", Collections.singletonList(EntityType.DEVICE))));
         count = entityService.countEntitiesByQuery(tenantId, new CustomerId(CustomerId.NULL_UUID), countQuery);
