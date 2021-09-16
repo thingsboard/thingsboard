547055bb4e38cd8d5a33db300da3cc6c9e716d87
Vladyslav_Prykhodko <vprykhodko@thingsboard.io>
2021-09-16 11:47:00 +0300

    UI: Fixed not updated data for timeseries widget in NONE aggregation type

--- ui-ngx/src/app/core/api/data-aggregator.ts
+++ ui-ngx/src/app/core/api/data-aggregator.ts
@@ -308,7 +308,7 @@ export class DataAggregator {
           }
           aggKeyData.delete(aggTimestamp);
           this.updatedData = true;
-        } else if (aggTimestamp < this.endTs) {
+        } else if (aggTimestamp < this.endTs || this.noAggregation) {
           const kvPair: [number, any] = [aggTimestamp, aggData.aggValue];
           keyData.push(kvPair);
         }
