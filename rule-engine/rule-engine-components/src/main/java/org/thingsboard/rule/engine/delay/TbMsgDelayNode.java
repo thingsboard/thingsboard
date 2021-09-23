72213235eb5d227cff3cd9df38ac2f7a33247580
Sergey Matvienko <smatvienko@thingsboard.io>
2021-09-23 21:07:06 +0300

    delay node is deprecated

--- rule-engine/rule-engine-components/src/main/java/org/thingsboard/rule/engine/delay/TbMsgDelayNode.java
+++ rule-engine/rule-engine-components/src/main/java/org/thingsboard/rule/engine/delay/TbMsgDelayNode.java
@@ -38,10 +38,10 @@ import static org.thingsboard.rule.engine.api.TbRelationTypes.SUCCESS;
 @Slf4j
 @RuleNode(
         type = ComponentType.ACTION,
-        name = "delay",
+        name = "delay (deprecated)",
         configClazz = TbMsgDelayNodeConfiguration.class,
-        nodeDescription = "Delays incoming message",
-        nodeDetails = "Delays messages for configurable period. Please note, this node acknowledges the message from the current queue (message will be removed from queue)",
+        nodeDescription = "Delays incoming message (deprecated)",
+        nodeDetails = "Deprecated! Delays messages for configurable period. Please note, this node acknowledges the message from the current queue (message will be removed from queue)",
         icon = "pause",
         uiResources = {"static/rulenode/rulenode-core-config.js"},
         configDirective = "tbActionNodeMsgDelayConfig"
