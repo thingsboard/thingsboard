e3306424b7e39a18a53437f61d5aff799004fdac
Sergey Matvienko <smatvienko@thingsboard.io>
2021-10-15 15:21:28 +0300

    AlarmNode: refactored alarm details build js template - moved to abstract class

--- rule-engine/rule-engine-components/src/main/java/org/thingsboard/rule/engine/action/TbAbstractAlarmNodeConfiguration.java
+++ rule-engine/rule-engine-components/src/main/java/org/thingsboard/rule/engine/action/TbAbstractAlarmNodeConfiguration.java
@@ -20,6 +20,20 @@ import lombok.Data;
 @Data
 public abstract class TbAbstractAlarmNodeConfiguration {
 
+    static final String ALARM_DETAILS_BUILD_JS_TEMPLATE = "" +
+            "//***DO NOT CHANGE THIS LINES***\n" +
+            "var details = {};\n" +
+            "if (metadata.prevAlarmDetails) {\n" +
+            "    details = JSON.parse(metadata.prevAlarmDetails);\n" +
+            "    //remove prevAlarmDetails from metadata\n" +
+            "    delete metadata.prevAlarmDetails;\n" +
+            "    //now metadata is the same as it comes IN this rule node" +
+            "}\n" +
+            "//***PLACE YOUR CODE BELOW***\n" +
+            "\n" +
+            "\n" +
+            "return details;";
+
     private String alarmType;
     private String alarmDetailsBuildJs;
 
