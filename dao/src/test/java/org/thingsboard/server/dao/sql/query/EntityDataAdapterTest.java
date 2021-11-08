3c340dc7d2a3c9cf1ecc73cd643c0de833a6a863
Sergey Matvienko <smatvienko@thingsboard.io>
2021-11-08 13:51:37 +0200

    tests added for update service for convertDeviceProfileAlarmRulesForVersion330

--- dao/src/test/java/org/thingsboard/server/dao/sql/query/EntityDataAdapterTest.java
+++ dao/src/test/java/org/thingsboard/server/dao/sql/query/EntityDataAdapterTest.java
@@ -15,14 +15,14 @@
  */
 package org.thingsboard.server.dao.sql.query;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-class EntityDataAdapterTest {
+public class EntityDataAdapterTest {
 
     @Test
-    void testConvertValue() {
+    public void testConvertValue() {
         assertThat(EntityDataAdapter.convertValue("500")).isEqualTo("500");
         assertThat(EntityDataAdapter.convertValue("500D")).isEqualTo("500D"); //do not convert to Double !!!
     }
