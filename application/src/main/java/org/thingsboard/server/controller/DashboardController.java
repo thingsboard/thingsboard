3e39791ed06e38a969938f9fbb5cb7d525985c53
Andrii Shvaika <ashvayka@thingsboard.io>
2021-11-09 15:27:14 +0200

    Fix UUID wiki url

--- application/src/main/java/org/thingsboard/server/controller/DashboardController.java
+++ application/src/main/java/org/thingsboard/server/controller/DashboardController.java
@@ -86,6 +86,7 @@ import static org.thingsboard.server.controller.ControllerConstants.TENANT_AUTHO
 import static org.thingsboard.server.controller.ControllerConstants.TENANT_ID;
 import static org.thingsboard.server.controller.ControllerConstants.TENANT_ID_PARAM_DESCRIPTION;
 import static org.thingsboard.server.controller.ControllerConstants.TENANT_OR_CUSTOMER_AUTHORITY_PARAGRAPH;
+import static org.thingsboard.server.controller.ControllerConstants.UUID_WIKI_LINK;
 
 @RestController
 @TbCoreComponent
@@ -166,7 +167,7 @@ public class DashboardController extends BaseController {
     }
 
     @ApiOperation(value = "Create Or Update Dashboard (saveDashboard)",
-            notes = "Create or update the Dashboard. When creating dashboard, platform generates Dashboard Id as [time-based UUID](https://en.wikipedia.org/wiki/Universally_unique_identifier#Version_1_(date-time_and_MAC_address)." +
+            notes = "Create or update the Dashboard. When creating dashboard, platform generates Dashboard Id as " + UUID_WIKI_LINK +
                     "The newly created Dashboard id will be present in the response. " +
                     "Specify existing Dashboard id to update the dashboard. " +
                     "Referencing non-existing dashboard Id will cause 'Not Found' error. " +
