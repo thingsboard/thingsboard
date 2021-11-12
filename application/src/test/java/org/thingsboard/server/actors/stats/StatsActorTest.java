69e5f4c2280f907749c2dca147fe7dc993e4867d
Sergey Matvienko <smatvienko@thingsboard.io>
2021-11-12 14:27:57 +0200

    stats: do not persist empty stats

---
+++ application/src/test/java/org/thingsboard/server/actors/stats/StatsActorTest.java
@@ -0,0 +1,84 @@
+/**
+ * ThingsBoard, Inc. ("COMPANY") CONFIDENTIAL
+ *
+ * Copyright © 2016-2021 ThingsBoard, Inc. All Rights Reserved.
+ *
+ * NOTICE: All information contained herein is, and remains
+ * the property of ThingsBoard, Inc. and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to ThingsBoard, Inc.
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ *
+ * Dissemination of this information or reproduction of this material is strictly forbidden
+ * unless prior written permission is obtained from COMPANY.
+ *
+ * Access to the source code contained herein is hereby forbidden to anyone except current COMPANY employees,
+ * managers or contractors who have executed Confidentiality and Non-disclosure agreements
+ * explicitly covering such access.
+ *
+ * The copyright notice above does not evidence any actual or intended publication
+ * or disclosure  of  this source code, which includes
+ * information that is confidential and/or proprietary, and is a trade secret, of  COMPANY.
+ * ANY REPRODUCTION, MODIFICATION, DISTRIBUTION, PUBLIC  PERFORMANCE,
+ * OR PUBLIC DISPLAY OF OR THROUGH USE  OF THIS  SOURCE CODE  WITHOUT
+ * THE EXPRESS WRITTEN CONSENT OF COMPANY IS STRICTLY PROHIBITED,
+ * AND IN VIOLATION OF APPLICABLE LAWS AND INTERNATIONAL TREATIES.
+ * THE RECEIPT OR POSSESSION OF THIS SOURCE CODE AND/OR RELATED INFORMATION
+ * DOES NOT CONVEY OR IMPLY ANY RIGHTS TO REPRODUCE, DISCLOSE OR DISTRIBUTE ITS CONTENTS,
+ * OR TO MANUFACTURE, USE, OR SELL ANYTHING THAT IT  MAY DESCRIBE, IN WHOLE OR IN PART.
+ */
+package org.thingsboard.server.actors.stats;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.thingsboard.server.actors.ActorSystemContext;
+import org.thingsboard.server.common.data.Event;
+import org.thingsboard.server.common.data.id.TenantId;
+import org.thingsboard.server.dao.event.EventService;
+import org.thingsboard.server.queue.discovery.TbServiceInfoProvider;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.willReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+class StatsActorTest {
+
+    StatsActor statsActor;
+    ActorSystemContext actorSystemContext;
+    EventService eventService;
+    TbServiceInfoProvider serviceInfoProvider;
+
+    @BeforeEach
+    void setUp() {
+        actorSystemContext = mock(ActorSystemContext.class);
+
+        eventService = mock(EventService.class);
+        willReturn(eventService).given(actorSystemContext).getEventService();
+        serviceInfoProvider = mock(TbServiceInfoProvider.class);
+        willReturn(serviceInfoProvider).given(actorSystemContext).getServiceInfoProvider();
+
+        statsActor = new StatsActor(actorSystemContext);
+    }
+
+    @Test
+    void givenEmptyStatMessage_whenOnStatsPersistMsg_thenNoAction() {
+        StatsPersistMsg emptyStats = new StatsPersistMsg(0, 0, TenantId.SYS_TENANT_ID, TenantId.SYS_TENANT_ID);
+        statsActor.onStatsPersistMsg(emptyStats);
+        verify(actorSystemContext, never()).getEventService();
+    }
+
+    @Test
+    void givenNonEmptyStatMessage_whenOnStatsPersistMsg_thenNoAction() {
+        statsActor.onStatsPersistMsg(new StatsPersistMsg(0, 1, TenantId.SYS_TENANT_ID, TenantId.SYS_TENANT_ID));
+        verify(eventService, times(1)).save(any(Event.class));
+        statsActor.onStatsPersistMsg(new StatsPersistMsg(1, 0, TenantId.SYS_TENANT_ID, TenantId.SYS_TENANT_ID));
+        verify(eventService, times(2)).save(any(Event.class));
+        statsActor.onStatsPersistMsg(new StatsPersistMsg(1, 1, TenantId.SYS_TENANT_ID, TenantId.SYS_TENANT_ID));
+        verify(eventService, times(3)).save(any(Event.class));
+    }
+
+}
