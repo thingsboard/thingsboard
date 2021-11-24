1c81f227b511aa5d5325b2ff537748c23d49d2bf
Andrii Shvaika <ashvayka@thingsboard.io>
2021-11-24 11:19:58 +0200

    Correct test class name according to mask in pom.xml

--- application/src/test/java/org/thingsboard/server/transport/mqtt/credentials/sql/BasicMqttCredentialsTest.java
+++ application/src/test/java/org/thingsboard/server/transport/mqtt/credentials/sql/BasicMqttCredentialsTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.thingsboard.server.transport.mqtt.credentials;
+package org.thingsboard.server.transport.mqtt.credentials.sql;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import org.apache.commons.lang3.RandomStringUtils;
