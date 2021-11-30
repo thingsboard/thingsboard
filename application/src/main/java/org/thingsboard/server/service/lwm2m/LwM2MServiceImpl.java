2f1b61b90fa9282ad39b94075e0598a97a762d60
nickAS21 <nick@thingsboard.io>
2021-11-30 11:46:06 +0200

    lwm2m - fix bug Lwm2mController if bootstrap.enabled = false or lwm2m.enabled = false in *.yml

--- application/src/main/java/org/thingsboard/server/service/lwm2m/LwM2MServiceImpl.java
+++ application/src/main/java/org/thingsboard/server/service/lwm2m/LwM2MServiceImpl.java
@@ -18,10 +18,10 @@ package org.thingsboard.server.service.lwm2m;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.codec.binary.Base64;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.stereotype.Service;
 import org.thingsboard.server.common.data.device.profile.lwm2m.bootstrap.LwM2MServerSecurityConfigDefault;
 import org.thingsboard.server.common.transport.config.ssl.SslCredentials;
-import org.thingsboard.server.queue.util.TbLwM2mTransportComponent;
 import org.thingsboard.server.transport.lwm2m.config.LwM2MSecureServerConfig;
 import org.thingsboard.server.transport.lwm2m.config.LwM2MTransportBootstrapConfig;
 import org.thingsboard.server.transport.lwm2m.config.LwM2MTransportServerConfig;
@@ -31,7 +31,7 @@ import java.util.Optional;
 @Slf4j
 @Service
 @RequiredArgsConstructor
-@TbLwM2mTransportComponent
+@ConditionalOnExpression("('${service.type:null}'=='monolith' || '${service.type:null}'=='tb-core') && '${transport.lwm2m.enabled:false}'=='true'")
 public class LwM2MServiceImpl implements LwM2MService {
 
     private final LwM2MTransportServerConfig serverConfig;
