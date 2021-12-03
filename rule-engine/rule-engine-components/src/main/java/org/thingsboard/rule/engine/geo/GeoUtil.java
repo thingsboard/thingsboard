a6753f0ca758b6136614e1620bf226b1952f5909
desoliture <ikozka@thingsboard.io>
2021-12-03 16:37:57 +0200

    fix coordinate containing checking for self-intersecting polygons

--- rule-engine/rule-engine-components/src/main/java/org/thingsboard/rule/engine/geo/GeoUtil.java
+++ rule-engine/rule-engine-components/src/main/java/org/thingsboard/rule/engine/geo/GeoUtil.java
@@ -18,19 +18,27 @@ package org.thingsboard.rule.engine.geo;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
+import org.locationtech.jts.geom.Coordinate;
+import org.locationtech.jts.geom.CoordinateSequence;
+import org.locationtech.jts.geom.Polygon;
 import org.locationtech.spatial4j.context.SpatialContext;
 import org.locationtech.spatial4j.context.jts.JtsSpatialContext;
 import org.locationtech.spatial4j.context.jts.JtsSpatialContextFactory;
 import org.locationtech.spatial4j.distance.DistanceUtils;
 import org.locationtech.spatial4j.shape.Point;
-import org.locationtech.spatial4j.shape.Shape;
-import org.locationtech.spatial4j.shape.ShapeFactory;
 import org.locationtech.spatial4j.shape.SpatialRelation;
+import org.locationtech.spatial4j.shape.impl.PointImpl;
+import org.locationtech.spatial4j.shape.jts.JtsGeometry;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class GeoUtil {
 
     private static final SpatialContext distCtx = SpatialContext.GEO;
     private static final JtsSpatialContext jtsCtx;
+
+    private static final JsonParser JSON_PARSER = new JsonParser();
 
     static {
         JtsSpatialContextFactory factory = new JtsSpatialContextFactory();
@@ -44,8 +52,8 @@ public class GeoUtil {
         return unit.fromKm(distCtx.getDistCalc().distance(xLL, yLL) * DistanceUtils.DEG_TO_KM);
     }
 
-    public static synchronized boolean contains(String polygon, Coordinates coordinates) {
-        JsonArray polygonArray = new JsonParser().parse(polygon).getAsJsonArray();
+    public static synchronized boolean contains(String polygonInString, Coordinates coordinates) {
+        JsonArray polygonArray = JSON_PARSER.parse(polygonInString).getAsJsonArray();
 
         JsonArray arrayWithCoords = polygonArray;
         JsonArray innerArray = polygonArray.get(0).getAsJsonArray();
@@ -53,28 +61,38 @@ public class GeoUtil {
             arrayWithCoords = innerArray;
         }
 
-        Shape shape = buildPolygonFromJsonCoords(arrayWithCoords);
-        Point point = jtsCtx.getShapeFactory().pointXY(coordinates.getLongitude(), coordinates.getLatitude());
-        return shape.relate(point).equals(SpatialRelation.CONTAINS);
+        List<Coordinate> coordinateList = parseCoordinates(arrayWithCoords);
+        Polygon polygon = buildPolygonFromCoordsList(coordinateList);
+
+        JtsGeometry geometry = jtsCtx.getShapeFactory().makeShape(polygon);
+        Point point = new PointImpl(coordinates.getLatitude(), coordinates.getLongitude(), jtsCtx);
+
+        return geometry.relate(point).equals(SpatialRelation.CONTAINS);
     }
 
-    private static Shape buildPolygonFromJsonCoords(JsonArray coordinates) {
-        ShapeFactory.PolygonBuilder polygonBuilder = jtsCtx.getShapeFactory().polygon();
-        boolean isFirst = true;
-        double firstLat = 0.0;
-        double firstLng = 0.0;
-        for (JsonElement element : coordinates) {
-            double lat = element.getAsJsonArray().get(0).getAsDouble();
-            double lng = element.getAsJsonArray().get(1).getAsDouble();
-            if (isFirst) {
-                firstLat = lat;
-                firstLng = lng;
-                isFirst = false;
-            }
-            polygonBuilder.pointXY(jtsCtx.getShapeFactory().normX(lng), jtsCtx.getShapeFactory().normX(lat));
+    private static List<Coordinate> parseCoordinates(JsonArray coordinates) {
+        List<Coordinate> allCoords = new ArrayList<>();
+
+        for (JsonElement coords : coordinates) {
+            double x = coords.getAsJsonArray().get(0).getAsDouble();
+            double y = coords.getAsJsonArray().get(1).getAsDouble();
+
+            allCoords.add(new Coordinate(x, y));
         }
-        polygonBuilder.pointXY(jtsCtx.getShapeFactory().normX(firstLng), jtsCtx.getShapeFactory().normX(firstLat));
-        return polygonBuilder.buildOrRect();
+
+        allCoords.add(allCoords.get(0));
+
+        return allCoords;
+    }
+
+    private static Polygon buildPolygonFromCoordsList(List<Coordinate> coordinates) {
+        CoordinateSequence coordinateSequence = jtsCtx
+                .getShapeFactory()
+                .getGeometryFactory()
+                .getCoordinateSequenceFactory()
+                .create(coordinates.toArray(new Coordinate[0]));
+
+        return jtsCtx.getShapeFactory().getGeometryFactory().createPolygon(coordinateSequence);
     }
 
     private static boolean containsPrimitives(JsonArray array) {
