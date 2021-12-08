8c7f90b74ceab1281547e20d2cc8f86f13d32a4a
Igor Kulikov <ikulikov@thingsboard.io>
2021-12-08 19:39:36 +0200

    Fix upgrade

--- application/src/main/data/upgrade/3.3.2/schema_update.sql
+++ application/src/main/data/upgrade/3.3.2/schema_update.sql
@@ -16,12 +16,13 @@
 
 CREATE TABLE IF NOT EXISTS entity_alarm (
     tenant_id uuid NOT NULL,
+    entity_type varchar(32),
     entity_id uuid NOT NULL,
     created_time bigint NOT NULL,
-    type varchar(255) NOT NULL,
+    alarm_type varchar(255) NOT NULL,
     customer_id uuid,
     alarm_id uuid,
-    CONSTRAINT entity_alarm_pkey PRIMARY KEY (entity_id, alarm_id),
+    CONSTRAINT entity_alarm_pkey PRIMARY KEY(entity_id, alarm_id),
     CONSTRAINT fk_entity_alarm_id FOREIGN KEY (alarm_id) REFERENCES alarm(id) ON DELETE CASCADE
 );
 
