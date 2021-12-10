409fddc2e89a26eeebc75a9bf9aad718e9e0db35
desoliture <ikozka@thingsboard.io>
2021-12-10 18:05:20 +0200

    add password policy for white-spaces

--- common/data/src/main/java/org/thingsboard/server/common/data/security/model/UserPasswordPolicy.java
+++ common/data/src/main/java/org/thingsboard/server/common/data/security/model/UserPasswordPolicy.java
@@ -35,6 +35,8 @@ public class UserPasswordPolicy implements Serializable {
     private Integer minimumDigits;
     @ApiModelProperty(position = 1, value = "Minimum number of special in the password." )
     private Integer minimumSpecialCharacters;
+    @ApiModelProperty(position = 1, value = "Allow white-spaces")
+    private Boolean allowWhitespaces;
 
     @ApiModelProperty(position = 1, value = "Password expiration period (days). Force expiration of the password." )
     private Integer passwordExpirationPeriodDays;
