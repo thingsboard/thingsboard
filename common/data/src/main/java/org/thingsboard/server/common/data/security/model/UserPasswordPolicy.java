bbecb26023cb41aa91918cb979fe018da9626460
desoliture <ikozka@thingsboard.io>
2021-12-14 11:35:09 +0200

    add default value to password rule "allow whitespaces" - true

--- common/data/src/main/java/org/thingsboard/server/common/data/security/model/UserPasswordPolicy.java
+++ common/data/src/main/java/org/thingsboard/server/common/data/security/model/UserPasswordPolicy.java
@@ -36,7 +36,7 @@ public class UserPasswordPolicy implements Serializable {
     @ApiModelProperty(position = 1, value = "Minimum number of special in the password." )
     private Integer minimumSpecialCharacters;
     @ApiModelProperty(position = 1, value = "Allow white-spaces")
-    private Boolean allowWhitespaces;
+    private Boolean allowWhitespaces = true;
 
     @ApiModelProperty(position = 1, value = "Password expiration period (days). Force expiration of the password." )
     private Integer passwordExpirationPeriodDays;
