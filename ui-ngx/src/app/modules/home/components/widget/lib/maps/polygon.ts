38bc05100e8c26fe4318ab2f55ffe25637ebf368
Vladyslav_Prykhodko <vprykhodko@thingsboard.io>
2021-12-20 18:35:18 +0200

    UI: Fixed incorrect work edit rectangle with cut

--- ui-ngx/src/app/modules/home/components/widget/lib/maps/polygon.ts
+++ ui-ngx/src/app/modules/home/components/widget/lib/maps/polygon.ts
@@ -15,7 +15,7 @@
 ///
 
 import L, { LatLngExpression, LeafletMouseEvent } from 'leaflet';
-import { createTooltip } from './maps-utils';
+import { createTooltip, isCutPolygon } from './maps-utils';
 import {
   fillPattern,
   functionValueCalculator,
@@ -39,7 +39,7 @@ export class Polygon {
         const polygonColor = this.getPolygonColor(settings);
         const polygonStrokeColor = this.getPolygonStrokeColor(settings);
         const polyData = data[this.settings.polygonKeyName];
-        const polyConstructor = polyData.length > 2 ? L.polygon : L.rectangle;
+        const polyConstructor = isCutPolygon(polyData) || polyData.length > 2 ? L.polygon : L.rectangle;
         this.leafletPoly = polyConstructor(polyData, {
           fill: true,
           fillColor: polygonColor,
@@ -102,7 +102,7 @@ export class Polygon {
       this.data = data;
       this.dataSources = dataSources;
       const polyData = data[this.settings.polygonKeyName];
-      if (polyData.length > 2) {
+      if (isCutPolygon(polyData) || polyData.length > 2) {
         if (this.leafletPoly instanceof L.Rectangle) {
           this.map.removeLayer(this.leafletPoly);
           const polygonColor = this.getPolygonColor(settings);
