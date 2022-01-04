4dccfecb2bcda63e6cc23f48d320e99c4ad463da
desoliture <ikozka@thingsboard.io>
2022-01-04 17:46:46 +0200

    add tests for `EntityRelationController`

---
+++ application/src/test/java/org/thingsboard/server/controller/sql/EntityRelationControllerSqlTest.java
@@ -0,0 +1,8 @@
+package org.thingsboard.server.controller.sql;
+
+import org.thingsboard.server.controller.BaseEntityRelationControllerTest;
+import org.thingsboard.server.dao.service.DaoSqlTest;
+
+@DaoSqlTest
+public class EntityRelationControllerSqlTest extends BaseEntityRelationControllerTest {
+}
