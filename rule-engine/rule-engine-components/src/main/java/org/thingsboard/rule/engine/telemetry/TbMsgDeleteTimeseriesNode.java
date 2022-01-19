15673e3410d96dc52d3b7465196d583c94002ffa
ShvaykaD <dshvaika@thingsboard.io>
2022-01-19 15:06:47 +0200

    update interval patterns validation

--- rule-engine/rule-engine-components/src/main/java/org/thingsboard/rule/engine/telemetry/TbMsgDeleteTimeseriesNode.java
+++ rule-engine/rule-engine-components/src/main/java/org/thingsboard/rule/engine/telemetry/TbMsgDeleteTimeseriesNode.java
@@ -15,13 +15,8 @@
  */
 package org.thingsboard.rule.engine.telemetry;
 
-import com.google.common.util.concurrent.FutureCallback;
-import com.google.common.util.concurrent.ListenableFuture;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.math.NumberUtils;
-import org.checkerframework.checker.nullness.qual.Nullable;
 import org.springframework.util.CollectionUtils;
-import org.thingsboard.common.util.DonAsynchron;
 import org.thingsboard.rule.engine.api.RuleNode;
 import org.thingsboard.rule.engine.api.TbContext;
 import org.thingsboard.rule.engine.api.TbNode;
@@ -30,7 +25,6 @@ import org.thingsboard.rule.engine.api.TbNodeException;
 import org.thingsboard.rule.engine.api.util.TbNodeUtils;
 import org.thingsboard.server.common.data.kv.BaseDeleteTsKvQuery;
 import org.thingsboard.server.common.data.kv.DeleteTsKvQuery;
-import org.thingsboard.server.common.data.kv.TsKvLatestRemovingResult;
 import org.thingsboard.server.common.data.plugin.ComponentType;
 import org.thingsboard.server.common.msg.TbMsg;
 
@@ -108,33 +102,18 @@ public class TbMsgDeleteTimeseriesNode implements TbNode {
     private void setDeleteTsIntervalsFromPatterns(TbMsg msg) {
         String startTsIntervalPattern = config.getStartTsIntervalPattern();
         String endTsIntervalPattern = config.getEndTsIntervalPattern();
-        String startIntervalPatternValue = getPatternValue(msg, startTsIntervalPattern);
-        String endIntervalPatternValue = getPatternValue(msg, endTsIntervalPattern);
-        if (startIntervalPatternValue == null) {
-            throw new IllegalArgumentException("Message value: '" +
-                    replaceRegex(startTsIntervalPattern) + "' is undefined!");
-        }
-        if (endIntervalPatternValue == null) {
-            throw new IllegalArgumentException("Message value: '" +
-                    replaceRegex(endTsIntervalPattern) + "' is undefined!");
-        }
-        if (!NumberUtils.isParsable(startIntervalPatternValue)) {
-            throw new IllegalArgumentException("Message value: '" +
-                    replaceRegex(startTsIntervalPattern) + "' is invalid!");
-        }
-        if (!NumberUtils.isParsable(endIntervalPatternValue)) {
-            throw new IllegalArgumentException("Message value: '" +
-                    replaceRegex(endTsIntervalPattern) + "' is invalid!");
-        }
-        deleteFromTs = Long.parseLong(startIntervalPatternValue);
-        deleteToTs = Long.parseLong(endIntervalPatternValue);
+        String startIntervalPatternValue = TbNodeUtils.processPattern(startTsIntervalPattern, msg);
+        String endIntervalPatternValue = TbNodeUtils.processPattern(endTsIntervalPattern, msg);
+        deleteFromTs = validateIntervalPatternValue(startTsIntervalPattern, startIntervalPatternValue);
+        deleteToTs = validateIntervalPatternValue(endTsIntervalPattern, endIntervalPatternValue);
     }
 
-    private String getPatternValue(TbMsg msg, String pattern) {
-        return TbNodeUtils.processPattern(pattern, msg);
+    private long validateIntervalPatternValue(String pattern, String substitution) {
+        try {
+            return Long.parseLong(substitution);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("Failed to parse message value for '" + pattern + "' pattern due to: ", e);
+        }
     }
 
-    private String replaceRegex(String pattern) {
-        return pattern.replaceAll("\\$\\{?\\[?", "").replaceAll("}?]?", "");
-    }
 }
