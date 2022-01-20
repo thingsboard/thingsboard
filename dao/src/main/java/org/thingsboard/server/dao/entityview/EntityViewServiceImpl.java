c28e13c03c289ca8b54a9c9a357ee28c8a19a74f
desoliture <ikozka@thingsboard.io>
2022-01-20 17:09:20 +0200

    remove todo

--- dao/src/main/java/org/thingsboard/server/dao/entityview/EntityViewServiceImpl.java
+++ dao/src/main/java/org/thingsboard/server/dao/entityview/EntityViewServiceImpl.java
@@ -447,7 +447,6 @@ public class EntityViewServiceImpl extends AbstractEntityService implements Enti
                         throw new DataValidationException("Entity view should be assigned to tenant!");
                     } else {
                         Tenant tenant = tenantService.findTenantById(entityView.getTenantId());
-                        // TODO: 13.01.22 Instead of finding and checking for null need to create and use tenantService.exists()
                         if (tenant == null) {
                             throw new DataValidationException("Entity view is referencing to non-existent tenant!");
                         }
