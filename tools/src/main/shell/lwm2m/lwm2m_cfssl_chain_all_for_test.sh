1f2eaf07edb14d63ec3597c09f00e2f587559161
nickAS21 <nick@thingsboard.io>
2022-01-30 22:21:00 +0200

    lwm2m: del shell from tests

--- tools/src/main/shell/lwm2m/lwm2m_cfssl_chain_all_for_test.sh
+++ tools/src/main/shell/lwm2m/lwm2m_cfssl_chain_all_for_test.sh
@@ -29,26 +29,41 @@ cd -- "$(
 	dirname "${0}"
 )" || exit 1
 
-Help()
+ResultInfo()
 {
-   # Display Help
-   echo "Description of the script functions."
-   echo
-   echo "Syntax: scriptTemplate [-g|h|v|V]"
-   echo "options:"
-   echo "h     Print this Help."
-   echo "v     Verbose mode."
-   echo "V     Print software version and exit."
-   echo
+#   # Display Help
+#   echo "Description of the script functions."
+#   echo
+#   echo "Syntax: scriptTemplate [-g|h|v|V]"
+#   echo "options:"
+#   echo "h     Print this Help."
+#   echo "v     Verbose mode."
+#   echo "V     Print software version and exit."
+#   echo
+if  [ "$IS_IHFO" = false ] ; then
+  if [ "$IS_SERVER_CREATED_KEY" = true ] ; then
+    ./lwm2m_cfssl_chain_server_for_test.sh > /dev/null 2>&1 &
+  fi
+  if [ "$IS_TRUST_CLIENT_CREATED_KEY" = true ] ; then
+    ./lwM2M_cfssl_chain_clients_for_test.sh ${INTERMEDIATE_START} ${INTERMEDIATE_FINISH} ${CLIENT_START} ${CLIENT_FINISH} > /dev/null 2>&1 &
+  fi
+else
+    if [ "$IS_SERVER_CREATED_KEY" = true ] ; then
+    ./lwm2m_cfssl_chain_server_for_test.sh
+  fi
+  if [ "$IS_TRUST_CLIENT_CREATED_KEY" = true ] ; then
+    ./lwM2M_cfssl_chain_clients_for_test.sh ${INTERMEDIATE_START} ${INTERMEDIATE_FINISH} ${CLIENT_START} ${CLIENT_FINISH}
+  fi
+fi
 }
 
-if [ "$1" == "-h" ] ; then
-    echo -e "Usage 1: ./`basename $0` \"Information is not displayed\" : \"Keys for the server are generated\" : \"Keys for the clients and trusts are generated\""
-    echo -e "Usage 2: ./`basename $0` true \"Information is displayed\" : \"Keys for the server are generated\" : \"Keys for the clients and trusts are generated\""
-    echo -e "Usage 3: ./`basename $0` true false \"Information is displayed\" : \"Keys for the server are not generated\" : \"Keys for the clients and trusts are generated\""
-    echo -e "Usage 4: ./`basename $0` true false false \"Information is displayed\" : \"Keys for the server are not generated\" : \"Keys for the clients and trusts are not generated\""
-    echo -e "Usage 5: ./`basename $0` true true false \"Information is displayed\" : \"Keys for the server are generated\" : \"Keys for the clients and trusts are not generated\""
-    echo "This Help File: ./`basename $0` -h"
+if [ "$1" == "-h" ] ||[ "$1" == "-?" ] || [ "$1" == "-help" ] ; then
+    echo -e "Usage 1:  \"Information is not displayed\" : \"Keys for the server are generated\" : \"Keys for the clients and trusts are generated\"\n./`basename $0`"
+    echo -e "Usage 2:  \"Information is displayed\" : \"Keys for the server are generated\" : \"Keys for the clients and trusts are generated\"\n./`basename $0` true \n./`basename $0` true true true "
+    echo -e "Usage 3:  \"Information is displayed\" : \"Keys for the server are not generated\" : \"Keys for the clients and trusts are generated\"\n./`basename $0` true false \n./`basename $0` true false true"
+    echo -e "Usage 4:  \"Information is displayed\" : \"Keys for the server are not generated\" : \"Keys for the clients and trusts are not generated\"\n./`basename $0` true false false"
+    echo -e "Usage 5:  \"Information is displayed\" : \"Keys for the server are generated\" : \"Keys for the clients and trusts are not generated\"\n./`basename $0` true true false"
+    echo -e "This Help File: \n./`basename $0` -h | -? | -help"
     exit 0
 fi
 
@@ -63,6 +78,14 @@ fi
 if [ -n "$3" ]; then
   IS_TRUST_CLIENT_CREATED_KEY=$3
 fi
+
+if  [ "$IS_SERVER_CREATED_KEY" = false ] && [ "$IS_TRUST_CLIENT_CREATED_KEY" = false ] ; then
+  echo -e "Result is null"
+  echo -e "This Help File: \n./`basename $0` -h | -? | -help"
+  exit 0
+fi
+
+
 
 if  [ "$IS_IHFO" = false ] ; then
   if [ "$IS_SERVER_CREATED_KEY" = true ] ; then
@@ -79,3 +102,14 @@ else
     ./lwM2M_cfssl_chain_clients_for_test.sh ${INTERMEDIATE_START} ${INTERMEDIATE_FINISH} ${CLIENT_START} ${CLIENT_FINISH}
   fi
 fi
+
+echo -e "Result into:"
+if [ "$IS_SERVER_CREATED_KEY" = true ] ; then
+   echo -e "./Server"
+fi
+if [ "$IS_TRUST_CLIENT_CREATED_KEY" = true ] ; then
+  echo -e "./Client"
+  echo -e "./Trust"
+fi
+
+echo -e "Finish"
