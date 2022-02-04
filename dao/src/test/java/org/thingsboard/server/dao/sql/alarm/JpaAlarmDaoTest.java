6318c7636c00c05832abfb31877e39cc31bf5da9
van-vanich <panahno13@gmail.com>
2022-02-04 15:55:31 +0200

    fix issue with failure test, because before other tests saved and don't delete Alarm

--- dao/src/test/java/org/thingsboard/server/dao/sql/alarm/JpaAlarmDaoTest.java
+++ dao/src/test/java/org/thingsboard/server/dao/sql/alarm/JpaAlarmDaoTest.java
@@ -29,6 +29,8 @@ import org.thingsboard.server.dao.alarm.AlarmDao;
 
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
@@ -44,7 +46,7 @@ public class JpaAlarmDaoTest extends AbstractJpaDaoTest {
 
 
     @Test
-    public void testFindLatestByOriginatorAndType() throws ExecutionException, InterruptedException {
+    public void testFindLatestByOriginatorAndType() throws ExecutionException, InterruptedException, TimeoutException {
         log.info("Current system time in millis = {}", System.currentTimeMillis());
         UUID tenantId = UUID.fromString("d4b68f40-3e96-11e7-a884-898080180d6b");
         UUID originator1Id = UUID.fromString("d4b68f41-3e96-11e7-a884-898080180d6b");
@@ -52,13 +54,15 @@ public class JpaAlarmDaoTest extends AbstractJpaDaoTest {
         UUID alarm1Id = UUID.fromString("d4b68f43-3e96-11e7-a884-898080180d6b");
         UUID alarm2Id = UUID.fromString("d4b68f44-3e96-11e7-a884-898080180d6b");
         UUID alarm3Id = UUID.fromString("d4b68f45-3e96-11e7-a884-898080180d6b");
+        int alarmCountBeforeSave = alarmDao.find(TenantId.fromUUID(tenantId)).size();
         saveAlarm(alarm1Id, tenantId, originator1Id, "TEST_ALARM");
         saveAlarm(alarm2Id, tenantId, originator1Id, "TEST_ALARM");
         saveAlarm(alarm3Id, tenantId, originator2Id, "TEST_ALARM");
-        assertEquals(3, alarmDao.find(TenantId.fromUUID(tenantId)).size());
+        int alarmCountAfterSave = alarmDao.find(TenantId.fromUUID(tenantId)).size();
+        assertEquals(3, alarmCountAfterSave - alarmCountBeforeSave);
         ListenableFuture<Alarm> future = alarmDao
                 .findLatestByOriginatorAndType(TenantId.fromUUID(tenantId), new DeviceId(originator1Id), "TEST_ALARM");
-        Alarm alarm = future.get();
+        Alarm alarm = future.get(30, TimeUnit.SECONDS);
         assertNotNull(alarm);
         assertEquals(alarm2Id, alarm.getId().getId());
     }
