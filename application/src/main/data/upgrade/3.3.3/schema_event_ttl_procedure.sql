64d3f5c8039d0e82f645aa41365356fea970806a
Andrii Shvaika <ashvayka@thingsboard.io>
2022-02-18 12:12:39 +0200

    Remove redundant event types from cleanup procedure

--- application/src/main/data/upgrade/3.3.3/schema_event_ttl_procedure.sql
+++ application/src/main/data/upgrade/3.3.3/schema_event_ttl_procedure.sql
@@ -32,16 +32,16 @@ BEGIN
     IF regular_events_start_ts > 0 AND regular_events_end_ts > 0 THEN
         EXECUTE format(
                 'WITH deleted AS (DELETE FROM event WHERE id in (SELECT id from event WHERE ts > %L::bigint AND ts < %L::bigint AND ' ||
-                '(event_type != %L::varchar AND event_type != %L::varchar AND event_type != %L::varchar AND event_type != %L::varchar)) RETURNING *) ' ||
+                '(event_type != %L::varchar AND event_type != %L::varchar)) RETURNING *) ' ||
                 'SELECT count(*) FROM deleted', regular_events_start_ts, regular_events_end_ts,
-                'DEBUG_RULE_NODE', 'DEBUG_RULE_CHAIN', 'DEBUG_CONVERTER', 'DEBUG_INTEGRATION') into ttl_deleted_count;
+                'DEBUG_RULE_NODE', 'DEBUG_RULE_CHAIN') into ttl_deleted_count;
     END IF;
     IF debug_events_start_ts > 0 AND debug_events_end_ts > 0 THEN
         EXECUTE format(
                 'WITH deleted AS (DELETE FROM event WHERE id in (SELECT id from event WHERE ts > %L::bigint AND ts < %L::bigint AND ' ||
-                '(event_type = %L::varchar OR event_type = %L::varchar OR event_type = %L::varchar OR event_type = %L::varchar)) RETURNING *) ' ||
+                '(event_type = %L::varchar OR event_type = %L::varchar)) RETURNING *) ' ||
                 'SELECT count(*) FROM deleted', debug_events_start_ts, debug_events_end_ts,
-                'DEBUG_RULE_NODE', 'DEBUG_RULE_CHAIN', 'DEBUG_CONVERTER', 'DEBUG_INTEGRATION') into debug_ttl_deleted_count;
+                'DEBUG_RULE_NODE', 'DEBUG_RULE_CHAIN') into debug_ttl_deleted_count;
     END IF;
     RAISE NOTICE 'Events removed by ttl: %', ttl_deleted_count;
     RAISE NOTICE 'Debug Events removed by ttl: %', debug_ttl_deleted_count;
