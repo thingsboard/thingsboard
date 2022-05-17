8852fc8f8e2524ade77fc99cbd21b0489309dd96
YevhenBondarenko <ybondarenko@thingsboard.io>
2022-05-17 08:11:19 +0200

    enabled autoconfiguration for MeterRegistry

--- transport/mqtt/src/main/java/org/thingsboard/server/mqtt/ThingsboardMqttTransportApplication.java
+++ transport/mqtt/src/main/java/org/thingsboard/server/mqtt/ThingsboardMqttTransportApplication.java
@@ -17,6 +17,10 @@ package org.thingsboard.server.mqtt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.cassandra.CassandraAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.cassandra.CassandraDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -25,7 +29,7 @@ import java.util.Arrays;
 
 @SpringBootConfiguration
 @EnableAsync
-@EnableScheduling
+@EnableAutoConfiguration
 @ComponentScan({"org.thingsboard.server.mqtt", "org.thingsboard.server.common", "org.thingsboard.server.transport.mqtt", "org.thingsboard.server.queue", "org.thingsboard.server.cache"})
 public class ThingsboardMqttTransportApplication {
 
