0bb9d531cc7eb699435b0a821ad9ea1c19f17378
Vladyslav_Prykhodko <vprykhodko@thingsboard.io>
2022-05-23 14:40:47 +0300

    UI: Fixed permission on page security

--- ui-ngx/src/app/core/auth/auth.service.ts
+++ ui-ngx/src/app/core/auth/auth.service.ts
@@ -242,7 +242,7 @@ export class AuthService {
     if (authState && authState.authUser) {
       if (authState.authUser.authority === Authority.TENANT_ADMIN || authState.authUser.authority === Authority.CUSTOMER_USER) {
         if ((this.userHasDefaultDashboard(authState) && authState.forceFullscreen) || authState.authUser.isPublic) {
-          if (path === 'profile') {
+          if (path === 'profile' || path === 'security') {
             if (this.userHasProfile(authState.authUser)) {
               return false;
             } else {
