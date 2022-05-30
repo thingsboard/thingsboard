518d9fd6f5cc93876008aaaa6c056b04749115a6
Andrii Shvaika <ashvayka@thingsboard.io>
2022-05-30 17:34:16 +0300

    SID for Auto Version Create Config

--- common/data/src/main/java/org/thingsboard/server/common/data/sync/vc/request/create/AutoVersionCreateConfig.java
+++ common/data/src/main/java/org/thingsboard/server/common/data/sync/vc/request/create/AutoVersionCreateConfig.java
@@ -16,9 +16,13 @@
 package org.thingsboard.server.common.data.sync.vc.request.create;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
+@EqualsAndHashCode(callSuper = true)
 @Data
 public class AutoVersionCreateConfig extends VersionCreateConfig {
+
+    private static final long serialVersionUID = 8245450889383315551L;
 
     private String branch;
 
