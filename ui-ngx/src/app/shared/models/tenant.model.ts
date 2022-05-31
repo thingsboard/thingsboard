9eb32add2bf5ebb330d14bfd23ce74d9f30df6cd
Viacheslav Klimov <viacheslavklimov11@gmail.com>
2022-05-31 18:11:24 +0300

    UI for export/import api rate limiting

--- ui-ngx/src/app/shared/models/tenant.model.ts
+++ ui-ngx/src/app/shared/models/tenant.model.ts
@@ -41,6 +41,9 @@ export interface DefaultTenantProfileConfiguration {
   transportDeviceTelemetryMsgRateLimit?: string;
   transportDeviceTelemetryDataPointsRateLimit?: string;
 
+  tenantEntityExportRateLimit?: string;
+  tenantEntityImportRateLimit?: string;
+
   maxTransportMessages: number;
   maxTransportDataPoints: number;
   maxREExecutions: number;
