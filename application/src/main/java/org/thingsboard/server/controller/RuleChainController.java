17736ce93fb4d8bf51110c70d8ad83b164f496bc
Igor Kulikov <ikulikov@thingsboard.io>
2022-06-03 15:17:00 +0300

    Remove unused imports

--- application/src/main/java/org/thingsboard/server/controller/RuleChainController.java
+++ application/src/main/java/org/thingsboard/server/controller/RuleChainController.java
@@ -74,8 +74,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.TreeSet;
-import java.util.UUID;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.TimeUnit;
 
