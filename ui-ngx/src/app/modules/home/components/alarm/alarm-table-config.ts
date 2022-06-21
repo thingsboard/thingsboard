a2422dab00595b1beb7dc7d6a7f5fc151ec0b23f
Vladyslav_Prykhodko <vprykhodko@thingsboard.io>
2022-06-21 16:05:10 +0300

    UI: Fixed permission check in show alarm details

--- ui-ngx/src/app/modules/home/components/alarm/alarm-table-config.ts
+++ ui-ngx/src/app/modules/home/components/alarm/alarm-table-config.ts
@@ -44,8 +44,14 @@ import {
   AlarmDetailsDialogData
 } from '@home/components/alarm/alarm-details-dialog.component';
 import { DAY, historyInterval } from '@shared/models/time/time.models';
+import { Store } from '@ngrx/store';
+import { AppState } from '@core/core.state';
+import { getCurrentAuthUser } from '@core/auth/auth.selectors';
+import { Authority } from '@shared/models/authority.enum';
 
 export class AlarmTableConfig extends EntityTableConfig<AlarmInfo, TimePageLink> {
+
+  private authUser = getCurrentAuthUser(this.store);
 
   searchStatus: AlarmSearchStatus;
 
@@ -55,7 +61,8 @@ export class AlarmTableConfig extends EntityTableConfig<AlarmInfo, TimePageLink>
               private datePipe: DatePipe,
               private dialog: MatDialog,
               public entityId: EntityId = null,
-              private defaultSearchStatus: AlarmSearchStatus = AlarmSearchStatus.ANY) {
+              private defaultSearchStatus: AlarmSearchStatus = AlarmSearchStatus.ANY,
+              private store: Store<AppState>) {
     super();
     this.loadDataOnInit = false;
     this.tableTitle = '';
@@ -102,7 +109,7 @@ export class AlarmTableConfig extends EntityTableConfig<AlarmInfo, TimePageLink>
       {
         name: this.translate.instant('alarm.details'),
         icon: 'more_horiz',
-        isEnabled: () => true,
+        isEnabled: (entity) => this.authUser.authority !== Authority.CUSTOMER_USER || entity.customerId.id === this.authUser.customerId,
         onAction: ($event, entity) => this.showAlarmDetails(entity)
       }
     );
