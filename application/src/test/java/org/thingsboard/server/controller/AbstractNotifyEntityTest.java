f7fcc9a7b0e92444ddc5eba8d6f5b0a30945ec40
nickAS21 <nick@thingsboard.io>
2022-06-22 06:58:55 +0300

    refactoring: tests error add change to matcher

--- application/src/test/java/org/thingsboard/server/controller/AbstractNotifyEntityTest.java
+++ application/src/test/java/org/thingsboard/server/controller/AbstractNotifyEntityTest.java
@@ -109,7 +109,8 @@ public abstract class AbstractNotifyEntityTest extends AbstractWebTest {
         EntityId entity_originator_NULL_UUID = createEntityId_NULL_UUID(entity);
         testNotificationMsgToEdgeServiceNever(entity_originator_NULL_UUID);
         ArgumentMatcher<HasName> matcherEntityEquals = argument -> argument.getClass().equals(entity.getClass());
-        ArgumentMatcher<Exception> matcherError = argument -> argument.getMessage().contains(exp.getMessage());
+        ArgumentMatcher<Exception> matcherError = argument -> argument.getMessage().contains(exp.getMessage())
+                & argument.getClass().equals(exp.getClass());
         logEntityActionErrorAdditionalInfo(matcherEntityEquals, entity_originator_NULL_UUID, tenantId, customer_NULL_UUID, userId,
                 userName, actionType, 1, matcherError, additionalInfo);
         testPushMsgToRuleEngineNever(entity_originator_NULL_UUID);
@@ -122,8 +123,10 @@ public abstract class AbstractNotifyEntityTest extends AbstractWebTest {
         EntityId entity_originator_NULL_UUID = createEntityId_NULL_UUID(entity);
         testNotificationMsgToEdgeServiceNever(entity_originator_NULL_UUID);
         ArgumentMatcher<HasName> matcherEntityIsNull = Objects::isNull;
-        ArgumentMatcher<Exception> matcherError = argument -> argument.getMessage().contains(exp.getMessage()) & argument.getClass().equals(exp.getClass());
-        logEntityActionErrorAdditionalInfo(matcherEntityIsNull, entity_originator_NULL_UUID, tenantId, customer_NULL_UUID, userId, userName, actionType, 1, matcherError, additionalInfo);
+        ArgumentMatcher<Exception> matcherError = argument -> argument.getMessage().contains(exp.getMessage()) &
+                argument.getClass().equals(exp.getClass());
+        logEntityActionErrorAdditionalInfo(matcherEntityIsNull, entity_originator_NULL_UUID, tenantId, customer_NULL_UUID,
+                userId, userName, actionType, 1, matcherError, additionalInfo);
         testPushMsgToRuleEngineNever(entity_originator_NULL_UUID);
     }
 
@@ -174,11 +177,12 @@ public abstract class AbstractNotifyEntityTest extends AbstractWebTest {
                                  ActionType actionType, int cntTime, Object... additionalInfo) {
         ArgumentMatcher<HasName> matcherEntityEquals = argument -> argument.equals(entity);
         ArgumentMatcher<EntityId> matcherOriginatorId = argument -> argument.equals(originatorId);
-        logEntityActionAdditionalInfo(matcherEntityEquals, matcherOriginatorId, tenantId, customerId, userId, userName, actionType, cntTime, additionalInfo);
+        logEntityActionAdditionalInfo(matcherEntityEquals, matcherOriginatorId, tenantId, customerId, userId, userName,
+                actionType, cntTime, additionalInfo);
     }
 
-    private void logEntityActionAdditionalInfo(ArgumentMatcher<HasName> matcherEntity, ArgumentMatcher<EntityId> matcherOriginatorId, TenantId tenantId,
-                                               CustomerId customerId, UserId userId, String userName,
+    private void logEntityActionAdditionalInfo(ArgumentMatcher<HasName> matcherEntity, ArgumentMatcher<EntityId> matcherOriginatorId,
+                                               TenantId tenantId, CustomerId customerId, UserId userId, String userName,
                                                ActionType actionType, int cntTime, Object... additionalInfo) {
         switch (additionalInfo.length) {
             case 1:
@@ -234,8 +238,8 @@ public abstract class AbstractNotifyEntityTest extends AbstractWebTest {
     }
 
     private void logEntityActionErrorAdditionalInfo(ArgumentMatcher<HasName> matcherEntity, EntityId originatorId, TenantId tenantId,
-                                                    CustomerId customerId, UserId userId, String userName,
-                                                    ActionType actionType, int cntTime, ArgumentMatcher<Exception> matcherError, Object... additionalInfo) {
+                                                    CustomerId customerId, UserId userId, String userName, ActionType actionType,
+                                                    int cntTime, ArgumentMatcher<Exception> matcherError, Object... additionalInfo) {
         switch (additionalInfo.length) {
             case 1:
                 Mockito.verify(auditLogService, times(cntTime))
@@ -261,6 +265,19 @@ public abstract class AbstractNotifyEntityTest extends AbstractWebTest {
                                 Mockito.argThat(matcherError),
                                 Mockito.eq(extractParameter(String.class, 0, additionalInfo)),
                                 Mockito.eq(extractParameter(String.class, 1, additionalInfo)));
+           case 3:
+                Mockito.verify(auditLogService, times(cntTime))
+                        .logEntityAction(Mockito.eq(tenantId),
+                                Mockito.eq(customerId),
+                                Mockito.eq(userId),
+                                Mockito.eq(userName),
+                                Mockito.eq(originatorId),
+                                Mockito.argThat(matcherEntity),
+                                Mockito.eq(actionType),
+                                Mockito.argThat(matcherError),
+                                Mockito.eq(extractParameter(String.class, 0, additionalInfo)),
+                                Mockito.eq(extractParameter(String.class, 1, additionalInfo)),
+                                Mockito.eq(extractParameter(String.class, 3, additionalInfo)));
                 break;
             default:
                 Mockito.verify(auditLogService, times(cntTime))
