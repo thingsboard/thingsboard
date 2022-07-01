7f1ab395b56e8e4233eb8f4f9711b4100231d797
Igor Kulikov <ikulikov@thingsboard.io>
2022-07-01 19:03:29 +0300

    Refactor WEB UI microservice to typescript.

--- msa/web-ui/server.ts
+++ msa/web-ui/server.ts
@@ -1,3 +1,19 @@
+///
+/// Copyright © 2016-2022 The Thingsboard Authors
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///     http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+
 /*
  * Copyright © 2016-2022 The Thingsboard Authors
  *
@@ -13,28 +29,31 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-const config = require('config'),
-      logger = require('./config/logger')('main'),
-      express = require('express'),
-      compression = require('compression'),
-      http = require('http'),
-      httpProxy = require('http-proxy'),
-      path = require('path'),
-      historyApiFallback = require("connect-history-api-fallback");
 
-var server;
+import express from 'express';
+import config from 'config';
+import { _logger}  from './config/logger';
+import path from 'path';
+import http, { ServerResponse } from 'http';
+import httpProxy from 'http-proxy';
+import compression from 'compression';
+import historyApiFallback from 'connect-history-api-fallback';
+
+const logger = _logger('main');
+
+let server: http.Server | null;
 
 (async() => {
     try {
         logger.info('Starting ThingsBoard Web UI Microservice...');
 
-        const bindAddress = config.get('server.address');
-        const bindPort = config.get('server.port');
+        const bindAddress: string = config.get('server.address');
+        const bindPort = Number(config.get('server.port'));
 
-        const thingsboardEnableProxy = config.get('thingsboard.enableProxy');
+        const thingsboardEnableProxy: string = config.get('thingsboard.enableProxy');
 
-        const thingsboardHost = config.get('thingsboard.host');
-        const thingsboardPort = config.get('thingsboard.port');
+        const thingsboardHost: string = config.get('thingsboard.host');
+        const thingsboardPort = Number(config.get('thingsboard.port'));
 
         logger.info('Bind address: %s', bindAddress);
         logger.info('Bind port: %s', bindPort);
@@ -44,9 +63,9 @@ var server;
 
         const useApiProxy = thingsboardEnableProxy === "true";
 
-        var webDir = path.join(__dirname, 'web');
+        let webDir = path.join(__dirname, 'web');
 
-        if (typeof process.env.WEB_FOLDER === 'string') {
+        if (typeof process.env.WEB_FOLDER !== 'undefined') {
             webDir = path.resolve(process.env.WEB_FOLDER);
         }
         logger.info('Web folder: %s', webDir);
@@ -54,7 +73,7 @@ var server;
         const app = express();
         server = http.createServer(app);
 
-        var apiProxy;
+        let apiProxy: httpProxy;
         if (useApiProxy) {
             apiProxy = httpProxy.createProxyServer({
                 target: {
@@ -65,13 +84,14 @@ var server;
 
             apiProxy.on('error', function (err, req, res) {
                 logger.warn('API proxy error: %s', err.message);
-                if (res.writeHead) {
-                  res.writeHead(500);
-                  if (err.code && err.code === 'ECONNREFUSED') {
-                    res.end('Unable to connect to ThingsBoard server.');
-                  } else {
-                    res.end('Thingsboard server connection error: ' + err.code ? err.code : '');
-                  }
+                if (res instanceof ServerResponse) {
+                    res.writeHead(500);
+                    const error = err as any;
+                    if (error.code && error.code === 'ECONNREFUSED') {
+                        res.end('Unable to connect to ThingsBoard server.');
+                    } else {
+                        res.end('Thingsboard server connection error: ' + error.code ? error.code : '');
+                    }
                 }
             });
             app.all('/api/*', (req, res) => {
@@ -95,18 +115,16 @@ var server;
 
         app.use(express.static(root));
 
-        server.listen(bindPort, bindAddress, (error) => {
-            if (error) {
-                logger.error('Failed to start ThingsBoard Web UI Microservice: %s', e.message);
-                logger.error(error.stack);
-                exit(-1);
-            } else {
-                logger.info('==> 🌎  Listening on port %s.', bindPort);
-                logger.info('Started ThingsBoard Web UI Microservice.');
-            }
+        server.listen(bindPort, bindAddress, () => {
+            logger.info('==> 🌎  Listening on port %s.', bindPort);
+            logger.info('Started ThingsBoard Web UI Microservice.');
+        }).on('error', (error) => {
+            logger.error('Failed to start ThingsBoard Web UI Microservice: %s', error.message);
+            logger.error(error.stack);
+            exit(-1);
         });
 
-    } catch (e) {
+    } catch (e: any) {
         logger.error('Failed to start ThingsBoard Web UI Microservice: %s', e.message);
         logger.error(e.stack);
         exit(-1);
@@ -117,11 +135,11 @@ process.on('exit', function () {
     exit(0);
 });
 
-function exit(status) {
+function exit(status: number) {
     logger.info('Exiting with status: %d ...', status);
     if (server) {
         logger.info('Stopping HTTP Server...');
-        var _server = server;
+        const _server = server;
         server = null;
         _server.close(() => {
             logger.info('HTTP Server stopped.');
