53381636e35c95e374776a91b2ffffdec15cc49c
Igor Kulikov <ikulikov@thingsboard.io>
2022-07-18 17:31:44 +0300

    Fix ota package cyclic dependency

--- dao/src/main/java/org/thingsboard/server/dao/service/validator/AbstractHasOtaPackageValidator.java
+++ dao/src/main/java/org/thingsboard/server/dao/service/validator/AbstractHasOtaPackageValidator.java
@@ -16,6 +16,7 @@
 package org.thingsboard.server.dao.service.validator;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.thingsboard.server.common.data.BaseData;
 import org.thingsboard.server.common.data.HasOtaPackage;
 import org.thingsboard.server.common.data.OtaPackage;
@@ -29,6 +30,7 @@ import org.thingsboard.server.dao.service.DataValidator;
 public abstract class AbstractHasOtaPackageValidator<D extends BaseData<?>> extends DataValidator<D> {
 
     @Autowired
+    @Lazy
     private OtaPackageService otaPackageService;
 
     protected <T extends HasOtaPackage> void validateOtaPackage(TenantId tenantId, T entity, DeviceProfileId deviceProfileId) {
