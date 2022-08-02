fd9736b051158d09bd84169fe9223b5ee6b7dd62
Yuriy Lytvynchuk <yuriy.litv@gmail.com>
2022-08-02 16:11:57 +0300

    add configDirective

--- rule-engine/rule-engine-components/src/main/java/org/thingsboard/rule/engine/transform/TbCopyFromMdToMsgNode.java
+++ rule-engine/rule-engine-components/src/main/java/org/thingsboard/rule/engine/transform/TbCopyFromMdToMsgNode.java
@@ -44,7 +44,7 @@ import java.util.concurrent.ExecutionException;
                 "If the msg is not a JSON object returns the incoming message as outbound message with <code>Failure</code> chain, " +
                 "otherwise returns transformed messages via <code>Success</code> chain",
         uiResources = {"static/rulenode/rulenode-core-config.js"},
-        configDirective = "",
+        configDirective = "tbTransformationNodeCopyFromMdToMsgConfig",
         icon = "functions"
 )
 public class TbCopyFromMdToMsgNode implements TbNode {
