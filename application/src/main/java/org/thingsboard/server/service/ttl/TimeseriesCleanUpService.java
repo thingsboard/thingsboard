a275454d10f6600f03ffb097fe772aa1f9af7d6c
van-vanich <panahno13@gmail.com>
2022-08-04 22:24:41 +0300

    use cleanup method from dao and replace deprecated method

--- application/src/main/java/org/thingsboard/server/service/ttl/TimeseriesCleanUpService.java
+++ application/src/main/java/org/thingsboard/server/service/ttl/TimeseriesCleanUpService.java
@@ -19,7 +19,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-import org.springframework.util.CollectionUtils;
 import org.springframework.util.StringUtils;
 import org.thingsboard.server.dao.timeseries.TimeseriesService;
 import org.thingsboard.server.queue.discovery.PartitionService;
@@ -54,17 +53,13 @@ public class TimeseriesCleanUpService extends AbstractCleanUpService {
 
     @PostConstruct
     public void init() {
-        excludedKeys = StringUtils.isEmpty(excludedKeysStr) ? null : new ArrayList<>(Arrays.asList(excludedKeysStr.trim().split("\\s*,\\s*")));
+        excludedKeys = StringUtils.hasLength(excludedKeysStr) ? null : new ArrayList<>(Arrays.asList(excludedKeysStr.trim().split("\\s*,\\s*")));
     }
 
     @Scheduled(initialDelayString = "${sql.ttl.ts.execution_interval_ms}", fixedDelayString = "${sql.ttl.ts.execution_interval_ms}")
     public void cleanUp() {
         if (ttlTaskExecutionEnabled && isSystemTenantPartitionMine()) {
-            if (CollectionUtils.isEmpty(excludedKeys)) {
-                timeseriesService.cleanup(systemTtl);
-            } else {
-                timeseriesService.cleanup(systemTtl, excludedKeys);
-            }
+            timeseriesService.cleanup(systemTtl, excludedKeys);
         }
     }
 
