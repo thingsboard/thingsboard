2c8b938540b2f2fdaa16d60de82f96859e69e371
Andrii Shvaika <ashvayka@thingsboard.io>
2022-08-04 15:22:24 +0300

    Tenant Routing Info performance optimization

--- application/src/main/java/org/thingsboard/server/service/queue/DefaultTenantRoutingInfoService.java
+++ application/src/main/java/org/thingsboard/server/service/queue/DefaultTenantRoutingInfoService.java
@@ -42,9 +42,8 @@ public class DefaultTenantRoutingInfoService implements TenantRoutingInfoService
 
     @Override
     public TenantRoutingInfo getRoutingInfo(TenantId tenantId) {
-        Tenant tenant = tenantService.findTenantById(tenantId);
-        if (tenant != null) {
-            TenantProfile tenantProfile = tenantProfileCache.get(tenant.getTenantProfileId());
+        TenantProfile tenantProfile = tenantProfileCache.get(tenantId);
+        if (tenantProfile != null) {
             return new TenantRoutingInfo(tenantId, tenantProfile.isIsolatedTbRuleEngine());
         } else {
             throw new RuntimeException("Tenant not found!");
