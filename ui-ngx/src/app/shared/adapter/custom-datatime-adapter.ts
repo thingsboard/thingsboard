2a80c0b38b7fb3f949e04ce00cc96166fa58b8e0
Artem Dzhereleiko <Dgerik2015@gmail.com>
2022-08-17 11:19:27 +0300

    UI: Refactoring

--- ui-ngx/src/app/shared/adapter/custom-datatime-adapter.ts
+++ ui-ngx/src/app/shared/adapter/custom-datatime-adapter.ts
@@ -17,21 +17,20 @@
 import { Injectable } from '@angular/core';
 import { NativeDatetimeAdapter } from '@mat-datetimepicker/core';
 
-
 @Injectable()
 export class CustomDateAdapter extends NativeDatetimeAdapter {
 
-  parse(value: string): Date {
+  parse(value: string | number): Date {
     if (typeof value === 'number') {
       return new Date(value);
     }
-    let newDate = null;
+    let newDate = value;
     const formatToParts = Intl.DateTimeFormat(this.locale).formatToParts();
     if (formatToParts[0].type.toLowerCase() === 'day') {
       const literal = formatToParts[1].value;
-      newDate = value.replace(new RegExp(`(\\d+[${literal}])(\\d+[${literal}])`), '$2$1');
+      newDate = newDate.replace(new RegExp(`(\\d+[${literal}])(\\d+[${literal}])`), '$2$1');
     }
-    return value ? new Date(Date.parse(newDate || value)) : null;
+    return newDate ? new Date(Date.parse(newDate)) : null;
   }
 
 }
