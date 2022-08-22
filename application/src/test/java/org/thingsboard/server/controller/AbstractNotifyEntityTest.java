8a2da31e58a62a66e8cb753bbd6bc6f6dc9d9726
nickAS21 <nick@thingsboard.io>
2022-08-22 22:35:46 +0300

    fix_bug: Entity delete with delete relations, licence

--- application/src/test/java/org/thingsboard/server/controller/AbstractNotifyEntityTest.java
+++ application/src/test/java/org/thingsboard/server/controller/AbstractNotifyEntityTest.java
@@ -1,12 +1,12 @@
 /**
  * Copyright © 2016-2022 The Thingsboard Authors
- * <p>
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -722,8 +722,7 @@ public abstract class AbstractNotifyEntityTest extends AbstractWebTest {
                 .andExpect(status().isNotFound())
                 .andExpect(statusReason(containsString(msgErrorNoFound(entityTestNameClass, expectTestEntityRelationToIdStr))));
         relationsInfos =
-                JacksonUtil.convertValue(doGet(urlEntityFroms, JsonNode.class), new TypeReference<>() {
-                });
+                JacksonUtil.convertValue(doGet(urlEntityFroms, JsonNode.class), new TypeReference<>() { });
         assertNotNull("Relations is not found!", relationsInfos);
         assertEquals("List of found relations is not equal to number of relations left!",
                 numOfRelations - 1, relationsInfos.size());
