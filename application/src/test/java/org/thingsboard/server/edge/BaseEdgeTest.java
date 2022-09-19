b7f66906f6603263c581f3a7e4e5680765c19b2f
Volodymyr Babak <volodymyr.babak@gmail.com>
2022-09-19 15:32:45 +0300

    Added edge assing/unassing customer test

---
+++ application/src/test/java/org/thingsboard/server/edge/BaseEdgeTest.java
@@ -0,0 +1,62 @@
+/**
+ * Copyright © 2016-2022 The Thingsboard Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.thingsboard.server.edge;
+
+import com.google.protobuf.AbstractMessage;
+import org.junit.Assert;
+import org.junit.Test;
+import org.thingsboard.server.common.data.Customer;
+import org.thingsboard.server.common.data.edge.Edge;
+import org.thingsboard.server.common.data.id.CustomerId;
+import org.thingsboard.server.common.data.id.EntityId;
+import org.thingsboard.server.gen.edge.v1.EdgeConfiguration;
+
+import java.util.UUID;
+
+abstract public class BaseEdgeTest extends AbstractEdgeTest {
+
+    @Test
+    public void testEdge_assignToCustomer_unassignFromCustomer() throws Exception {
+        // assign edge to customer
+        edgeImitator.expectMessageAmount(1);
+        Customer customer = new Customer();
+        customer.setTitle("Edge Customer");
+        Customer savedCustomer = doPost("/api/customer", customer, Customer.class);
+        Assert.assertTrue(edgeImitator.waitForMessages());
+
+        edgeImitator.expectMessageAmount(1);
+        doPost("/api/customer/" + savedCustomer.getUuidId()
+                + "/edge/" + edge.getUuidId(), Edge.class);
+        Assert.assertTrue(edgeImitator.waitForMessages());
+        AbstractMessage latestMessage = edgeImitator.getLatestMessage();
+        Assert.assertTrue(latestMessage instanceof EdgeConfiguration);
+        EdgeConfiguration edgeConfiguration = (EdgeConfiguration) latestMessage;
+        Assert.assertEquals(savedCustomer.getUuidId().getMostSignificantBits(), edgeConfiguration.getCustomerIdMSB());
+        Assert.assertEquals(savedCustomer.getUuidId().getLeastSignificantBits(), edgeConfiguration.getCustomerIdLSB());
+
+        // unassign edge from customer
+        edgeImitator.expectMessageAmount(1);
+        doDelete("/api/customer/edge/" + edge.getUuidId(), Edge.class);
+        Assert.assertTrue(edgeImitator.waitForMessages());
+        latestMessage = edgeImitator.getLatestMessage();
+        Assert.assertTrue(latestMessage instanceof EdgeConfiguration);
+        edgeConfiguration = (EdgeConfiguration) latestMessage;
+        Assert.assertEquals(
+                new CustomerId(EntityId.NULL_UUID),
+                new CustomerId(new UUID(edgeConfiguration.getCustomerIdMSB(), edgeConfiguration.getCustomerIdLSB())));
+
+    }
+}
