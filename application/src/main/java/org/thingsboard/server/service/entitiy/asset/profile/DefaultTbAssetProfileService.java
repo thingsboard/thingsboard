0717a56a690f9a1b637e480086a6f315cae5153f
Igor Kulikov <ikulikov@thingsboard.io>
2022-09-27 10:33:22 +0300

    Forbid changes to 'TbServiceQueue' asset profile.

--- application/src/main/java/org/thingsboard/server/service/entitiy/asset/profile/DefaultTbAssetProfileService.java
+++ application/src/main/java/org/thingsboard/server/service/entitiy/asset/profile/DefaultTbAssetProfileService.java
@@ -22,6 +22,7 @@ import org.thingsboard.server.common.data.EntityType;
 import org.thingsboard.server.common.data.User;
 import org.thingsboard.server.common.data.asset.AssetProfile;
 import org.thingsboard.server.common.data.audit.ActionType;
+import org.thingsboard.server.common.data.exception.ThingsboardErrorCode;
 import org.thingsboard.server.common.data.exception.ThingsboardException;
 import org.thingsboard.server.common.data.id.AssetProfileId;
 import org.thingsboard.server.common.data.id.TenantId;
@@ -29,6 +30,8 @@ import org.thingsboard.server.common.data.plugin.ComponentLifecycleEvent;
 import org.thingsboard.server.dao.asset.AssetProfileService;
 import org.thingsboard.server.queue.util.TbCoreComponent;
 import org.thingsboard.server.service.entitiy.AbstractTbEntityService;
+
+import static org.thingsboard.server.dao.asset.BaseAssetService.TB_SERVICE_QUEUE;
 
 @Service
 @TbCoreComponent
@@ -43,6 +46,14 @@ public class DefaultTbAssetProfileService extends AbstractTbEntityService implem
         ActionType actionType = assetProfile.getId() == null ? ActionType.ADDED : ActionType.UPDATED;
         TenantId tenantId = assetProfile.getTenantId();
         try {
+            if (TB_SERVICE_QUEUE.equals(assetProfile.getName())) {
+                throw new ThingsboardException("Unable to save asset profile with name " + TB_SERVICE_QUEUE, ThingsboardErrorCode.BAD_REQUEST_PARAMS);
+            } else if (assetProfile.getId() != null) {
+                AssetProfile foundAssetProfile = assetProfileService.findAssetProfileById(tenantId, assetProfile.getId());
+                if (foundAssetProfile != null && TB_SERVICE_QUEUE.equals(foundAssetProfile.getName())) {
+                    throw new ThingsboardException("Updating asset profile with name " + TB_SERVICE_QUEUE + " is prohibited!", ThingsboardErrorCode.BAD_REQUEST_PARAMS);
+                }
+            }
             AssetProfile savedAssetProfile = checkNotNull(assetProfileService.saveAssetProfile(assetProfile));
             autoCommit(user, savedAssetProfile.getId());
             tbClusterService.broadcastEntityStateChangeEvent(tenantId, savedAssetProfile.getId(),
