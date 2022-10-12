341df84003d148b26c68879259e938fa9ec17c0c
nickAS21 <nick@thingsboard.io>
2022-10-12 14:33:47 +0300

    fix_bug: WIDGET_TYPE

--- application/src/test/java/org/thingsboard/server/controller/BaseWidgetTypeControllerTest.java
+++ application/src/test/java/org/thingsboard/server/controller/BaseWidgetTypeControllerTest.java
@@ -18,24 +18,35 @@ package org.thingsboard.server.controller;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.hibernate.exception.ConstraintViolationException;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.AdditionalAnswers;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.test.context.ContextConfiguration;
 import org.thingsboard.server.common.data.Tenant;
 import org.thingsboard.server.common.data.User;
 import org.thingsboard.server.common.data.security.Authority;
 import org.thingsboard.server.common.data.widget.WidgetType;
 import org.thingsboard.server.common.data.widget.WidgetTypeDetails;
 import org.thingsboard.server.common.data.widget.WidgetsBundle;
+import org.thingsboard.server.dao.widget.WidgetTypeDao;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 import static org.hamcrest.Matchers.containsString;
+import static org.mockito.ArgumentMatchers.any;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@ContextConfiguration(classes = {BaseWidgetTypeControllerTest.Config.class})
 public abstract class BaseWidgetTypeControllerTest extends AbstractControllerTest {
 
     private IdComparator<WidgetType> idComparator = new IdComparator<>();
@@ -43,10 +54,24 @@ public abstract class BaseWidgetTypeControllerTest extends AbstractControllerTes
     private Tenant savedTenant;
     private WidgetsBundle savedWidgetsBundle;
     private User tenantAdmin;
+    private JsonNode descriptor ;
+
+    @Autowired
+    private WidgetTypeDao widgetTypeDao;
+
+    static class Config {
+        @Bean
+        @Primary
+        public WidgetTypeDao widgetTypeDao(WidgetTypeDao widgetTypeDao) {
+            return Mockito.mock(WidgetTypeDao.class, AdditionalAnswers.delegatesTo(widgetTypeDao));
+        }
+    }
 
     @Before
     public void beforeTest() throws Exception {
         loginSysAdmin();
+
+        descriptor = new ObjectMapper().readValue("{ \"someKey\": \"someValue\" }", JsonNode.class);
 
         Tenant tenant = new Tenant();
         tenant.setTitle("My tenant");
@@ -77,19 +102,16 @@ public abstract class BaseWidgetTypeControllerTest extends AbstractControllerTes
 
     @Test
     public void testSaveWidgetType() throws Exception {
-        WidgetTypeDetails widgetType = new WidgetTypeDetails();
-        widgetType.setBundleAlias(savedWidgetsBundle.getAlias());
-        widgetType.setName("Widget Type");
-        widgetType.setDescriptor(new ObjectMapper().readValue("{ \"someKey\": \"someValue\" }", JsonNode.class));
-        WidgetTypeDetails savedWidgetType = doPost("/api/widgetType", widgetType, WidgetTypeDetails.class);
+        String name = "Widget Type";
+        WidgetTypeDetails savedWidgetType = createAndSavedWidgetTypeDetails(name);
 
         Assert.assertNotNull(savedWidgetType);
         Assert.assertNotNull(savedWidgetType.getId());
         Assert.assertNotNull(savedWidgetType.getAlias());
         Assert.assertTrue(savedWidgetType.getCreatedTime() > 0);
         Assert.assertEquals(savedTenant.getId(), savedWidgetType.getTenantId());
-        Assert.assertEquals(widgetType.getName(), savedWidgetType.getName());
-        Assert.assertEquals(widgetType.getDescriptor(), savedWidgetType.getDescriptor());
+        Assert.assertEquals(name, savedWidgetType.getName());
+        Assert.assertEquals(descriptor, savedWidgetType.getDescriptor());
         Assert.assertEquals(savedWidgetsBundle.getAlias(), savedWidgetType.getBundleAlias());
 
         savedWidgetType.setName("New Widget Type");
@@ -102,11 +124,7 @@ public abstract class BaseWidgetTypeControllerTest extends AbstractControllerTes
 
     @Test
     public void testUpdateWidgetTypeFromDifferentTenant() throws Exception {
-        WidgetTypeDetails widgetType = new WidgetTypeDetails();
-        widgetType.setBundleAlias(savedWidgetsBundle.getAlias());
-        widgetType.setName("Widget Type");
-        widgetType.setDescriptor(new ObjectMapper().readValue("{ \"someKey\": \"someValue\" }", JsonNode.class));
-        WidgetTypeDetails savedWidgetType = doPost("/api/widgetType", widgetType, WidgetTypeDetails.class);
+        WidgetTypeDetails savedWidgetType = createAndSavedWidgetTypeDetails(("Widget Type"));
 
         loginDifferentTenant();
         doPost("/api/widgetType", savedWidgetType, WidgetTypeDetails.class, status().isForbidden());
@@ -115,11 +133,7 @@ public abstract class BaseWidgetTypeControllerTest extends AbstractControllerTes
 
     @Test
     public void testFindWidgetTypeById() throws Exception {
-        WidgetTypeDetails widgetType = new WidgetTypeDetails();
-        widgetType.setBundleAlias(savedWidgetsBundle.getAlias());
-        widgetType.setName("Widget Type");
-        widgetType.setDescriptor(new ObjectMapper().readValue("{ \"someKey\": \"someValue\" }", JsonNode.class));
-        WidgetTypeDetails savedWidgetType = doPost("/api/widgetType", widgetType, WidgetTypeDetails.class);
+        WidgetTypeDetails savedWidgetType = createAndSavedWidgetTypeDetails("Widget Type");
         WidgetTypeDetails foundWidgetType = doGet("/api/widgetType/" + savedWidgetType.getId().getId().toString(), WidgetTypeDetails.class);
         Assert.assertNotNull(foundWidgetType);
         Assert.assertEquals(savedWidgetType, foundWidgetType);
@@ -127,11 +141,7 @@ public abstract class BaseWidgetTypeControllerTest extends AbstractControllerTes
 
     @Test
     public void testDeleteWidgetType() throws Exception {
-        WidgetTypeDetails widgetType = new WidgetTypeDetails();
-        widgetType.setBundleAlias(savedWidgetsBundle.getAlias());
-        widgetType.setName("Widget Type");
-        widgetType.setDescriptor(new ObjectMapper().readValue("{ \"someKey\": \"someValue\" }", JsonNode.class));
-        WidgetTypeDetails savedWidgetType = doPost("/api/widgetType", widgetType, WidgetTypeDetails.class);
+        WidgetTypeDetails savedWidgetType = createAndSavedWidgetTypeDetails("Widget Type");
 
         doDelete("/api/widgetType/"+savedWidgetType.getId().getId().toString())
                 .andExpect(status().isOk());
@@ -184,11 +194,7 @@ public abstract class BaseWidgetTypeControllerTest extends AbstractControllerTes
 
     @Test
     public void testUpdateWidgetTypeBundleAlias() throws Exception {
-        WidgetTypeDetails widgetType = new WidgetTypeDetails();
-        widgetType.setBundleAlias(savedWidgetsBundle.getAlias());
-        widgetType.setName("Widget Type");
-        widgetType.setDescriptor(new ObjectMapper().readValue("{ \"someKey\": \"someValue\" }", JsonNode.class));
-        WidgetTypeDetails savedWidgetType = doPost("/api/widgetType", widgetType, WidgetTypeDetails.class);
+        WidgetTypeDetails savedWidgetType = createAndSavedWidgetTypeDetails("Widget Type");
         savedWidgetType.setBundleAlias("some_alias");
         doPost("/api/widgetType", savedWidgetType)
                 .andExpect(status().isBadRequest())
@@ -198,11 +204,7 @@ public abstract class BaseWidgetTypeControllerTest extends AbstractControllerTes
 
     @Test
     public void testUpdateWidgetTypeAlias() throws Exception {
-        WidgetTypeDetails widgetType = new WidgetTypeDetails();
-        widgetType.setBundleAlias(savedWidgetsBundle.getAlias());
-        widgetType.setName("Widget Type");
-        widgetType.setDescriptor(new ObjectMapper().readValue("{ \"someKey\": \"someValue\" }", JsonNode.class));
-        WidgetTypeDetails savedWidgetType = doPost("/api/widgetType", widgetType, WidgetTypeDetails.class);
+        WidgetTypeDetails savedWidgetType = createAndSavedWidgetTypeDetails("Widget Type");
         savedWidgetType.setAlias("some_alias");
         doPost("/api/widgetType", savedWidgetType)
                 .andExpect(status().isBadRequest())
@@ -232,15 +234,51 @@ public abstract class BaseWidgetTypeControllerTest extends AbstractControllerTes
 
     @Test
     public void testGetWidgetType() throws Exception {
-        WidgetTypeDetails widgetType = new WidgetTypeDetails();
-        widgetType.setBundleAlias(savedWidgetsBundle.getAlias());
-        widgetType.setName("Widget Type");
-        widgetType.setDescriptor(new ObjectMapper().readValue("{ \"someKey\": \"someValue\" }", JsonNode.class));
-        WidgetTypeDetails savedWidgetType = doPost("/api/widgetType", widgetType, WidgetTypeDetails.class);
+        WidgetTypeDetails savedWidgetType = createAndSavedWidgetTypeDetails("Widget Type");
         WidgetType foundWidgetType = doGet("/api/widgetType?isSystem={isSystem}&bundleAlias={bundleAlias}&alias={alias}",
                 WidgetType.class, false, savedWidgetsBundle.getAlias(), savedWidgetType.getAlias());
         Assert.assertNotNull(foundWidgetType);
         Assert.assertEquals(new WidgetType(savedWidgetType), foundWidgetType);
     }
+
+    @Test
+    public void testDeleteWidgetTypeWithTransactionalOk() throws Exception {
+        WidgetTypeDetails savedWidgetType = createAndSavedWidgetTypeDetails("MOCK_TransactionalOk");
+        WidgetTypeDetails foundWidgetType = doGet("/api/widgetType/" + savedWidgetType.getId().getId().toString(), WidgetTypeDetails.class);
+        Assert.assertNotNull(foundWidgetType);
+
+        doDelete("/api/widgetType/"+savedWidgetType.getId().getId().toString())
+                .andExpect(status().isOk());
+
+        doGet("/api/widgetType/"+savedWidgetType.getId().getId().toString())
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void testDeleteWidgetTypeWithTransactionalException() throws Exception {
+        WidgetTypeDetails savedWidgetType = createAndSavedWidgetTypeDetails("MOCK_TransactionalException");
+        WidgetTypeDetails foundWidgetType = doGet("/api/widgetType/" + savedWidgetType.getId().getId().toString(), WidgetTypeDetails.class);
+        Assert.assertNotNull(foundWidgetType);
+
+        Mockito.doThrow(new ConstraintViolationException("mock message", new SQLException(), "MOCK_CONSTRAINT")).when(widgetTypeDao).removeById(any(), any());
+        try {
+            doDelete("/api/widgetType/"+savedWidgetType.getId().getId().toString())
+                    .andExpect(status().isInternalServerError());
+
+            WidgetTypeDetails foundWidgetTypeAfter = doGet("/api/widgetType/" + savedWidgetType.getId().getId().toString(), WidgetTypeDetails.class);
+            Assert.assertNotNull(foundWidgetTypeAfter);
+        } finally {
+            Mockito.reset(widgetTypeDao);
+        }
+    }
+
+    private WidgetTypeDetails createAndSavedWidgetTypeDetails(String name) {
+        WidgetTypeDetails widgetType = new WidgetTypeDetails();
+        widgetType.setBundleAlias(savedWidgetsBundle.getAlias());
+        widgetType.setName(name);
+        widgetType.setDescriptor(descriptor);
+        return doPost("/api/widgetType", widgetType, WidgetTypeDetails.class);
+    }
+
 
 }
