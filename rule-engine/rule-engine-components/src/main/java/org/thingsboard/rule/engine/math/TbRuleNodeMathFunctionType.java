dc6eaef0376885bb7f211566a26a105e4c889cbd
Yuriy Lytvynchuk <yuriy.litv@gmail.com>
2022-10-26 17:02:15 +0300

    fix space

--- rule-engine/rule-engine-components/src/main/java/org/thingsboard/rule/engine/math/TbRuleNodeMathFunctionType.java
+++ rule-engine/rule-engine-components/src/main/java/org/thingsboard/rule/engine/math/TbRuleNodeMathFunctionType.java
@@ -23,7 +23,7 @@ public enum TbRuleNodeMathFunctionType {
     SIN, SINH, COS, COSH, TAN, TANH, ACOS, ASIN, ATAN, ATAN2(2),
     EXP, EXPM1, SQRT, CBRT, GET_EXP(1, 1, true), HYPOT(2), LOG, LOG10, LOG1P,
     CEIL(1, 1, true), FLOOR(1, 1, true), FLOOR_DIV(2), FLOOR_MOD(2),
-    ABS, MIN(2), MAX(2), POW( 2), SIGNUM, RAD, DEG,
+    ABS, MIN(2), MAX(2), POW(2), SIGNUM, RAD, DEG,
 
     CUSTOM(0, 16, false); //Custom function based on exp4j
 
