bf83eff7492e275a8959137bb7c4bdbc1f1f24c0
oyurov <oyurov@thingsboard.io>
2022-11-18 11:27:19 +0100

    Change modifier access to default

--- application/src/main/java/org/thingsboard/server/service/security/auth/oauth2/Oauth2AuthenticationSuccessHandler.java
+++ application/src/main/java/org/thingsboard/server/service/security/auth/oauth2/Oauth2AuthenticationSuccessHandler.java
@@ -128,7 +128,7 @@ public class Oauth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationS
         httpCookieOAuth2AuthorizationRequestRepository.removeAuthorizationRequestCookies(request, response);
     }
 
-    protected String getRedirectUrl(String baseUrl, SecurityUser securityUser) {
+    String getRedirectUrl(String baseUrl, SecurityUser securityUser) {
         JwtPair tokenPair = tokenFactory.createTokenPair(securityUser);
         if (baseUrl.indexOf("?") > 0) {
             baseUrl += "&";
