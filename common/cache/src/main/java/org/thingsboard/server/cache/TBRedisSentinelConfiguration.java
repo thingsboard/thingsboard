2c53656da494453e8e57f0150368c5f6acc66562
Kyryl <kfeshchenko@thingsboard.io>
2022-12-27 15:19:22 +0200

    added msa README, changed yml strucuture, deleted latest and ports in docker

--- common/cache/src/main/java/org/thingsboard/server/cache/TBRedisSentinelConfiguration.java
+++ common/cache/src/main/java/org/thingsboard/server/cache/TBRedisSentinelConfiguration.java
@@ -30,14 +30,14 @@ public class TBRedisSentinelConfiguration extends TBRedisCacheConfiguration {
     @Value("${redis.password:}")
     private String password;
 
-    @Value("${redis.cluster.sentinel.master:}")
+    @Value("${redis.sentinel.master:}")
     private String master;
 
-    @Value("${redis.cluster.sentinel.sentinels:}")
+    @Value("${redis.sentinel.sentinels:}")
     private String sentinels;
 
-    @Value("${redis.cluster.useDefaultPoolConfig:true}")
-    private boolean useDefaultPoolConfig;
+    @Value("${redis.sentinel.usePoolConfig:true}")
+    private boolean usePoolConfig;
 
     @Value("${redis.db:}")
     private Integer database;
@@ -48,7 +48,7 @@ public class TBRedisSentinelConfiguration extends TBRedisCacheConfiguration {
         redisSentinelConfiguration.setMaster(this.master);
         redisSentinelConfiguration.setDatabase(this.database);
         redisSentinelConfiguration.setPassword(password);
-        if (useDefaultPoolConfig) {
+        if (usePoolConfig) {
             return new JedisConnectionFactory(redisSentinelConfiguration);
         } else {
             return new JedisConnectionFactory(redisSentinelConfiguration, buildPoolConfig());
