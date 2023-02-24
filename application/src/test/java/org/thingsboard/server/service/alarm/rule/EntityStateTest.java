75f247f5bd85efa59ad92cf263099eb2d2f3526a
YevhenBondarenko <ybondarenko@thingsboard.io>
2023-02-24 11:49:09 +0100

    replaced EntityStateTest test

--- application/src/test/java/org/thingsboard/server/service/alarm/rule/EntityStateTest.java
+++ application/src/test/java/org/thingsboard/server/service/alarm/rule/EntityStateTest.java
@@ -19,24 +19,27 @@ import com.google.common.util.concurrent.Futures;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
+import org.mockito.Mockito;
 import org.thingsboard.common.util.JacksonUtil;
 import org.thingsboard.rule.engine.api.RuleEngineAlarmService;
-import org.thingsboard.rule.engine.api.TbContext;
-import org.thingsboard.rule.engine.profile.TbDeviceProfileNodeConfiguration;
+import org.thingsboard.server.cluster.TbClusterService;
 import org.thingsboard.server.common.data.DataConstants;
-import org.thingsboard.server.common.data.DeviceProfile;
 import org.thingsboard.server.common.data.alarm.Alarm;
 import org.thingsboard.server.common.data.alarm.AlarmSeverity;
+import org.thingsboard.server.common.data.alarm.rule.AlarmRule;
+import org.thingsboard.server.common.data.alarm.rule.AlarmRuleOriginatorTargetEntity;
+import org.thingsboard.server.common.data.alarm.rule.filter.AlarmRuleAllDevicesEntityFilter;
 import org.thingsboard.server.common.data.device.profile.AlarmCondition;
 import org.thingsboard.server.common.data.device.profile.AlarmConditionFilter;
 import org.thingsboard.server.common.data.device.profile.AlarmConditionFilterKey;
 import org.thingsboard.server.common.data.device.profile.AlarmConditionKeyType;
 import org.thingsboard.server.common.data.device.profile.AlarmRuleCondition;
 import org.thingsboard.server.common.data.device.profile.AlarmRuleConfiguration;
-import org.thingsboard.server.common.data.device.profile.DeviceProfileData;
 import org.thingsboard.server.common.data.device.profile.SimpleAlarmConditionSpec;
 import org.thingsboard.server.common.data.id.AlarmId;
+import org.thingsboard.server.common.data.id.AlarmRuleId;
 import org.thingsboard.server.common.data.id.DeviceId;
+import org.thingsboard.server.common.data.id.TenantId;
 import org.thingsboard.server.common.data.query.BooleanFilterPredicate;
 import org.thingsboard.server.common.data.query.EntityKeyValueType;
 import org.thingsboard.server.common.data.query.FilterPredicateValue;
@@ -55,7 +58,6 @@ import java.util.UUID;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyCollection;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
@@ -64,114 +66,116 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 public class EntityStateTest {
-//
-//    private TbAlarmRuleContext ctx;
-//
-//    @Before
-//    public void beforeEach() {
-//        ctx = mock(TbAlarmRuleContext.class);
-//
-//        when(ctx.getDeviceService()).thenReturn(mock(DeviceService.class));
-//
-//        AttributesService attributesService = mock(AttributesService.class);
-//        when(attributesService.find(any(), any(), any(), anyCollection())).thenReturn(Futures.immediateFuture(Collections.emptyList()));
-//        when(ctx.getAttributesService()).thenReturn(attributesService);
-//
-//        RuleEngineAlarmService alarmService = mock(RuleEngineAlarmService.class);
-//        when(alarmService.findLatestByOriginatorAndType(any(), any(), any())).thenReturn(Futures.immediateFuture(null));
-//        when(alarmService.createOrUpdateAlarm(any())).thenAnswer(invocationOnMock -> {
-//            Alarm alarm = invocationOnMock.getArgument(0);
-//            alarm.setId(new AlarmId(UUID.randomUUID()));
-//            return alarm;
-//        });
-//        when(ctx.getAlarmService()).thenReturn(alarmService);
-//
-//        when(ctx.newMsg(any(), any(), any(), any(), any(), any())).thenAnswer(invocationOnMock -> {
-//            String data = invocationOnMock.getArgument(invocationOnMock.getArguments().length - 1);
-//            return TbMsg.newMsg(null, null, new TbMsgMetaData(), data);
-//        });
-//
-//    }
-//
-//    @Test
-//    public void whenAttributeIsDeleted_thenUnneededAlarmRulesAreNotReevaluated() throws Exception {
-//
-//        AlarmRuleConfiguration alarmConfig = createAlarmConfigWithBoolAttrCondition("enabled", false);
-//        DeviceId deviceId = new DeviceId(UUID.randomUUID());
-//        EntityState deviceState = createDeviceState(deviceId, alarmConfig);
-//
-//        TbMsg attributeUpdateMsg = TbMsg.newMsg(SessionMsgType.POST_ATTRIBUTES_REQUEST.name(),
-//                deviceId, new TbMsgMetaData(), "{ \"enabled\": false }");
-//
-//        deviceState.process(ctx, attributeUpdateMsg);
-//
-//        ArgumentCaptor<TbMsg> resultMsgCaptor = ArgumentCaptor.forClass(TbMsg.class);
-//        verify(ctx).enqueueForTellNext(resultMsgCaptor.capture(), eq("Alarm Created"));
-//        Alarm alarm = JacksonUtil.fromString(resultMsgCaptor.getValue().getData(), Alarm.class);
-//
-//        deviceState.process(ctx, TbMsg.newMsg(DataConstants.ALARM_CLEAR, deviceId, new TbMsgMetaData(), JacksonUtil.toString(alarm)));
-//        reset(ctx);
-//
-//        String deletedAttributes = "{ \"attributes\": [ \"other\" ] }";
-//        deviceState.process(ctx, TbMsg.newMsg(DataConstants.ATTRIBUTES_DELETED, deviceId, new TbMsgMetaData(), deletedAttributes));
-//        verify(ctx, never()).enqueueForTellNext(any(), anyString());
-//    }
-//
-//    @Test
-//    public void whenDeletingClearedAlarm_thenNoError() throws Exception {
-//        AlarmRuleConfiguration alarmConfig = createAlarmConfigWithBoolAttrCondition("enabled", false);
-//        DeviceId deviceId = new DeviceId(UUID.randomUUID());
-//        DeviceState deviceState = createDeviceState(deviceId, alarmConfig);
-//
-//        TbMsg attributeUpdateMsg = TbMsg.newMsg(SessionMsgType.POST_ATTRIBUTES_REQUEST.name(),
-//                deviceId, new TbMsgMetaData(), "{ \"enabled\": false }");
-//
-//        deviceState.process(ctx, attributeUpdateMsg);
-//        ArgumentCaptor<TbMsg> resultMsgCaptor = ArgumentCaptor.forClass(TbMsg.class);
-//        verify(ctx).enqueueForTellNext(resultMsgCaptor.capture(), eq("Alarm Created"));
-//        Alarm alarm = JacksonUtil.fromString(resultMsgCaptor.getValue().getData(), Alarm.class);
-//
-//        deviceState.process(ctx, TbMsg.newMsg(DataConstants.ALARM_CLEAR, deviceId, new TbMsgMetaData(), JacksonUtil.toString(alarm)));
-//
-//        TbMsg alarmDeleteNotification = TbMsg.newMsg(DataConstants.ALARM_DELETE, deviceId, new TbMsgMetaData(), JacksonUtil.toString(alarm));
-//        assertDoesNotThrow(() -> {
-//            deviceState.process(ctx, alarmDeleteNotification);
-//        });
-//    }
-//
-//
-//    private EntityState createDeviceState(DeviceId deviceId, AlarmRuleConfiguration... alarmConfigs) {
-//        DeviceProfile deviceProfile = new DeviceProfile();
-//        DeviceProfileData profileData = new DeviceProfileData();
-//        profileData.setAlarms(List.of(alarmConfigs));
-//        deviceProfile.setProfileData(profileData);
-//
-//        ProfileState profileState = new ProfileState(deviceProfile);
-//        return new EntityState(ctx, new TbDeviceProfileNodeConfiguration(),
-//                deviceId, profileState, null);
-//    }
-//
-//    private AlarmRuleConfiguration createAlarmConfigWithBoolAttrCondition(String key, boolean value) {
-//
-//        AlarmConditionFilter condition = new AlarmConditionFilter();
-//        condition.setKey(new AlarmConditionFilterKey(AlarmConditionKeyType.ATTRIBUTE, key));
-//        condition.setValueType(EntityKeyValueType.BOOLEAN);
-//        BooleanFilterPredicate predicate = new BooleanFilterPredicate();
-//        predicate.setOperation(BooleanFilterPredicate.BooleanOperation.EQUAL);
-//        predicate.setValue(new FilterPredicateValue<>(value));
-//        condition.setPredicate(predicate);
-//
-//        AlarmRuleConfiguration alarmConfig = new AlarmRuleConfiguration();
-//        alarmConfig.setId("MyAlarmID");
-//        alarmConfig.setAlarmType("MyAlarm");
-//        AlarmRuleCondition alarmRule = new AlarmRuleCondition();
-//        AlarmCondition alarmCondition = new AlarmCondition();
-//        alarmCondition.setSpec(new SimpleAlarmConditionSpec());
-//        alarmCondition.setCondition(List.of(condition));
-//        alarmRule.setCondition(alarmCondition);
-//        alarmConfig.setCreateRules(new TreeMap<>(Map.of(AlarmSeverity.CRITICAL, alarmRule)));
-//
-//        return alarmConfig;
-//    }
+
+    private TbAlarmRuleContext ctx;
+    private TbClusterService clusterService;
+    private TenantId tenantId;
+
+    @Before
+    public void beforeEach() {
+        ctx = mock(TbAlarmRuleContext.class);
+        clusterService = mock(TbClusterService.class);
+
+        when(ctx.getDeviceService()).thenReturn(mock(DeviceService.class));
+        when(ctx.getClusterService()).thenReturn(clusterService);
+
+        AttributesService attributesService = mock(AttributesService.class);
+        when(attributesService.find(any(), any(), any(), anyCollection())).thenReturn(Futures.immediateFuture(Collections.emptyList()));
+        when(ctx.getAttributesService()).thenReturn(attributesService);
+
+        RuleEngineAlarmService alarmService = mock(RuleEngineAlarmService.class);
+        when(alarmService.findLatestByOriginatorAndType(any(), any(), any())).thenReturn(Futures.immediateFuture(null));
+        when(alarmService.createOrUpdateAlarm(any())).thenAnswer(invocationOnMock -> {
+            Alarm alarm = invocationOnMock.getArgument(0);
+            alarm.setId(new AlarmId(UUID.randomUUID()));
+            return alarm;
+        });
+        when(ctx.getAlarmService()).thenReturn(alarmService);
+
+        tenantId = new TenantId(UUID.randomUUID());
+
+    }
+
+    @Test
+    public void whenAttributeIsDeleted_thenUnneededAlarmRulesAreNotReevaluated() throws Exception {
+        AlarmRule alarmRule = createAlarmRule(createAlarmConfigWithBoolAttrCondition("enabled", false));
+        DeviceId deviceId = new DeviceId(UUID.randomUUID());
+        EntityState deviceState = createEntityState(deviceId, alarmRule);
+
+        TbMsg attributeUpdateMsg = TbMsg.newMsg(SessionMsgType.POST_ATTRIBUTES_REQUEST.name(),
+                deviceId, new TbMsgMetaData(), "{ \"enabled\": false }");
+
+        deviceState.process(null, attributeUpdateMsg);
+
+        ArgumentCaptor<TbMsg> resultMsgCaptor = ArgumentCaptor.forClass(TbMsg.class);
+        Mockito.verify(clusterService).pushMsgToRuleEngine(eq(tenantId), eq(deviceId), resultMsgCaptor.capture(), eq(Collections.singleton("Alarm Created")), any());
+        Alarm alarm = JacksonUtil.fromString(resultMsgCaptor.getValue().getData(), Alarm.class);
+
+        deviceState.process(null, TbMsg.newMsg(DataConstants.ALARM_CLEAR, deviceId, new TbMsgMetaData(), JacksonUtil.toString(alarm)));
+        reset(clusterService);
+
+        String deletedAttributes = "{ \"attributes\": [ \"other\" ] }";
+        deviceState.process(null, TbMsg.newMsg(DataConstants.ATTRIBUTES_DELETED, deviceId, new TbMsgMetaData(), deletedAttributes));
+        verify(clusterService, never()).pushMsgToRuleEngine(any(), any(), any(), any(), any());
+    }
+
+    @Test
+    public void whenDeletingClearedAlarm_thenNoError() throws Exception {
+        AlarmRule alarmRule = createAlarmRule(createAlarmConfigWithBoolAttrCondition("enabled", false));
+        DeviceId deviceId = new DeviceId(UUID.randomUUID());
+        EntityState deviceState = createEntityState(deviceId, alarmRule);
+
+        TbMsg attributeUpdateMsg = TbMsg.newMsg(SessionMsgType.POST_ATTRIBUTES_REQUEST.name(),
+                deviceId, new TbMsgMetaData(), "{ \"enabled\": false }");
+
+        deviceState.process(null, attributeUpdateMsg);
+        ArgumentCaptor<TbMsg> resultMsgCaptor = ArgumentCaptor.forClass(TbMsg.class);
+        Mockito.verify(clusterService).pushMsgToRuleEngine(eq(tenantId), eq(deviceId), resultMsgCaptor.capture(), eq(Collections.singleton("Alarm Created")), any());
+        Alarm alarm = JacksonUtil.fromString(resultMsgCaptor.getValue().getData(), Alarm.class);
+
+        deviceState.process(null, TbMsg.newMsg(DataConstants.ALARM_CLEAR, deviceId, new TbMsgMetaData(), JacksonUtil.toString(alarm)));
+
+        TbMsg alarmDeleteNotification = TbMsg.newMsg(DataConstants.ALARM_DELETE, deviceId, new TbMsgMetaData(), JacksonUtil.toString(alarm));
+        assertDoesNotThrow(() -> {
+            deviceState.process(null, alarmDeleteNotification);
+        });
+    }
+
+    private EntityState createEntityState(DeviceId deviceId, AlarmRule... rules) {
+        EntityRulesState deviceRuleState = new EntityRulesState(List.of(rules));
+
+        return new EntityState(tenantId, deviceId, null, ctx, deviceRuleState, null);
+    }
+
+    private AlarmRule createAlarmRule(AlarmRuleConfiguration alarmRuleConfiguration) {
+        AlarmRule alarmRule = new AlarmRule(new AlarmRuleId(UUID.randomUUID()));
+        alarmRule.setAlarmType("MyAlarm");
+        alarmRule.setConfiguration(alarmRuleConfiguration);
+        return alarmRule;
+    }
+
+    private AlarmRuleConfiguration createAlarmConfigWithBoolAttrCondition(String key, boolean value) {
+
+        AlarmConditionFilter condition = new AlarmConditionFilter();
+        condition.setKey(new AlarmConditionFilterKey(AlarmConditionKeyType.ATTRIBUTE, key));
+        condition.setValueType(EntityKeyValueType.BOOLEAN);
+        BooleanFilterPredicate predicate = new BooleanFilterPredicate();
+        predicate.setOperation(BooleanFilterPredicate.BooleanOperation.EQUAL);
+        predicate.setValue(new FilterPredicateValue<>(value));
+        condition.setPredicate(predicate);
+
+        AlarmRuleConfiguration alarmConfig = new AlarmRuleConfiguration();
+        AlarmRuleCondition alarmRule = new AlarmRuleCondition();
+        AlarmCondition alarmCondition = new AlarmCondition();
+        alarmCondition.setSpec(new SimpleAlarmConditionSpec());
+        alarmCondition.setCondition(List.of(condition));
+        alarmRule.setCondition(alarmCondition);
+        alarmConfig.setCreateRules(new TreeMap<>(Map.of(AlarmSeverity.CRITICAL, alarmRule)));
+
+        alarmConfig.setSourceEntityFilters(Collections.singletonList(new AlarmRuleAllDevicesEntityFilter()));
+        alarmConfig.setAlarmTargetEntity(new AlarmRuleOriginatorTargetEntity());
+
+        return alarmConfig;
+    }
 
 }
