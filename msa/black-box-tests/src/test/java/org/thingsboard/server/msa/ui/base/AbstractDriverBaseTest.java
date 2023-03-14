16061ec4fcf27e355ae22444676de8318d4e0b65
Seraphym-Tuhai <stuhai@thingsboard.io>
2023-03-14 11:48:10 +0200

    rename openLocalhost() method

--- msa/black-box-tests/src/test/java/org/thingsboard/server/msa/ui/base/AbstractDriverBaseTest.java
+++ msa/black-box-tests/src/test/java/org/thingsboard/server/msa/ui/base/AbstractDriverBaseTest.java
@@ -81,12 +81,12 @@ abstract public class AbstractDriverBaseTest extends AbstractContainerTest {
             driver = new ChromeDriver(options);
         }
         driver.manage().window().setSize(dimension);
-        openLocalhost();
+        openBaseUiUrl();
     }
 
     @BeforeMethod
     public void open() {
-        openLocalhost();
+        openBaseUiUrl();
     }
 
     @AfterMethod
@@ -105,7 +105,7 @@ abstract public class AbstractDriverBaseTest extends AbstractContainerTest {
         return (String) js.executeScript("return window.localStorage.getItem('jwt_token');");
     }
 
-    public void openLocalhost() {
+    public void openBaseUiUrl() {
         driver.get(getBaseUiUrl());
     }
 
