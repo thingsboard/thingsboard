fff7c3fd3d8da45e12c2d01cbdf5e2276ad242ed
Sergey Matvienko <smatvienko@thingsboard.io>
2023-03-28 18:22:59 +0200

    CacheEvictEvents made public (6 of 12 was package-private), removed duplicated lombok annotations

--- dao/src/main/java/org/thingsboard/server/dao/edge/EdgeCacheEvictEvent.java
+++ dao/src/main/java/org/thingsboard/server/dao/edge/EdgeCacheEvictEvent.java
@@ -16,12 +16,10 @@
 package org.thingsboard.server.dao.edge;
 
 import lombok.Data;
-import lombok.RequiredArgsConstructor;
 import org.thingsboard.server.common.data.id.TenantId;
 
 @Data
-@RequiredArgsConstructor
-class EdgeCacheEvictEvent {
+public class EdgeCacheEvictEvent {
 
     private final TenantId tenantId;
     private final String newName;
--- dao/src/main/java/org/thingsboard/server/dao/ota/OtaPackageCacheEvictEvent.java
+++ dao/src/main/java/org/thingsboard/server/dao/ota/OtaPackageCacheEvictEvent.java
@@ -16,12 +16,10 @@
 package org.thingsboard.server.dao.ota;
 
 import lombok.Data;
-import lombok.RequiredArgsConstructor;
 import org.thingsboard.server.common.data.id.OtaPackageId;
 
 @Data
-@RequiredArgsConstructor
-class OtaPackageCacheEvictEvent {
+public class OtaPackageCacheEvictEvent {
 
     private final OtaPackageId id;
 
