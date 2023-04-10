408761870212173c796ad63b1372155c2ce771d1
Seraphym-Tuhai <stuhai@thingsboard.io>
2023-04-10 13:23:26 +0300

    back removed import DockerClientFactory

--- msa/black-box-tests/src/test/java/org/thingsboard/server/msa/TestProperties.java
+++ msa/black-box-tests/src/test/java/org/thingsboard/server/msa/TestProperties.java
@@ -16,6 +16,7 @@
 package org.thingsboard.server.msa;
 
 import lombok.extern.slf4j.Slf4j;
+import org.testcontainers.DockerClientFactory;
 
 @Slf4j
 public class TestProperties {
@@ -35,7 +36,7 @@ public class TestProperties {
 
     public static String getBaseUiUrl() {
         if (instance.isActive()) {
-            //return "https://host.docker.internal" // this alternative requires docker-selenium.yml extra_hosts: - "host.docker.internal:host-gateway"
+            //return "https://host.docker.internal"; // this alternative requires docker-selenium.yml extra_hosts: - "host.docker.internal:host-gateway"
             //return "https://" + DockerClientFactory.instance().dockerHostIpAddress(); //this alternative will get Docker IP from testcontainers
             return "https://haproxy"; //communicate inside current docker-compose network to the load balancer container
         }
