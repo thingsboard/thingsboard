1a3ee8512d58625940b25e46bc6488a3539fdc5e
Andrii Dovhopoliy <109090830+adovh@users.noreply.github.com>
2023-04-12 00:31:37 +0300

    Fixed xss vulnerabilities in attributes and telemetry (#8238)
    * added noxss validation on kventries
    
    * added noxss validation on kventries
    
    * added ConstraintValidator usages for validation
    
    * added test
    
    * upd
    
    * removed star imports
    
    * fixed licence
    
    * removed NoXss Annotation from values
    
    * fixed test
    
    * added test
    
    * removed redundant imports
    
    * fixed test
    
    * upd
    
    * fixed telemetry test controller structure
    
    * tskv noxss validation improvements
    
    * upd test attributes
    
    ---------
    
    Co-authored-by: YevhenBondarenko <ybondarenko@thingsboard.io>

---
+++ dao/src/test/java/org/thingsboard/server/dao/service/ConstraintValidatorTest.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright © 2016-2023 The Thingsboard Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.thingsboard.server.dao.service;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.thingsboard.server.common.data.kv.JsonDataEntry;
+import org.thingsboard.server.common.data.kv.StringDataEntry;
+import org.thingsboard.server.dao.exception.DataValidationException;
+
+class ConstraintValidatorTest {
+
+    @Test
+    void validateFields() {
+        StringDataEntry stringDataEntryValid = new StringDataEntry("key", "value");
+
+        StringDataEntry stringDataEntryInvalid1 = new StringDataEntry("<object type=\"text/html\"><script>alert(document)</script></object>", "value");
+        StringDataEntry stringDataEntryInvalid2 = new StringDataEntry("key", "<object type=\"text/html\"><script>alert(document)</script></object>");
+
+        JsonDataEntry jsonDataEntryInvalid = new JsonDataEntry("key", "{\"value\": <object type=\"text/html\"><script>alert(document)</script></object>}");
+
+        Assert.assertThrows(DataValidationException.class, () -> ConstraintValidator.validateFields(stringDataEntryInvalid1));
+//        Assert.assertThrows(DataValidationException.class, () -> ConstraintValidator.validateFields(stringDataEntryInvalid2));
+//        Assert.assertThrows(DataValidationException.class, () -> ConstraintValidator.validateFields(jsonDataEntryInvalid));
+        ConstraintValidator.validateFields(stringDataEntryValid);
+    }
+}
