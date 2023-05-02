510cd8f8976ce240ca3744ee8772a45f2a8f36b2
Artem Dzhereleiko <Dgerik2015@gmail.com>
2023-05-02 11:14:11 +0300

    UI: change alarm menu icon

--- ui-ngx/src/app/modules/home/pages/alarm/alarm-routing.module.ts
+++ ui-ngx/src/app/modules/home/pages/alarm/alarm-routing.module.ts
@@ -42,7 +42,7 @@ const routes: Routes = [
       title: 'alarm.alarms',
       breadcrumb: {
         label: 'alarm.alarms',
-        icon: 'notifications'
+        icon: 'mdi:alert-outline'
       },
       isPage: true,
       alarmsMode: AlarmsMode.ALL
