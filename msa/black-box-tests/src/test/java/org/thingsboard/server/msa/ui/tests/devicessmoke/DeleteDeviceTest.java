149e3fb8ed37a28d2c8156f46890e28485892077
Seraphym-Tuhai <stuhai@thingsboard.io>
2023-05-03 16:05:16 +0300

    added tests on create and delete device

---
+++ msa/black-box-tests/src/test/java/org/thingsboard/server/msa/ui/tests/devicessmoke/DeleteDeviceTest.java
@@ -0,0 +1,59 @@
+/**
+ * Copyright © 2016-2023 The Thingsboard Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.thingsboard.server.msa.ui.tests.devicessmoke;
+
+import io.qameta.allure.Feature;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.thingsboard.server.common.data.Device;
+import org.thingsboard.server.msa.ui.utils.EntityPrototypes;
+
+import static org.thingsboard.server.msa.ui.utils.Const.ENTITY_NAME;
+
+@Feature("Delete device")
+public class DeleteDeviceTest extends AbstractDeviceTest {
+
+    @BeforeMethod
+    public void createDevice() {
+        Device device = testRestClient.postDevice("", EntityPrototypes.defaultDevicePrototype(ENTITY_NAME));
+        deviceName = device.getName();
+    }
+
+    @Test(groups = "smoke")
+    public void deleteDeviceByRightSideBtn() {
+        sideBarMenuView.devicesBtn().click();
+        devicePage.deleteDeviceByRightSideBtn(deviceName);
+
+        devicePage.assertEntityIsNotPresent(deviceName);
+    }
+
+    @Test(groups = "smoke")
+    public void deleteSelectedDevice() {
+        sideBarMenuView.devicesBtn().click();
+        devicePage.deleteSelected(deviceName);
+
+        devicePage.assertEntityIsNotPresent(deviceName);
+    }
+
+    @Test(groups = "smoke")
+    public void deleteDeviceFromDetailsTab() {
+        sideBarMenuView.devicesBtn().click();
+        devicePage.entity(deviceName).click();
+        devicePage.deleteDeviceFromDetailsTab();
+
+        devicePage.assertEntityIsNotPresent(deviceName);
+    }
+}
