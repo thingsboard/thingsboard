333159373d39ae084c6de0629380148df6a8e842
Sergey Matvienko <smatvienko@thingsboard.io>
2023-05-17 10:32:30 +0200

    removed unused import org.springframework.data.jpa.repository.query.Procedure;

--- dao/src/main/java/org/thingsboard/server/dao/sql/alarm/AlarmRepository.java
+++ dao/src/main/java/org/thingsboard/server/dao/sql/alarm/AlarmRepository.java
@@ -19,7 +19,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.jpa.repository.query.Procedure;
 import org.springframework.data.repository.query.Param;
 import org.thingsboard.server.common.data.alarm.AlarmSeverity;
 import org.thingsboard.server.dao.model.sql.AlarmEntity;
