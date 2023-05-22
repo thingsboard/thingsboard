916d5d80b5ae9ac030acf6e1c88e21e04713f677
Andrii Shvaika <ashvayka@thingsboard.io>
2023-05-22 13:27:58 +0300

    Fix 3.5.1 update script location

--- application/src/main/java/org/thingsboard/server/service/install/SqlDatabaseUpgradeService.java
+++ application/src/main/java/org/thingsboard/server/service/install/SqlDatabaseUpgradeService.java
@@ -718,7 +718,7 @@ public class SqlDatabaseUpgradeService implements DatabaseEntitiesUpgradeService
                 try (Connection conn = DriverManager.getConnection(dbUrl, dbUserName, dbPassword)) {
                     log.info("Updating schema ...");
                     if (isOldSchema(conn, 3005000)) {
-                        schemaUpdateFile = Paths.get(installScripts.getDataDir(), "upgrade", "3.5.2", SCHEMA_UPDATE_SQL);
+                        schemaUpdateFile = Paths.get(installScripts.getDataDir(), "upgrade", "3.5.1", SCHEMA_UPDATE_SQL);
                         loadSql(schemaUpdateFile, conn);
                         conn.createStatement().execute("UPDATE tb_schema_settings SET schema_version = 3005002;");
                     }
