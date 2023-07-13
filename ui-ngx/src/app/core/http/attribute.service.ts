ba7d134de73181da2a6d26df98e2f073de8ddbd0
rusikv <rvasylkiv@thingsboard.io>
2023-07-13 20:15:25 +0300

    added rewrite param to delete latest timeseries, enabled single selection deletion

--- ui-ngx/src/app/core/http/attribute.service.ts
+++ ui-ngx/src/app/core/http/attribute.service.ts
@@ -50,7 +50,7 @@ export class AttributeService {
   }
 
   public deleteEntityTimeseries(entityId: EntityId, timeseries: Array<AttributeData>, deleteAllDataForKeys = false,
-                                startTs?: number, endTs?: number, rewriteLatestIfDeleted = false, deleteLatest = false,
+                                startTs?: number, endTs?: number, rewriteLatestIfDeleted = false, deleteLatest = true,
                                 config?: RequestConfig): Observable<any> {
     const keys = timeseries.map(attribute => encodeURIComponent(attribute.key)).join(',');
     let url = `/api/plugins/telemetry/${entityId.entityType}/${entityId.id}/timeseries/delete` +
@@ -64,9 +64,11 @@ export class AttributeService {
     return this.http.delete(url, defaultHttpOptionsFromConfig(config));
   }
 
-  public deleteEntityLatestTimeseries(entityId: EntityId, timeseries: Array<AttributeData>, config?: RequestConfig): Observable<any> {
+  public deleteEntityLatestTimeseries(entityId: EntityId, timeseries: Array<AttributeData>, rewrite = true,
+                                      config?: RequestConfig): Observable<any> {
     const keys = timeseries.map(attribute => encodeURIComponent(attribute.key)).join(',');
-    let url = `/api/plugins/telemetry/${entityId.entityType}/${entityId.id}/timeseries/latest/delete?keys=${keys}`;
+    let url = `/api/plugins/telemetry/${entityId.entityType}/${entityId.id}/timeseries/latest/delete?keys=${keys}` +
+        `$rewrite=${rewrite}`;
     return this.http.delete(url, defaultHttpOptionsFromConfig(config));
   }
 
@@ -111,7 +113,7 @@ export class AttributeService {
     let deleteEntityTimeseriesObservable: Observable<any>;
     if (deleteTimeseries.length) {
       deleteEntityTimeseriesObservable = this.deleteEntityTimeseries(entityId, deleteTimeseries, true,
-        null, null, false, false, config);
+        null, null, false, true, config);
     } else {
       deleteEntityTimeseriesObservable = of(null);
     }
