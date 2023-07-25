83d525aa92e3a16903ca0067fc9b7795aa0a032d
Vladyslav_Prykhodko <vprykhodko@thingsboard.io>
2023-07-25 10:35:14 +0300

    UI: Clear code after merge

--- ui-ngx/src/app/app.component.ts
+++ ui-ngx/src/app/app.component.ts
@@ -30,7 +30,7 @@ import { combineLatest } from 'rxjs';
 import { selectIsAuthenticated, selectIsUserLoaded } from '@core/auth/auth.selectors';
 import { distinctUntilChanged, filter, map, skip } from 'rxjs/operators';
 import { AuthService } from '@core/auth/auth.service';
-import { svgIcons } from '@shared/models/icon.models';
+import { svgIcons, svgIconsUrl } from '@shared/models/icon.models';
 
 @Component({
   selector: 'tb-root',
@@ -65,10 +65,9 @@ export class AppComponent implements OnInit {
       );
     }
 
-    this.matIconRegistry.addSvgIcon('windows', this.domSanitizer.bypassSecurityTrustResourceUrl('/assets/windows.svg'));
-    this.matIconRegistry.addSvgIcon('macos', this.domSanitizer.bypassSecurityTrustResourceUrl('/assets/macos.svg'));
-    this.matIconRegistry.addSvgIcon('linux', this.domSanitizer.bypassSecurityTrustResourceUrl('/assets/linux.svg'));
-    this.matIconRegistry.addSvgIcon('docker', this.domSanitizer.bypassSecurityTrustResourceUrl('/assets/docker.svg'));
+    for (const svgIcon of Object.keys(svgIconsUrl)) {
+      this.matIconRegistry.addSvgIcon(svgIcon, this.domSanitizer.bypassSecurityTrustResourceUrl(svgIcons[svgIcon]));
+    }
 
     this.storageService.testLocalStorage();
 
