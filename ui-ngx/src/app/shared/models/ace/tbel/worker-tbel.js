9c57108a595684e20030a8bebb11ae3d9f980f5f
nick <nick@avalr.com.ua>
2023-08-16 07:37:55 +0300

    tbel:  add break to for, foreach

--- ui-ngx/src/app/shared/models/ace/tbel/worker-tbel.js
+++ ui-ngx/src/app/shared/models/ace/tbel/worker-tbel.js
@@ -4656,6 +4656,7 @@ var JSHINT = (function() {
       case "else":
       case "finally":
       case "for":
+      case "foreach":
       case "if":
       case "in":
       case "instanceof":
@@ -8135,7 +8136,7 @@ var JSHINT = (function() {
       state.funct["(breakage)"] += 1;
       state.funct["(loopage)"] += 1;
 
-      state.funct["(breakage)"] -= 1;
+      // state.funct["(breakage)"] -= 1;
       state.funct["(loopage)"] -= 1;
     } else {
       nolinebreak(state.tokens.curr);
@@ -9755,7 +9756,7 @@ Lexer.prototype = {
   scanKeyword: function() {
     var result = /^[a-zA-Z_$][a-zA-Z0-9_$]*/.exec(this.input);
     var keywords = [
-      "if", "in", "do", "var", "for", "new",
+      "if", "in", "do", "var", "for", "foreach", "new",
       "try", "let", "this", "else", "case",
       "void", "with", "enum", "while", "break",
       "catch", "throw", "const", "yield", "class",
