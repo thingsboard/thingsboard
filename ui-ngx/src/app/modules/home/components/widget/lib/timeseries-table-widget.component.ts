5fcc06e028fbd80b5cf226e1d9e5ca7fccee5306
Artem Dzhereleiko <Dgerik2015@gmail.com>
2023-08-18 11:57:36 +0300

    UI: get columns title by label

--- ui-ngx/src/app/modules/home/components/widget/lib/timeseries-table-widget.component.ts
+++ ui-ngx/src/app/modules/home/components/widget/lib/timeseries-table-widget.component.ts
@@ -460,7 +460,7 @@ export class TimeseriesTableWidgetComponent extends PageComponent implements OnI
         } else if (value === 'actions') {
           title = 'Actions';
         } else {
-          title = header.dataKey.name;
+          title = header.dataKey.label;
         }
         return {
           title,
