a59b6b5921d0a67438987aab90fbe8fb7426247c
dashevchenko <dshevchenko@thingsboard.io>
2023-08-21 17:32:12 +0300

    fixed testConnection method for mail sender if oauth2 enabled

--- application/src/test/java/org/thingsboard/server/service/state/DefaultDeviceStateServiceTest.java
+++ application/src/test/java/org/thingsboard/server/service/state/DefaultDeviceStateServiceTest.java
@@ -42,6 +42,7 @@ import org.thingsboard.server.dao.timeseries.TimeseriesService;
 import org.thingsboard.server.queue.discovery.PartitionService;
 import org.thingsboard.server.queue.discovery.QueueKey;
 import org.thingsboard.server.queue.discovery.event.PartitionChangeEvent;
+import org.thingsboard.server.queue.usagestats.DefaultTbApiUsageReportClient;
 import org.thingsboard.server.service.telemetry.TelemetrySubscriptionService;
 
 import java.util.Collections;
@@ -86,7 +87,7 @@ public class DefaultDeviceStateServiceTest {
 
     @Before
     public void setUp() {
-        service = spy(new DefaultDeviceStateService(deviceService, attributesService, tsService, clusterService, partitionService, entityQueryRepository, null, null, mock(NotificationRuleProcessor.class)));
+        service = spy(new DefaultDeviceStateService(deviceService, attributesService, tsService, clusterService, partitionService, entityQueryRepository, null, mock(DefaultTbApiUsageReportClient.class), mock(NotificationRuleProcessor.class)));
         telemetrySubscriptionService = Mockito.mock(TelemetrySubscriptionService.class);
         ReflectionTestUtils.setField(service, "tsSubService", telemetrySubscriptionService);
         ReflectionTestUtils.setField(service, "defaultStateCheckIntervalInSec", 60);
@@ -225,7 +226,7 @@ public class DefaultDeviceStateServiceTest {
 
         Mockito.reset(telemetrySubscriptionService);
 
-        long increase = 100;
+        long increase = 1000;
         long newTimeout = System.currentTimeMillis() - deviceState.getLastActivityTime() + increase;
 
         service.onDeviceInactivityTimeoutUpdate(tenantId, deviceId, newTimeout);
@@ -238,6 +239,8 @@ public class DefaultDeviceStateServiceTest {
 
         service.onDeviceActivity(tenantId, deviceId, System.currentTimeMillis());
         activityVerify(true);
+
+        Mockito.reset(telemetrySubscriptionService);
         Thread.sleep(newTimeout);
         service.checkStates();
         activityVerify(false);
