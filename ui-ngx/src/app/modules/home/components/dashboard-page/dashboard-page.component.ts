7f5b74d34c8257a6e7b49c7964e4410bb5db2149
Vladyslav_Prykhodko <vprykhodko@thingsboard.io>
2023-08-25 15:43:58 +0300

    UI: Fixed show exit dialog twice

--- ui-ngx/src/app/modules/home/components/dashboard-page/dashboard-page.component.ts
+++ ui-ngx/src/app/modules/home/components/dashboard-page/dashboard-page.component.ts
@@ -161,12 +161,14 @@ import { HasDirtyFlag } from '@core/guards/confirm-on-exit.guard';
 })
 export class DashboardPageComponent extends PageComponent implements IDashboardController, HasDirtyFlag, OnInit, AfterViewInit, OnDestroy {
 
+  private forcePristine = true;
+
   get isDirty(): boolean {
-    return this.isEdit;
+    return this.isEdit && this.forcePristine;
   }
 
   set isDirty(value: boolean) {
-
+    this.forcePristine = value;
   }
 
   authState: AuthState = getCurrentAuthState(this.store);
