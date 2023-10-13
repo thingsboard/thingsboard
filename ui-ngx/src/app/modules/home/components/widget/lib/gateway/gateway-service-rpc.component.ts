5be35519545d0394fbdfb0add6a5f784526770e2
Maksym Dudnik <dudnikmaksim@gmail.com>
2023-10-13 14:11:58 +0300

    rpc params hotfix

--- ui-ngx/src/app/modules/home/components/widget/lib/gateway/gateway-service-rpc.component.ts
+++ ui-ngx/src/app/modules/home/components/widget/lib/gateway/gateway-service-rpc.component.ts
@@ -16,14 +16,8 @@
 
 import { AfterViewInit, Component, Input } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { WidgetContext } from '@home/models/widget-component.models';
 import { ContentType } from '@shared/models/constants';
-import {
-  JsonObjectEditDialogComponent,
-  JsonObjectEditDialogData
-} from '@shared/components/dialog/json-object-edit-dialog.component';
-import { jsonRequired } from '@shared/components/json-object-edit.component';
 
 @Component({
   selector: 'tb-gateway-service-rpc',
@@ -36,9 +30,6 @@ export class GatewayServiceRPCComponent implements AfterViewInit {
   ctx: WidgetContext;
 
   contentTypes = ContentType;
-
-  @Input()
-  dialogRef: MatDialogRef<any>;
 
   commandForm: FormGroup;
 
@@ -56,12 +47,11 @@ export class GatewayServiceRPCComponent implements AfterViewInit {
 
   private connectorType: string;
 
-  constructor(private fb: FormBuilder,
-              private dialog: MatDialog) {
+  constructor(private fb: FormBuilder) {
     this.commandForm = this.fb.group({
       command: [null,[Validators.required]],
       time: [60, [Validators.required, Validators.min(1)]],
-      params: [{}, [jsonRequired]],
+      params: [""],
       result: [null]
     });
   }
@@ -71,6 +61,7 @@ export class GatewayServiceRPCComponent implements AfterViewInit {
     if (!this.isConnector) {
       this.commandForm.get('command').setValue(this.RPCCommands[0]);
     } else {
+      this.commandForm.get('params').addValidators(Validators.required);
       this.connectorType = this.ctx.stateController.getStateParams().connector_rpc.value.type;
     }
   }
@@ -78,32 +69,12 @@ export class GatewayServiceRPCComponent implements AfterViewInit {
   sendCommand() {
     const formValues = this.commandForm.value;
     const commandPrefix = this.isConnector ? `${this.connectorType}_` : 'gateway_';
-    this.ctx.controlApi.sendTwoWayCommand(commandPrefix+formValues.command.toLowerCase(), formValues.params,formValues.time).subscribe({
+    this.ctx.controlApi.sendTwoWayCommand(commandPrefix+formValues.command.toLowerCase(), formValues.params, formValues.time).subscribe({
       next: resp => this.commandForm.get('result').setValue(JSON.stringify(resp)),
       error: error => {
         console.log(error);
         this.commandForm.get('result').setValue(JSON.stringify(error.error));
       }
     });
-  }
-
-  openEditJSONDialog($event: Event) {
-    if ($event) {
-      $event.stopPropagation();
-    }
-    this.dialog.open<JsonObjectEditDialogComponent, JsonObjectEditDialogData, object>(JsonObjectEditDialogComponent, {
-      disableClose: true,
-      panelClass: ['tb-dialog', 'tb-fullscreen-dialog'],
-      data: {
-        jsonValue: JSON.parse(this.commandForm.get('params').value),
-        required: true
-      }
-    }).afterClosed().subscribe(
-      (res) => {
-        if (res) {
-          this.commandForm.get('params').setValue(JSON.stringify(res));
-        }
-      }
-    );
   }
 }
