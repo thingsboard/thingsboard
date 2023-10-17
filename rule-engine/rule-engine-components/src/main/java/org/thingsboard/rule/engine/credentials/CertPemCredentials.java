cf32544e71f7dd3bd43059fd2b4328c1a3747dc2
YevhenBondarenko <ybondarenko@thingsboard.io>
2023-10-17 23:20:00 +0200

    added SslUtil and tests

--- rule-engine/rule-engine-components/src/main/java/org/thingsboard/rule/engine/credentials/CertPemCredentials.java
+++ rule-engine/rule-engine-components/src/main/java/org/thingsboard/rule/engine/credentials/CertPemCredentials.java
@@ -20,35 +20,17 @@ import io.netty.handler.ssl.SslContext;
 import io.netty.handler.ssl.SslContextBuilder;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
-import org.bouncycastle.asn1.pkcs.PrivateKeyInfo;
-import org.bouncycastle.cert.X509CertificateHolder;
-import org.bouncycastle.cert.jcajce.JcaX509CertificateConverter;
-import org.bouncycastle.jce.provider.BouncyCastleProvider;
-import org.bouncycastle.openssl.PEMDecryptorProvider;
-import org.bouncycastle.openssl.PEMEncryptedKeyPair;
-import org.bouncycastle.openssl.PEMKeyPair;
-import org.bouncycastle.openssl.PEMParser;
-import org.bouncycastle.openssl.jcajce.JcaPEMKeyConverter;
-import org.bouncycastle.openssl.jcajce.JcePEMDecryptorProviderBuilder;
-import org.bouncycastle.operator.InputDecryptorProvider;
-import org.bouncycastle.pkcs.PKCS8EncryptedPrivateKeyInfo;
-import org.bouncycastle.pkcs.PKCSException;
-import org.bouncycastle.pkcs.jcajce.JcePKCSPBEInputDecryptorProviderBuilder;
+import org.thingsboard.common.util.SslUtil;
 import org.thingsboard.server.common.data.StringUtils;
 
 import javax.net.ssl.KeyManagerFactory;
 import javax.net.ssl.TrustManagerFactory;
-import java.io.IOException;
-import java.io.StringReader;
-import java.security.GeneralSecurityException;
 import java.security.KeyStore;
 import java.security.PrivateKey;
-import java.security.Security;
 import java.security.cert.CertPath;
 import java.security.cert.Certificate;
 import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -61,16 +43,11 @@ public class CertPemCredentials implements ClientCredentials {
     public static final String X_509 = "X.509";
     public static final String CERT_ALIAS_PREFIX = "cert-";
     public static final String CA_CERT_CERT_ALIAS_PREFIX = "caCert-cert-";
+
     protected String caCert;
     private String cert;
     private String privateKey;
-    private String password;
-
-    public CertPemCredentials() {
-        if (Security.getProvider(BouncyCastleProvider.PROVIDER_NAME) == null) {
-            Security.addProvider(new BouncyCastleProvider());
-        }
-    }
+    private String password = "";
 
     @Override
     public CredentialsType getType() {
@@ -95,7 +72,7 @@ public class CertPemCredentials implements ClientCredentials {
     }
 
     protected TrustManagerFactory createAndInitTrustManagerFactory() throws Exception {
-        List<X509Certificate> caCerts = readCertFile(caCert);
+        List<X509Certificate> caCerts = SslUtil.readCertFile(caCert);
 
         KeyStore caKeyStore = KeyStore.getInstance(KeyStore.getDefaultType());
         caKeyStore.load(null, null);
@@ -108,15 +85,15 @@ public class CertPemCredentials implements ClientCredentials {
         return trustManagerFactory;
     }
 
-    protected KeyManagerFactory createAndInitKeyManagerFactory() throws Exception {
+    private KeyManagerFactory createAndInitKeyManagerFactory() throws Exception {
         KeyManagerFactory kmf = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
         kmf.init(loadKeyStore(), password.toCharArray());
         return kmf;
     }
 
-    private KeyStore loadKeyStore() throws Exception {
-        List<X509Certificate> certificates = readCertFile(this.cert);
-        PrivateKey privateKey = readPrivateKey(this.privateKey, this.password);
+    protected KeyStore loadKeyStore() throws Exception {
+        List<X509Certificate> certificates = SslUtil.readCertFile(this.cert);
+        PrivateKey privateKey = SslUtil.readPrivateKey(this.privateKey, password);
 
         KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
         keyStore.load(null);
@@ -135,47 +112,4 @@ public class CertPemCredentials implements ClientCredentials {
         return keyStore;
     }
 
-    protected List<X509Certificate> readCertFile(String fileContent) throws IOException, GeneralSecurityException {
-        List<X509Certificate> certificates = new ArrayList<>();
-        JcaX509CertificateConverter certConverter = new JcaX509CertificateConverter();
-        try (PEMParser pemParser = new PEMParser(new StringReader(fileContent))) {
-            Object object;
-            while ((object = pemParser.readObject()) != null) {
-                if (object instanceof X509CertificateHolder) {
-                    X509Certificate x509Cert = certConverter.getCertificate((X509CertificateHolder) object);
-                    certificates.add(x509Cert);
-                }
-            }
-        }
-        return certificates;
-    }
-
-    protected PrivateKey readPrivateKey(String fileContent, String password) throws IOException, PKCSException {
-        PrivateKey privateKey = null;
-        JcaPEMKeyConverter keyConverter = new JcaPEMKeyConverter();
-
-        if (StringUtils.isNotEmpty(fileContent)) {
-            try (PEMParser pemParser = new PEMParser(new StringReader(fileContent))) {
-                Object object;
-                while ((object = pemParser.readObject()) != null) {
-                    if (object instanceof PEMEncryptedKeyPair) {
-                        PEMDecryptorProvider decProv = new JcePEMDecryptorProviderBuilder().build(password.toCharArray());
-                        privateKey = keyConverter.getKeyPair(((PEMEncryptedKeyPair) object).decryptKeyPair(decProv)).getPrivate();
-                        break;
-                    } else if (object instanceof PKCS8EncryptedPrivateKeyInfo) {
-                        InputDecryptorProvider decProv =
-                                new JcePKCSPBEInputDecryptorProviderBuilder().setProvider(new BouncyCastleProvider()).build(password.toCharArray());
-                        privateKey = keyConverter.getPrivateKey(((PKCS8EncryptedPrivateKeyInfo) object).decryptPrivateKeyInfo(decProv));
-                        break;
-                    } else if (object instanceof PEMKeyPair) {
-                        privateKey = keyConverter.getKeyPair((PEMKeyPair) object).getPrivate();
-                        break;
-                    } else if (object instanceof PrivateKeyInfo) {
-                        privateKey = keyConverter.getPrivateKey((PrivateKeyInfo) object);
-                    }
-                }
-            }
-        }
-        return privateKey;
-    }
 }
