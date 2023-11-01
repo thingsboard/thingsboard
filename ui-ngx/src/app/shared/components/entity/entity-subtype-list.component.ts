900bb9a57ca4ea284f5fe7eb8099cf22ef3d0f6f
Artem Dzhereleiko <Dgerik2015@gmail.com>
2023-11-01 12:28:24 +0200

    UI: Refactoring entity subtype list

--- ui-ngx/src/app/shared/components/entity/entity-subtype-list.component.ts
+++ ui-ngx/src/app/shared/components/entity/entity-subtype-list.component.ts
@@ -33,6 +33,7 @@ import { FloatLabelType, MatFormFieldAppearance, SubscriptSizing } from '@angula
 import { coerceArray, coerceBoolean } from '@shared/decorators/coercion';
 import { PageLink } from '@shared/models/page/page-link';
 import { PageData } from '@shared/models/page/page-data';
+import { UtilsService } from '@core/services/utils.service';
 
 @Component({
   selector: 'tb-entity-subtype-list',
@@ -129,6 +130,7 @@ export class EntitySubTypeListComponent implements ControlValueAccessor, OnInit,
               private edgeService: EdgeService,
               private entityViewService: EntityViewService,
               private alarmService: AlarmService,
+              private utils: UtilsService,
               private fb: FormBuilder) {
     this.entitySubtypeListFormGroup = this.fb.group({
       entitySubtypeList: [this.entitySubtypeList, this.required ? [Validators.required] : []],
@@ -370,6 +372,10 @@ export class EntitySubTypeListComponent implements ControlValueAccessor, OnInit,
       this.entitySubtypeInput.nativeElement.blur();
       this.entitySubtypeInput.nativeElement.focus();
     }, 0);
+  }
+
+  customTranslate(entity: string) {
+    return this.utils.customTranslation(entity, entity);
   }
 
 }
