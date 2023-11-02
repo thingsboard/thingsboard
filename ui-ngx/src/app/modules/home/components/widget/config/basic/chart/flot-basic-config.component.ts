bcf23f3f9633ef5a2a07ab6ed7c0c62f44aef75b
rusikv <rvasylkiv@thingsboard.io>
2023-11-02 18:08:15 +0200

    UI: Fix for datasources component validation in basic mode

--- ui-ngx/src/app/modules/home/components/widget/config/basic/chart/flot-basic-config.component.ts
+++ ui-ngx/src/app/modules/home/components/widget/config/basic/chart/flot-basic-config.component.ts
@@ -82,8 +82,6 @@ export class FlotBasicConfigComponent extends BasicWidgetConfigComponent {
       legendConfig: [configData.config.settings?.legendConfig, []],
       actions: [configData.config.actions || {}, []]
     });
-    this.flotWidgetConfigForm.get('series').valueChanges
-      .subscribe(() => this.flotWidgetConfigForm.get('datasources').updateValueAndValidity());
   }
 
   protected prepareOutputConfig(config: any): WidgetConfigComponentData {
