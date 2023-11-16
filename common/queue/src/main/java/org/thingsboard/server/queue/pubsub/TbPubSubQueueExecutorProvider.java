1be39aa9aa74c574432cd4bc5980a837f578ecf7
dashevchenko <dshevchenko@thingsboard.io>
2023-11-16 16:16:38 +0200

    fixed license, env name refactoring

--- common/queue/src/main/java/org/thingsboard/server/queue/pubsub/TbPubSubQueueExecutorProvider.java
+++ common/queue/src/main/java/org/thingsboard/server/queue/pubsub/TbPubSubQueueExecutorProvider.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright © 2016-2023 The Thingsboard Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.thingsboard.server.queue.pubsub;
 
 import org.springframework.beans.factory.annotation.Value;
@@ -7,6 +22,7 @@ import org.thingsboard.common.util.ExecutorProvider;
 import org.thingsboard.common.util.ThingsBoardThreadFactory;
 
 import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 
@@ -33,5 +49,12 @@ public class TbPubSubQueueExecutorProvider implements ExecutorProvider {
     @Override
     public ScheduledExecutorService getExecutor() {
         return executor;
+    }
+
+    @PreDestroy
+    private void destroy() {
+        if (executor != null) {
+            executor.shutdown();
+        }
     }
 }
