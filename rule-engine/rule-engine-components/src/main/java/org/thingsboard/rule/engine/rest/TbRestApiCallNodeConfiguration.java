bdaa5551af4fe941c4fb7d6e5db58950def823d8
Ignacy Kuchciński <ignacy.kuchcinski@external.t-mobile.pl>
2023-11-16 13:20:36 +0100

    Add option to skip verification to External REST API node

--- rule-engine/rule-engine-components/src/main/java/org/thingsboard/rule/engine/rest/TbRestApiCallNodeConfiguration.java
+++ rule-engine/rule-engine-components/src/main/java/org/thingsboard/rule/engine/rest/TbRestApiCallNodeConfiguration.java
@@ -47,6 +47,7 @@ public class TbRestApiCallNodeConfiguration implements NodeConfiguration<TbRestA
     private String proxyScheme;
     private ClientCredentials credentials;
     private boolean ignoreRequestBody;
+    private boolean skipVerification;
 
     @Override
     public TbRestApiCallNodeConfiguration defaultConfiguration() {
@@ -62,6 +63,7 @@ public class TbRestApiCallNodeConfiguration implements NodeConfiguration<TbRestA
         configuration.setEnableProxy(false);
         configuration.setCredentials(new AnonymousCredentials());
         configuration.setIgnoreRequestBody(false);
+        configuration.setSkipVerification(false);
         return configuration;
     }
 
