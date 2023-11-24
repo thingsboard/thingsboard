5d89d339e61c724cfda6042d29d4e58053099890
Xuan Truong Pham <xuantruongpham@viphap.com>
2023-11-24 05:00:41 +0000

    update login page & check license

--- ui-ngx/src/assets/app.js
+++ ui-ngx/src/assets/app.js
@@ -13,77 +13,61 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-window.addEventListener("load", function (event) {
-    // var logo = document.getElementsByClassName("tb-logo-title");
-    // logo.src = "https://thaocrouch.s3.ap-southeast-1.amazonaws.com/logo.png";
-    // var target = document.querySelector("title");
-    // var observer = new MutationObserver(function (mutations) {
-    //   mutations.forEach(function (mutation) {
-    //     var title = document.title;
-    //     if (title.includes("ThingsBoard")) {
-    //       document.title = title.replace("ThingsBoard", "NewSense");
-    //     }
-    //   });
-    // });
-    // var config = {
-    //   childList: true,
-    // };
-    // observer.observe(target, config);
-    // checkLicense();
-  });
-  
-  function checkLicense() {
-    var settings = {
-      url: "https://license.viphap.com/license/validate-ui",
-      method: "POST",
-      timeout: 0,
-      headers: {
-        "Content-Type": "application/json",
-      },
-      data: JSON.stringify({
-        Tenant: document.title.split("|")[0].trim(),
-        Domain: window.location.hostname,
-      }),
-    };
-  
-    $.ajax(settings)
-      .done(function (response, status, jqXHR) {
-        if (jqXHR.status != 200) {
-          let content = `
-                      <div class="license">
-                          <div class="message">
-                              <img src="https://thaocrouch.s3.ap-southeast-1.amazonaws.com/logo.png" alt="Viphap NewSense">
-                              <p>Hi,</p>
-                              <p>would like to inform you that your license for our software/product is about to expire.</p>
-                              <p>To continue using our software/product, you will need to renew your license before the expiration date. Failure to do so may result in the software/product becoming
-                                  inactive.</p>
-                              <p>Renewal options and instructions can be found on our website at <a href="https://viphapgroup.com">Viphap Group</a>. If you have any questions or need further
-                                  assistance, please contact our support team at <a href="mail:hotro@viphap.com">hotro@viphap.com</a> or <a href="tel:19001080">1900 1080</a>.</p>
-                              <p>Thank you for being a valued customer and we look forward to providing you with continued access to our software/product.</p>
-                          </div>
-                      </div>
-                  `;
-          $("body").empty();
-          $("body").append(content);
-        }
-      })
-      .error(function (jqXHR, status, error) {
+window.addEventListener("load", (event) => {
+  checkLicense();
+});
+
+function checkLicense() {
+  var settings = {
+    url: "https://license.viphap.com/license/validate-ui",
+    method: "POST",
+    timeout: 0,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    data: JSON.stringify({
+      Tenant: document.title.split("|")[0].trim(),
+      Domain: window.location.hostname,
+    }),
+  };
+
+  $.ajax(settings)
+    .done(function (response, status, jqXHR) {
+      if (jqXHR.status != 200) {
         let content = `
-              <div class="license">
-                  <div class="message">
-                      <img src="https://thaocrouch.s3.ap-southeast-1.amazonaws.com/logo.png" alt="Viphap NewSense">
-                      <p>Hi,</p>
-                      <p>would like to inform you that your license for our software/product is about to expire.</p>
-                      <p>To continue using our software/product, you will need to renew your license before the expiration date. Failure to do so may result in the software/product becoming
-                          inactive.</p>
-                      <p>Renewal options and instructions can be found on our website at <a href="https://viphapgroup.com">Viphap Group</a>. If you have any questions or need further
-                          assistance, please contact our support team at <a href="mail:hotro@viphap.com">hotro@viphap.com</a> or <a href="tel:19001080">1900 1080</a>.</p>
-                      <p>Thank you for being a valued customer and we look forward to providing you with continued access to our software/product.</p>
+                  <div class="license">
+                      <div class="message">
+                          <img src="https://newsense.viphap.com/assets/logo_text_white.png" alt="VIPHAP NewSense">
+                          <p>Hi,</p>
+                          <p>would like to inform you that your license for our software/product is about to expire.</p>
+                          <p>To continue using our software/product, you will need to renew your license before the expiration date. Failure to do so may result in the software/product becoming
+                              inactive.</p>
+                          <p>Renewal options and instructions can be found on our website at <a href="https://viphap.com">VIPHAP</a>. If you have any questions or need further
+                              assistance, please contact our support team at <a href="mail:info@viphap.com">info@viphap.com</a>.</p>
+                          <p>Thank you for being a valued customer and we look forward to providing you with continued access to our software/product.</p>
+                      </div>
                   </div>
-              </div>
               `;
         $("body").empty();
         $("body").append(content);
-      });
-  }
-  
+      }
+    })
+    .error(function (jqXHR, status, error) {
+      let content = `
+          <div class="license">
+              <div class="message">
+                  <img src="https://newsense.viphap.com/assets/logo_text_white.png" alt="Viphap NewSense">
+                  <p>Hi,</p>
+                  <p>would like to inform you that your license for our software/product is about to expire.</p>
+                  <p>To continue using our software/product, you will need to renew your license before the expiration date. Failure to do so may result in the software/product becoming
+                      inactive.</p>
+                  <p>Renewal options and instructions can be found on our website at <a href="https://viphap.com">VIPHAP</a>. If you have any questions or need further
+                      assistance, please contact our support team at <a href="mail:info@viphap.com">info@viphap.com</a>.</p>
+                  <p>Thank you for being a valued customer and we look forward to providing you with continued access to our software/product.</p>
+              </div>
+          </div>
+          `;
+      $("body").empty();
+      $("body").append(content);
+    });
+}
