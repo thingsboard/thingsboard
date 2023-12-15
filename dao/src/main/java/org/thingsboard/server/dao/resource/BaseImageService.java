9b92a753ace1f2a82b43bf8209c0500ed5f66575
ViacheslavKlimov <viacheslavklimov11@gmail.com>
2023-12-15 12:48:30 +0200

    Endpoint for updating image public status

--- dao/src/main/java/org/thingsboard/server/dao/resource/BaseImageService.java
+++ dao/src/main/java/org/thingsboard/server/dao/resource/BaseImageService.java
@@ -221,8 +221,8 @@ public class BaseImageService extends BaseResourceService implements ImageServic
     }
 
     @Override
-    public TbResourceInfo getPublicImageInfoByPublicKey(String publicKey) {
-        return resourceInfoDao.findPublicResourceByKey(ResourceType.IMAGE, publicKey);
+    public TbResourceInfo getPublicImageInfoByKey(String publicResourceKey) {
+        return resourceInfoDao.findPublicResourceByKey(ResourceType.IMAGE, publicResourceKey);
     }
 
     @Override
