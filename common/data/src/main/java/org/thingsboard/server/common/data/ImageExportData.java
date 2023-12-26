22e598c36d8fe74403893ccdf8c3a6e82a7d9566
ViacheslavKlimov <viacheslavklimov11@gmail.com>
2023-12-26 18:21:34 +0200

    Fix image's isPublic on import

--- common/data/src/main/java/org/thingsboard/server/common/data/ImageExportData.java
+++ common/data/src/main/java/org/thingsboard/server/common/data/ImageExportData.java
@@ -16,22 +16,26 @@
 package org.thingsboard.server.common.data;
 
 import io.swagger.annotations.ApiModel;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @ApiModel
 @Slf4j
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class ImageExportData {
 
-    private final String mediaType;
-    private final String fileName;
-    private final String title;
-    private final String resourceKey;
-    private final boolean isPublic;
-    private final String publicResourceKey;
-    private final String data;
+    private String mediaType;
+    private String fileName;
+    private String title;
+    private String resourceKey;
+    private boolean isPublic;
+    private String publicResourceKey;
+    private String data;
 
 }
