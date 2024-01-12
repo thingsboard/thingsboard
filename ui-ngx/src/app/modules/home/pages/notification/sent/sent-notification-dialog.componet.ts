a68b12bdd5c9e8be18c24707dab1840d1d8b5eca
Vladyslav_Prykhodko <vprykhodko@thingsboard.io>
2024-01-12 12:46:04 +0200

    UI: Minor improvement notification settings

--- ui-ngx/src/app/modules/home/pages/notification/sent/sent-notification-dialog.componet.ts
+++ ui-ngx/src/app/modules/home/pages/notification/sent/sent-notification-dialog.componet.ts
@@ -328,6 +328,7 @@ export class SentNotificationDialogComponent extends
         return '/settings/outgoing-mail';
       case NotificationDeliveryMethod.SMS:
       case NotificationDeliveryMethod.SLACK:
+      case NotificationDeliveryMethod.MOBILE_APP:
         return '/settings/notifications';
     }
   }
