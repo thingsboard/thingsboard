111c3a490aa8f2a91c6699d67189556578d2e484
Andrii Landiak <andriylandiak@gmail.com>
2024-03-20 13:38:57 +0200

    Fix compile error

--- dao/src/main/java/org/thingsboard/server/dao/edge/EdgeSessionRedisCache.java
+++ dao/src/main/java/org/thingsboard/server/dao/edge/EdgeSessionRedisCache.java
@@ -21,7 +21,7 @@ import org.springframework.stereotype.Service;
 import org.thingsboard.server.cache.CacheSpecsMap;
 import org.thingsboard.server.cache.RedisTbTransactionalCache;
 import org.thingsboard.server.cache.TBRedisCacheConfiguration;
-import org.thingsboard.server.cache.TbFSTRedisSerializer;
+import org.thingsboard.server.cache.TbJsonRedisSerializer;
 import org.thingsboard.server.common.data.CacheConstants;
 import org.thingsboard.server.common.data.id.EdgeId;
 
@@ -30,7 +30,7 @@ import org.thingsboard.server.common.data.id.EdgeId;
 public class EdgeSessionRedisCache extends RedisTbTransactionalCache<EdgeId, String> {
 
     public EdgeSessionRedisCache(TBRedisCacheConfiguration configuration, CacheSpecsMap cacheSpecsMap, RedisConnectionFactory connectionFactory) {
-        super(CacheConstants.EDGE_SESSIONS_CACHE, cacheSpecsMap, connectionFactory, configuration, new TbFSTRedisSerializer<>());
+        super(CacheConstants.EDGE_SESSIONS_CACHE, cacheSpecsMap, connectionFactory, configuration, new TbJsonRedisSerializer<>(String.class));
     }
 
 }
