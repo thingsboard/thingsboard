4daf53bf25af545789b83ffbe8c98f0aa03e911b
ViacheslavKlimov <viacheslavklimov11@gmail.com>
2024-05-16 11:52:41 +0300

    Lazy Jpa bootstrap mode

--- dao/src/main/java/org/thingsboard/server/dao/TimescaleTsLatestDaoConfig.java
+++ dao/src/main/java/org/thingsboard/server/dao/TimescaleTsLatestDaoConfig.java
@@ -19,6 +19,7 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.repository.config.BootstrapMode;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.thingsboard.server.dao.util.TbAutoConfiguration;
 import org.thingsboard.server.dao.util.TimescaleDBTsLatestDao;
@@ -26,7 +27,7 @@ import org.thingsboard.server.dao.util.TimescaleDBTsLatestDao;
 @Configuration
 @TbAutoConfiguration
 @ComponentScan({"org.thingsboard.server.dao.sqlts.timescale"})
-@EnableJpaRepositories({"org.thingsboard.server.dao.sqlts.insert.latest.sql", "org.thingsboard.server.dao.sqlts.latest"})
+@EnableJpaRepositories(value = {"org.thingsboard.server.dao.sqlts.insert.latest.sql", "org.thingsboard.server.dao.sqlts.latest"}, bootstrapMode = BootstrapMode.LAZY)
 @EntityScan({"org.thingsboard.server.dao.model.sqlts.latest"})
 @EnableTransactionManagement
 @TimescaleDBTsLatestDao
