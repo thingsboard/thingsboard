9d58492dacf229f50d5d7b2c668833941f6ea499
ViacheslavKlimov <viacheslavklimov11@gmail.com>
2024-05-16 11:52:41 +0300

    Lazy Jpa bootstrap mode

--- dao/src/main/java/org/thingsboard/server/dao/SqlTimeseriesDaoConfig.java
+++ dao/src/main/java/org/thingsboard/server/dao/SqlTimeseriesDaoConfig.java
@@ -18,13 +18,14 @@ package org.thingsboard.server.dao;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.repository.config.BootstrapMode;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.thingsboard.server.dao.util.SqlTsOrTsLatestAnyDao;
 import org.thingsboard.server.dao.util.TbAutoConfiguration;
 
 @Configuration
 @TbAutoConfiguration
-@EnableJpaRepositories({"org.thingsboard.server.dao.sqlts.dictionary"})
+@EnableJpaRepositories(value = {"org.thingsboard.server.dao.sqlts.dictionary"}, bootstrapMode = BootstrapMode.LAZY)
 @EntityScan({"org.thingsboard.server.dao.model.sqlts.dictionary"})
 @EnableTransactionManagement
 @SqlTsOrTsLatestAnyDao
