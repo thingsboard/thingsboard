b0844471acf2d63c5d6e678720007b06d7d59219
IrynaMatveieva <irina.m2004@icloud.com>
2024-06-04 11:11:29 +0300

    added test for default config and added check to verify all acknowledged msgs

--- rule-engine/rule-engine-components/src/test/java/org/thingsboard/rule/engine/action/TbMsgCountNodeTest.java
+++ rule-engine/rule-engine-components/src/test/java/org/thingsboard/rule/engine/action/TbMsgCountNodeTest.java
@@ -16,6 +16,7 @@
 package org.thingsboard.rule.engine.action;
 
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -25,9 +26,12 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.stubbing.Answer;
 import org.thingsboard.common.util.JacksonUtil;
+import org.thingsboard.common.util.ThingsBoardThreadFactory;
 import org.thingsboard.rule.engine.api.TbContext;
 import org.thingsboard.rule.engine.api.TbNodeConfiguration;
 import org.thingsboard.rule.engine.api.TbNodeException;
+import org.thingsboard.server.common.data.id.DeviceId;
+import org.thingsboard.server.common.data.id.RuleNodeId;
 import org.thingsboard.server.common.data.id.TenantId;
 import org.thingsboard.server.common.data.msg.TbMsgType;
 import org.thingsboard.server.common.data.msg.TbNodeConnectionType;
@@ -43,7 +47,6 @@ import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.times;
@@ -55,8 +58,8 @@ import static org.mockito.Mockito.when;
 public class TbMsgCountNodeTest {
 
     private final TenantId TENANT_ID = TenantId.fromUUID(UUID.fromString("773ccb0e-e822-44b7-97aa-10de83ad081d"));
-    private final ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
-    private final int msgCountInterval = 1;
+    private final ThingsBoardThreadFactory factory = ThingsBoardThreadFactory.forName("msg-count-node-test");
+    private final ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor(factory);
 
 
     private TbMsgCountNode node;
@@ -69,11 +72,24 @@ public class TbMsgCountNodeTest {
     @BeforeEach
     public void setUp() {
         node = new TbMsgCountNode();
-        config = new TbMsgCountNodeConfiguration();
+        config = new TbMsgCountNodeConfiguration().defaultConfiguration();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        executorService.shutdownNow();
+        node.destroy();
+    }
+
+    @Test
+    public void verifyDefaultConfig() {
+        assertThat(config.getInterval()).isEqualTo(1);
+        assertThat(config.getTelemetryPrefix()).isEqualTo("messageCount");
     }
 
     @Test
     public void givenIncomingMsgs_whenOnMsg_thenSendsMsgWithMsgCount() throws TbNodeException, InterruptedException {
+        int msgCountInterval = 1;
         config.setInterval(msgCountInterval);
         config.setTelemetryPrefix("count");
         var configuration = new TbNodeConfiguration(JacksonUtil.valueToTree(config));
@@ -86,12 +102,15 @@ public class TbMsgCountNodeTest {
         invokeTellSelf(isMsgWithCounterSent, config.getInterval());
         when(ctxMock.getTenantId()).thenReturn(TENANT_ID);
         when(ctxMock.getServiceId()).thenReturn("tb-rule-engine");
-        TbMsg tickMsg = TbMsg.newMsg(TbMsgType.MSG_COUNT_SELF_MSG, null, TbMsgMetaData.EMPTY, TbMsg.EMPTY_STRING);
-        when(ctxMock.newMsg(any(), any(TbMsgType.class), any(), any(), any(), any())).thenReturn(tickMsg);
+        RuleNodeId ruleNodeId = new RuleNodeId(UUID.fromString("4ac734ea-2434-4402-b61b-eaa66b650711"));
+        when(ctxMock.getSelfId()).thenReturn(ruleNodeId);
+        TbMsg tickMsg = TbMsg.newMsg(TbMsgType.MSG_COUNT_SELF_MSG, ruleNodeId, TbMsgMetaData.EMPTY, TbMsg.EMPTY_STRING);
+        when(ctxMock.newMsg(null, TbMsgType.MSG_COUNT_SELF_MSG, ruleNodeId, null, TbMsgMetaData.EMPTY, TbMsg.EMPTY_STRING)).thenReturn(tickMsg);
 
         node.init(ctxMock, configuration);
 
-        TbMsg msg = TbMsg.newMsg(TbMsgType.POST_TELEMETRY_REQUEST, null, TbMsgMetaData.EMPTY, TbMsg.EMPTY_STRING);
+        DeviceId deviceId = new DeviceId(UUID.fromString("e7e2392a-16fe-4b2b-81db-ec99a0edd54f"));
+        TbMsg msg = TbMsg.newMsg(TbMsgType.POST_TELEMETRY_REQUEST, deviceId, TbMsgMetaData.EMPTY, TbMsg.EMPTY_STRING);
         for (int i = 0; i < msgCount; i++) {
             if (isMsgWithCounterSent.get()) {
                 break;
@@ -102,7 +121,9 @@ public class TbMsgCountNodeTest {
 
         awaitTellSelfLatch.await();
 
-        verify(ctxMock, times(currentMsgNumber.get())).ack(any(TbMsg.class));
+        ArgumentCaptor<TbMsg> msgCaptor = ArgumentCaptor.forClass(TbMsg.class);
+        verify(ctxMock, times(currentMsgNumber.get())).ack(msgCaptor.capture());
+        msgCaptor.getAllValues().forEach(captor -> assertThat(captor).isEqualTo(msg));
         ArgumentCaptor<TbMsg> msgWithCounterCaptor = ArgumentCaptor.forClass(TbMsg.class);
         verify(ctxMock).enqueueForTellNext(msgWithCounterCaptor.capture(), eq(TbNodeConnectionType.SUCCESS));
         TbMsg resultedMsg = msgWithCounterCaptor.getValue();
