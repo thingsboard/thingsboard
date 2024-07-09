164185c0f34c095c8e2c24b598dd4b8b6331faec
YevhenBondarenko <ybondarenko@thingsboard.io>
2024-07-09 17:17:23 +0200

    added upgrade script for attributes and latest ts

---
+++ application/src/main/data/upgrade/3.7.0/schema_update.sql
@@ -0,0 +1,23 @@
+--
+-- Copyright © 2016-2024 The Thingsboard Authors
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+--
+
+-- UPDATE PUBLIC CUSTOMERS START
+
+CREATE SEQUENCE IF NOT EXISTS attribute_kv_version_seq cache 1000;
+CREATE SEQUENCE IF NOT EXISTS ts_kv_latest_version_seq cache 1000;
+
+ALTER TABLE attribute_kv ADD COLUMN version bigint default 0;
+ALTER TABLE ts_kv_latest ADD COLUMN version bigint default 0;
