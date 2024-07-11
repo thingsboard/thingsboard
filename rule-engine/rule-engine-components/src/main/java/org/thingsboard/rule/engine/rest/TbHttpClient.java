58185d4a4c42bb8bfb363e467eaeb1c82afd1325
YevhenBondarenko <ybondarenko@thingsboard.io>
2024-07-11 11:23:03 +0200

    hotfix Connection reset

--- rule-engine/rule-engine-components/src/main/java/org/thingsboard/rule/engine/rest/TbHttpClient.java
+++ rule-engine/rule-engine-components/src/main/java/org/thingsboard/rule/engine/rest/TbHttpClient.java
@@ -137,6 +137,7 @@ public class TbHttpClient {
 
             this.webClient = WebClient.builder()
                     .clientConnector(new ReactorClientHttpConnector(httpClient))
+                    .defaultHeader(HttpHeaders.CONNECTION, "close") //In previous realization this header was present! (Added for hotfix "Connection reset")
                     .build();
         } catch (SSLException e) {
             throw new TbNodeException(e);
