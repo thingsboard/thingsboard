49707f9cfc704a454dc9585fb55dbb0dd6b187b2
ViacheslavKlimov <viacheslavklimov11@gmail.com>
2024-08-06 17:12:50 +0300

    Fix Timescale dao configuration

--- dao/src/main/java/org/thingsboard/server/dao/config/TimescaleDaoConfig.java
+++ dao/src/main/java/org/thingsboard/server/dao/config/TimescaleDaoConfig.java
@@ -15,7 +15,6 @@
  */
 package org.thingsboard.server.dao.config;
 
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -28,7 +27,6 @@ import org.thingsboard.server.dao.util.TimescaleDBTsDao;
 @TbAutoConfiguration
 @ComponentScan({"org.thingsboard.server.dao.sqlts.timescale"})
 @EnableJpaRepositories(value = {"org.thingsboard.server.dao.sqlts.timescale", "org.thingsboard.server.dao.sqlts.insert.timescale"}, bootstrapMode = BootstrapMode.LAZY)
-@EntityScan({"org.thingsboard.server.dao.model.sqlts.timescale"})
 @EnableTransactionManagement
 @TimescaleDBTsDao
 public class TimescaleDaoConfig {
--- dao/src/main/java/org/thingsboard/server/dao/config/TimescaleTsLatestDaoConfig.java
+++ dao/src/main/java/org/thingsboard/server/dao/config/TimescaleTsLatestDaoConfig.java
@@ -15,7 +15,6 @@
  */
 package org.thingsboard.server.dao.config;
 
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -28,7 +27,6 @@ import org.thingsboard.server.dao.util.TimescaleDBTsLatestDao;
 @TbAutoConfiguration
 @ComponentScan({"org.thingsboard.server.dao.sqlts.timescale"})
 @EnableJpaRepositories(value = {"org.thingsboard.server.dao.sqlts.insert.latest.sql", "org.thingsboard.server.dao.sqlts.latest"}, bootstrapMode = BootstrapMode.LAZY)
-@EntityScan({"org.thingsboard.server.dao.model.sqlts.latest"})
 @EnableTransactionManagement
 @TimescaleDBTsLatestDao
 public class TimescaleTsLatestDaoConfig {
