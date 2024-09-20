abd98a3a37c57028351fbee784d8234679e56319
luke.miao <282583553@qq.com>
2024-09-20 11:42:43 +0800

    add support for batch insertion

--- dao/src/main/java/org/thingsboard/server/dao/sqlts/insert/iotdb/IoTDBInsertTsRepository.java
+++ dao/src/main/java/org/thingsboard/server/dao/sqlts/insert/iotdb/IoTDBInsertTsRepository.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.thingsboard.server.dao.sqlts.insert.iotdb;
 
 import com.google.common.collect.Lists;
