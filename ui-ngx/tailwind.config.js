098af9ab806578daaec62d18118b3c2e9dfdc061
Igor Kulikov <ikulikov@thingsboard.io>
2024-10-24 12:38:55 +0300

    Update tailwind css safelist

--- ui-ngx/tailwind.config.js
+++ ui-ngx/tailwind.config.js
@@ -167,7 +167,10 @@ module.exports = {
   },
   safelist: [
     'lt-md:gap-3',
-    'md:!hidden'
+    'md:!hidden',
+    'gap-6',
+    'gap-7',
+    'gap-10'
   ],
   corePlugins: {
     preflight: false
