dc4596213410110e3646d2418a077da598d11563
mpetrov <mpetrov@thingsboard.io>
2024-11-08 14:20:13 +0200

    Made value not required property in Transformation Rule Node debug metadata

--- ui-ngx/src/app/shared/components/kv-map.component.ts
+++ ui-ngx/src/app/shared/components/kv-map.component.ts
@@ -20,12 +20,12 @@ import {
   ControlValueAccessor,
   UntypedFormArray,
   UntypedFormBuilder,
-  UntypedFormControl,
   UntypedFormGroup,
   NG_VALIDATORS,
   NG_VALUE_ACCESSOR,
   Validator,
-  Validators
+  Validators,
+  ValidationErrors
 } from '@angular/forms';
 import { PageComponent } from '@shared/components/page.component';
 import { Store } from '@ngrx/store';
@@ -54,6 +54,8 @@ import { takeUntil } from 'rxjs/operators';
 export class KeyValMapComponent extends PageComponent implements ControlValueAccessor, OnInit, OnDestroy, Validator {
 
   @Input() disabled: boolean;
+
+  @Input() isValueRequired = true;
 
   @Input() titleText: string;
 
@@ -118,7 +120,7 @@ export class KeyValMapComponent extends PageComponent implements ControlValueAcc
         if (Object.prototype.hasOwnProperty.call(keyValMap, property)) {
           keyValsControls.push(this.fb.group({
             key: [property, [Validators.required]],
-            value: [keyValMap[property], [Validators.required]]
+            value: [keyValMap[property], this.isValueRequired ? [Validators.required] : []]
           }));
         }
       }
@@ -139,24 +141,12 @@ export class KeyValMapComponent extends PageComponent implements ControlValueAcc
     const keyValsFormArray = this.kvListFormGroup.get('keyVals') as UntypedFormArray;
     keyValsFormArray.push(this.fb.group({
       key: ['', [Validators.required]],
-      value: ['', [Validators.required]]
+      value: ['', this.isValueRequired ? [Validators.required] : []]
     }));
   }
 
-  public validate(c: UntypedFormControl) {
-    const kvList: {key: string; value: string}[] = this.kvListFormGroup.get('keyVals').value;
-    let valid = true;
-    for (const entry of kvList) {
-      if (!entry.key || !entry.value) {
-        valid = false;
-        break;
-      }
-    }
-    return (valid) ? null : {
-      keyVals: {
-        valid: false,
-      },
-    };
+  public validate(): ValidationErrors | null {
+    return this.kvListFormGroup.valid ? null : { keyVals: { valid: false } };
   }
 
   private updateModel() {
