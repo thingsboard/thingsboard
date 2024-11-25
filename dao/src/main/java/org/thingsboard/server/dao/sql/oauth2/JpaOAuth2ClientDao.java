c8d627371b0725a95d7a3acdc882c4701e28593f
dashevchenko <dshevchenko@thingsboard.io>
2024-11-25 18:48:01 +0200

    fixed oauth2 authorization for apps of version 1.3.0 and less

--- dao/src/main/java/org/thingsboard/server/dao/sql/oauth2/JpaOAuth2ClientDao.java
+++ dao/src/main/java/org/thingsboard/server/dao/sql/oauth2/JpaOAuth2ClientDao.java
@@ -80,8 +80,8 @@ public class JpaOAuth2ClientDao extends JpaAbstractDao<OAuth2ClientEntity, OAuth
     }
 
     @Override
-    public String findAppSecret(UUID id, String pkgName) {
-        return repository.findAppSecret(id, pkgName);
+    public String findAppSecret(UUID id, String pkgName, PlatformType platformType) {
+        return repository.findAppSecret(id, pkgName, platformType != null ? platformType.name() : null);
     }
 
     @Override
