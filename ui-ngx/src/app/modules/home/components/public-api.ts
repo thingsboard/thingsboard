b5dd07153725f35d728c3bd538e3c31927a5ad86
kalytka <ykalytka@thingsboard.io>
2024-12-06 17:56:05 +0200

    Refactoring

--- ui-ngx/src/app/modules/home/components/public-api.ts
+++ ui-ngx/src/app/modules/home/components/public-api.ts
@@ -19,8 +19,8 @@ export * from './widget/config/basic/basic-widget-config.module';
 export * from './widget/lib/settings/common/widget-settings-common.module';
 export * from './widget/widget-components.module';
 export * from './widget/config/widget-config-components.module';
-export * from './widget/lib/chart/echarts-widget.models';
 
+export * from './widget/lib/chart/echarts-widget.models';
 export * from './widget/config/widget-config.component.models';
 export * from './widget/lib/table-widget.models';
 export * from './widget/lib/flot-widget.models';
