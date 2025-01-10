c0124b10f6356f44804f91577e4b97a9f9da58b7
mpetrov <mpetrov@thingsboard.io>
2025-01-10 17:16:59 +0200

    Added utils functions for echart implementation in extensions

--- ui-ngx/src/app/modules/home/components/widget/lib/chart/echarts-widget.models.ts
+++ ui-ngx/src/app/modules/home/components/widget/lib/chart/echarts-widget.models.ts
@@ -47,19 +47,29 @@ import {
 } from 'echarts/charts';
 import { LabelLayout } from 'echarts/features';
 import { CanvasRenderer, SVGRenderer } from 'echarts/renderers';
-import { CallbackDataParams } from 'echarts/types/dist/shared';
+import { CallbackDataParams, XAXisOption, YAXisOption, ZRColor } from 'echarts/types/dist/shared';
 import GlobalModel from 'echarts/types/src/model/Global';
 import Axis2D from 'echarts/types/src/coord/cartesian/Axis2D';
 import SeriesModel from 'echarts/types/src/model/Series';
 import { MarkLine2DDataItemOption } from 'echarts/types/src/component/marker/MarkLineModel';
 import { measureSymbolOffset } from '@home/components/widget/lib/chart/chart.models';
+import {
+  cssUnits,
+  fontStyles,
+  fontWeights
+} from '@shared/models/widget-settings.models';
+import { TimeSeriesChartTooltipWidgetSettings } from '@home/components/widget/lib/chart/time-series-chart-tooltip.models';
+import { Renderer2, SecurityContext } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
-class EChartsModule {
+export class EChartsModule {
+  constructor(private echarts) {
+  }
   private initialized = false;
 
   init() {
     if (!this.initialized) {
-      echarts.use([
+      this.echarts.use([
         TooltipComponent,
         GridComponent,
         VisualMapComponent,
@@ -81,7 +91,7 @@ class EChartsModule {
   }
 }
 
-export const echartsModule = new EChartsModule();
+export const echartsModule = new EChartsModule(echarts);
 
 export type EChartsOption = echarts.ComposeOption<
   | TooltipComponentOption
@@ -262,3 +272,252 @@ export const getFocusedSeriesIndex = (chart: ECharts): number => {
   }
   return -1;
 };
+
+export const defaultTooltipSettings: Partial<TimeSeriesChartTooltipWidgetSettings> = {
+  tooltipDateFont: {
+    family: 'Roboto',
+    size: 12,
+    sizeUnit: cssUnits[0],
+    style: fontStyles[0],
+    weight: fontWeights[0],
+    lineHeight: '16px'
+  },
+  tooltipDateColor: 'rgba(0, 0, 0, 0.76)'
+}
+
+export const defaultValueSettings: Partial<TimeSeriesChartTooltipWidgetSettings> = {
+  tooltipDateFont: {
+    family: 'Roboto',
+    size: 11,
+    sizeUnit: cssUnits[0],
+    style: fontStyles[0],
+    weight: '500',
+    lineHeight: '16px'
+  },
+  tooltipDateColor: 'rgba(0, 0, 0, 0.76)'
+}
+
+export const getDefaultXAxis = (min: number, max: number): XAXisOption => {
+  return {
+    id: 'xAxis',
+    mainType: 'xAxis',
+    show: true,
+    type: 'time',
+    position: "bottom",
+    offset: 0,
+    nameLocation: 'middle',
+    max,
+    min,
+    nameTextStyle: {
+      color: 'rgba(0, 0, 0, 0.54)',
+      fontStyle: 'normal',
+      fontWeight: 600,
+      fontFamily: 'Roboto',
+      fontSize: 12,
+    },
+    axisPointer: {
+      shadowStyle: {
+        color: 'rgba(210,219,238,0.2)'
+      }
+    },
+    splitLine: {
+      show: true
+    },
+    axisTick: {
+      show: true,
+      lineStyle: {
+        color: 'rgba(0, 0, 0, 0.54)'
+      }
+    },
+    axisLine: {
+      onZero: false,
+      show: true,
+      lineStyle: {
+        color: 'rgba(0, 0, 0, 0.54)'
+      }
+    },
+    axisLabel: {
+      color: 'rgba(0, 0, 0, 0.54)',
+      fontFamily: 'Roboto',
+      fontSize: 10,
+      fontStyle: 'normal',
+      fontWeight: 400,
+      show: true,
+      hideOverlap: true,
+    }
+  }
+}
+
+export const getDefaultYAxis = (formatter: (value: unknown) => string): YAXisOption => {
+  return {
+    type: 'value',
+    position: 'left',
+    mainType: 'yAxis',
+    id: 'yAxis',
+    offset: 0,
+    nameLocation: 'middle',
+    nameRotate: 90,
+    alignTicks: true,
+    scale: true,
+    show: true,
+    axisLabel: {
+      color: 'rgba(0, 0, 0, 0.54)',
+      fontFamily: 'Roboto',
+      fontSize: 12,
+      fontStyle: 'normal',
+      fontWeight: 400,
+      show: true,
+      formatter,
+    },
+    splitLine: {
+      show: true,
+    },
+    axisLine: {
+      show: true,
+      lineStyle: {
+        color: 'rgba(0, 0, 0, 0.54)'
+      }
+    },
+    axisTick: {
+      lineStyle: {
+        color: 'rgba(0, 0, 0, 0.54)'
+      },
+      show: true
+    },
+    nameTextStyle: {
+      color: 'rgba(0, 0, 0, 0.54)',
+      fontFamily: 'Roboto',
+      fontSize: 12,
+      fontStyle: 'normal',
+      fontWeight: 600
+    }
+  }
+}
+
+export const getDefaultChartOptions = (): Partial<EChartsOption> => {
+  return {
+    animation: true,
+    animationDelay: 0,
+    animationDelayUpdate: 0,
+    animationDuration: 500,
+    animationDurationUpdate: 300,
+    animationEasing: "cubicOut",
+    animationEasingUpdate: "cubicOut",
+    animationThreshold: 2000,
+    backgroundColor: "transparent",
+    darkMode: false,
+    tooltip: {
+      show: true,
+      trigger: 'axis',
+      confine: true,
+      padding: [8, 12],
+      appendTo: 'body',
+      textStyle: {
+        fontFamily: 'Roboto',
+        fontSize: 12,
+        fontWeight: 'normal',
+        lineHeight: 16
+      }
+    },
+    grid: [{
+      backgroundColor: null,
+      borderColor: "#ccc",
+      borderWidth: 1,
+      bottom: 45,
+      left: 5,
+      right: 5,
+      show: false,
+      top: 10
+    }],
+    dataZoom: [
+      {
+        type: 'inside',
+        disabled: false,
+        realtime: true,
+        filterMode:  'none'
+      },
+      {
+        type: 'slider',
+        show: true,
+        showDetail: false,
+        realtime: true,
+        filterMode: 'none',
+        bottom: 5
+      }
+    ]
+  }
+}
+
+export const getTooltipDateElement = (renderer: Renderer2, dateText: string, settings = defaultTooltipSettings): HTMLElement => {
+  const dateElement: HTMLElement = renderer.createElement('div');
+  renderer.appendChild(dateElement, renderer.createText(dateText));
+  renderer.setStyle(dateElement, 'font-family', settings.tooltipDateFont.family);
+  renderer.setStyle(dateElement, 'font-size', settings.tooltipDateFont.size + settings.tooltipDateFont.sizeUnit);
+  renderer.setStyle(dateElement, 'font-style', settings.tooltipDateFont.style);
+  renderer.setStyle(dateElement, 'font-weight', settings.tooltipDateFont.weight);
+  renderer.setStyle(dateElement, 'line-height', settings.tooltipDateFont.lineHeight);
+  renderer.setStyle(dateElement, 'color', settings.tooltipDateColor);
+  return dateElement
+}
+
+export const getTooltipElement = (renderer: Renderer2, gapSize: string): HTMLElement => {
+  const tooltipElement: HTMLElement = renderer.createElement('div');
+  renderer.setStyle(tooltipElement, 'display', 'flex');
+  renderer.setStyle(tooltipElement, 'flex-direction', 'column');
+  renderer.setStyle(tooltipElement, 'align-items', 'flex-start');
+  renderer.setStyle(tooltipElement, 'gap', gapSize);
+  return tooltipElement;
+}
+
+export const getLabelValueElement = (renderer: Renderer2): HTMLElement => {
+  const labelValueElement: HTMLElement = renderer.createElement('div');
+  renderer.setStyle(labelValueElement, 'display', 'flex');
+  renderer.setStyle(labelValueElement, 'flex-direction', 'row');
+  renderer.setStyle(labelValueElement, 'align-items', 'center');
+  renderer.setStyle(labelValueElement, 'align-self', 'stretch');
+  renderer.setStyle(labelValueElement, 'gap', '12px');
+  return labelValueElement;
+}
+
+export const getLabelElement = (renderer: Renderer2): HTMLElement => {
+  const labelElement: HTMLElement = renderer.createElement('div');
+  renderer.setStyle(labelElement, 'display', 'flex');
+  renderer.setStyle(labelElement, 'align-items', 'center');
+  renderer.setStyle(labelElement, 'gap', '8px');
+  return labelElement;
+}
+
+export const getCircleElement = (renderer: Renderer2, color: ZRColor): HTMLElement => {
+  const circleElement: HTMLElement = renderer.createElement('div');
+  renderer.setStyle(circleElement, 'width', '8px');
+  renderer.setStyle(circleElement, 'height', '8px');
+  renderer.setStyle(circleElement, 'border-radius', '50%');
+  renderer.setStyle(circleElement, 'background', color);
+  return circleElement;
+}
+
+export const getLabelTextElement = (renderer: Renderer2, sanitizer: DomSanitizer, seriesName: string): HTMLElement => {
+  const labelTextElement: HTMLElement = renderer.createElement('div');
+  renderer.setProperty(labelTextElement, 'innerHTML', sanitizer.sanitize(SecurityContext.HTML, seriesName));
+  renderer.setStyle(labelTextElement, 'font-family', 'Roboto');
+  renderer.setStyle(labelTextElement, 'font-size', '12px');
+  renderer.setStyle(labelTextElement, 'font-style', 'normal');
+  renderer.setStyle(labelTextElement, 'font-weight', 400);
+  renderer.setStyle(labelTextElement, 'line-height', '16px');
+  renderer.setStyle(labelTextElement, 'color', 'rgba(0, 0, 0, 0.76)');
+  return labelTextElement;
+}
+
+export const getValueElement = (renderer: Renderer2, sanitizer: DomSanitizer, value: string, settings = defaultValueSettings): HTMLElement => {
+  const valueElement: HTMLElement = renderer.createElement('div');
+  renderer.setProperty(valueElement, 'innerHTML', sanitizer.sanitize(SecurityContext.HTML, value));
+  renderer.setStyle(valueElement, 'flex', '1');
+  renderer.setStyle(valueElement, 'text-align', 'end');
+  renderer.setStyle(valueElement, 'font-family', settings.tooltipDateFont.family);
+  renderer.setStyle(valueElement, 'font-size', settings.tooltipDateFont.size + settings.tooltipDateFont.sizeUnit);
+  renderer.setStyle(valueElement, 'font-style', settings.tooltipDateFont.style);
+  renderer.setStyle(valueElement, 'font-weight', settings.tooltipDateFont.weight);
+  renderer.setStyle(valueElement, 'line-height', settings.tooltipDateFont.lineHeight);
+  renderer.setStyle(valueElement, 'color', settings.tooltipDateColor);
+  return valueElement;
+}
