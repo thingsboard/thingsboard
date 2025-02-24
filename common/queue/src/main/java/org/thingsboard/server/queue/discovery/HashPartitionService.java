ab9bef4d90e6a1ccf84c66af123433bb936b499b
ViacheslavKlimov <viacheslavklimov11@gmail.com>
2025-02-24 12:19:17 +0200

    Fix HashPartitionServiceTest

--- common/queue/src/main/java/org/thingsboard/server/queue/discovery/HashPartitionService.java
+++ common/queue/src/main/java/org/thingsboard/server/queue/discovery/HashPartitionService.java
@@ -62,9 +62,9 @@ public class HashPartitionService implements PartitionService {
     private String coreTopic;
     @Value("${queue.core.partitions:10}")
     private Integer corePartitions;
-    @Value("${queue.calculated_fields.event_topic}")
+    @Value("${queue.calculated_fields.event_topic:tb_cf_event}")
     private String cfEventTopic;
-    @Value("${queue.calculated_fields.state_topic}")
+    @Value("${queue.calculated_fields.state_topic:tb_cf_state}")
     private String cfStateTopic;
     @Value("${queue.calculated_fields.partitions:10}")
     private Integer cfPartitions;
