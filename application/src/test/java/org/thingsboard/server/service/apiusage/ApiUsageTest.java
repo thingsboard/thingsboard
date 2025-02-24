545ea89713bdf3b4f31b3275d42b5c5f5e5575ae
Artem Barysh <abarysh@thingsboard.io>
2025-02-24 14:59:54 +0200

    Increased timeout to 30 seconds

--- application/src/test/java/org/thingsboard/server/service/apiusage/ApiUsageTest.java
+++ application/src/test/java/org/thingsboard/server/service/apiusage/ApiUsageTest.java
@@ -94,7 +94,7 @@ public class ApiUsageTest extends AbstractControllerTest {
             doPostAsync(url, telemetryPayload, String.class, status().isOk());
         }
 
-        await().atMost(5, TimeUnit.SECONDS).untilAsserted(() -> assertEquals(ApiUsageStateValue.WARNING, apiUsageStateService.findTenantApiUsageState(tenantId).getDbStorageState()));
+        await().atMost(TIMEOUT, TimeUnit.SECONDS).untilAsserted(() -> assertEquals(ApiUsageStateValue.WARNING, apiUsageStateService.findTenantApiUsageState(tenantId).getDbStorageState()));
 
         long VALUE_DISABLE = (long) (MAX_DP_ENABLE_VALUE - (MAX_DP_ENABLE_VALUE * WARN_THRESHOLD_VALUE)) / 2;
 
@@ -102,7 +102,7 @@ public class ApiUsageTest extends AbstractControllerTest {
             doPostAsync(url, telemetryPayload, String.class, status().isOk());
         }
 
-        await().atMost(5, TimeUnit.SECONDS)
+        await().atMost(TIMEOUT, TimeUnit.SECONDS)
                 .untilAsserted(() -> {
                     assertEquals(ApiUsageStateValue.DISABLED, apiUsageStateService.findTenantApiUsageState(tenantId).getDbStorageState());
                 });
