07256796897cbeef008c4b2b258c78d8fee8f633
trikimiki <41070061+trikimiki@users.noreply.github.com>
2025-02-27 14:59:40 +0200

    remove checkFolders from other scripts reason - it breaks running TB for MacOS users; will update site documentation accordingly

--- docker/docker-upgrade-tb.sh
+++ docker/docker-upgrade-tb.sh
@@ -44,8 +44,6 @@ ADDITIONAL_CACHE_ARGS=$(additionalComposeCacheArgs) || exit $?
 
 ADDITIONAL_STARTUP_SERVICES=$(additionalStartupServices) || exit $?
 
-checkFolders --create || exit $?
-
 COMPOSE_ARGS_PULL="\
       -f docker-compose.yml ${ADDITIONAL_CACHE_ARGS} ${ADDITIONAL_COMPOSE_ARGS} ${ADDITIONAL_COMPOSE_QUEUE_ARGS} \
       pull \
