c4cb741e86671bdbb8b76255251685d902f61558
mpetrov <mpetrov@thingsboard.io>
2025-03-12 11:59:45 +0200

    Added help popup width

--- ui-ngx/src/app/shared/components/js-func.component.ts
+++ ui-ngx/src/app/shared/components/js-func.component.ts
@@ -106,6 +106,8 @@ export class JsFuncComponent implements OnInit, OnChanges, OnDestroy, ControlVal
 
   @Input() globalVariables: Array<string>;
 
+  @Input() helpPopupStyle: Record<string, any> = {};
+
   @Input()
   @coerceBoolean()
   disableUndefinedCheck = false;
