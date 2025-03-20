83790fa0fb6a208ca872a7c9cb704d2e8dab80e6
Sergey Matvienko <smatvienko@thingsboard.io>
2025-03-20 11:02:52 +0100

    ReconnectStrategyExponential jitter, max added, refactored, tested

--- netty-mqtt/src/main/java/org/thingsboard/mqtt/ReconnectStrategyExponential.java
+++ netty-mqtt/src/main/java/org/thingsboard/mqtt/ReconnectStrategyExponential.java
@@ -18,37 +18,61 @@ package org.thingsboard.mqtt;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.TimeUnit;
 
 @Getter
 @Slf4j
 public class ReconnectStrategyExponential implements ReconnectStrategy {
 
-    public static final int DEFAULT_RECONNECT_INTERVAL = 10;
-    final long reconnectIntervalMinSeconds;
-    final long reconnectIntervalMaxSeconds = 30;
-    long lastDisconnectNanoTime = 0; //isotonic time
-    int retryCount = 0;
+    public static final int DEFAULT_RECONNECT_INTERVAL_SEC = 10;
+    public static final int MAX_RECONNECT_INTERVAL_SEC = 60;
+    public static final int EXP_MAX = 8;
+    public static final long JITTER_MAX = 1;
+    private final long reconnectIntervalMinSeconds;
+    private final long reconnectIntervalMaxSeconds;
+    private long lastDisconnectNanoTime = 0; //isotonic time
+    private long retryCount = 0;
 
-    public ReconnectStrategyExponential(long reconnectIntervalMin) {
-        this.reconnectIntervalMinSeconds = calculateIntervalMin(reconnectIntervalMin);
+    public ReconnectStrategyExponential(long reconnectIntervalMinSeconds) {
+        this.reconnectIntervalMaxSeconds = calculateIntervalMax(reconnectIntervalMinSeconds);
+        this.reconnectIntervalMinSeconds = calculateIntervalMin(reconnectIntervalMinSeconds);
     }
 
-    private long calculateIntervalMin(long reconnectIntervalMin) {
-        return Math.min((reconnectIntervalMin > 0 ? reconnectIntervalMin : DEFAULT_RECONNECT_INTERVAL), this.reconnectIntervalMaxSeconds);
+    long calculateIntervalMax(long reconnectIntervalMinSeconds) {
+        return reconnectIntervalMinSeconds > MAX_RECONNECT_INTERVAL_SEC ? reconnectIntervalMinSeconds : MAX_RECONNECT_INTERVAL_SEC;
+    }
+
+    long calculateIntervalMin(long reconnectIntervalMinSeconds) {
+        return Math.min((reconnectIntervalMinSeconds > 0 ? reconnectIntervalMinSeconds : DEFAULT_RECONNECT_INTERVAL_SEC), this.reconnectIntervalMaxSeconds);
     }
 
     @Override
     synchronized public long getNextReconnectDelay() {
         final long currentNanoTime = getNanoTime();
-        final long lastDisconnectIntervalNanos = currentNanoTime - lastDisconnectNanoTime;
+        final long coolDownSpentNanos = currentNanoTime - lastDisconnectNanoTime;
         lastDisconnectNanoTime = currentNanoTime;
-        if (TimeUnit.NANOSECONDS.toSeconds(lastDisconnectIntervalNanos) > reconnectIntervalMaxSeconds + reconnectIntervalMinSeconds) {
-            log.debug("Reset retry counter");
+        if (isCooledDown(coolDownSpentNanos)) {
             retryCount = 0;
             return reconnectIntervalMinSeconds;
         }
-        return Math.min(reconnectIntervalMaxSeconds, reconnectIntervalMinSeconds + (1L << retryCount++));
+        return calculateNextReconnectDelay() + calculateJitter();
+    }
+
+    long calculateJitter() {
+        return ThreadLocalRandom.current().nextInt() >= 0 ? JITTER_MAX : 0;
+    }
+
+    long calculateNextReconnectDelay() {
+        return Math.min(reconnectIntervalMaxSeconds, reconnectIntervalMinSeconds + calculateExp(retryCount++));
+    }
+
+    long calculateExp(long e) {
+        return 1L << Math.min(e, EXP_MAX);
+    }
+
+    boolean isCooledDown(long coolDownSpentNanos) {
+        return TimeUnit.NANOSECONDS.toSeconds(coolDownSpentNanos) > reconnectIntervalMaxSeconds + reconnectIntervalMinSeconds;
     }
 
     long getNanoTime() {
