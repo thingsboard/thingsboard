6e5e6b064743e5c1b94eb4eed921344eba8bfa74
dashevchenko <dshevchenko@thingsboard.io>
2025-05-21 15:19:38 +0300

    clean imports

--- common/proto/src/main/java/org/thingsboard/server/common/adaptor/JsonConverter.java
+++ common/proto/src/main/java/org/thingsboard/server/common/adaptor/JsonConverter.java
@@ -60,7 +60,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.Optional;
 import java.util.Set;
 import java.util.TreeMap;
 import java.util.function.Consumer;
