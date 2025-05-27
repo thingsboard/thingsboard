80a33492d587c932452acd3584ee82aec04e1cf6
ViacheslavKlimov <viacheslavklimov11@gmail.com>
2025-05-27 18:26:19 +0300

    Add proto number for ComponentLifecycleEvent

--- common/data/src/main/java/org/thingsboard/server/common/data/EntityType.java
+++ common/data/src/main/java/org/thingsboard/server/common/data/EntityType.java
@@ -18,6 +18,7 @@ package org.thingsboard.server.common.data;
 import lombok.Getter;
 import org.apache.commons.lang3.StringUtils;
 
+import java.util.Arrays;
 import java.util.EnumSet;
 import java.util.List;
 
@@ -76,6 +77,15 @@ public enum EntityType {
     public static final List<String> NORMAL_NAMES = EnumSet.allOf(EntityType.class).stream()
             .map(EntityType::getNormalName).toList();
 
+    private static final EntityType[] BY_PROTO;
+
+    static {
+        BY_PROTO = new EntityType[Arrays.stream(values()).mapToInt(EntityType::getProtoNumber).max().orElse(0) + 1];
+        for (EntityType entityType : values()) {
+            BY_PROTO[entityType.getProtoNumber()] = entityType;
+        }
+    }
+
     EntityType(int protoNumber) {
         this.protoNumber = protoNumber;
         this.tableName = name().toLowerCase();
@@ -96,6 +106,13 @@ public enum EntityType {
             }
         }
         return false;
+    }
+
+    public static EntityType forProtoNumber(int protoNumber) {
+        if (protoNumber < 0 || protoNumber >= BY_PROTO.length) {
+            throw new IllegalArgumentException("Invalid EntityType proto number " + protoNumber);
+        }
+        return BY_PROTO[protoNumber];
     }
 
 }
