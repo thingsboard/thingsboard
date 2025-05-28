a5f2b42d1592e0f7a0f6f41638a54229fefdf21f
Artem Dzhereleiko <Dgerik2015@gmail.com>
2025-05-28 12:23:45 +0300

    UI: Ref

--- ui-ngx/src/app/modules/home/components/widget/config/basic/chart/bar-chart-basic-config.component.ts
+++ ui-ngx/src/app/modules/home/components/widget/config/basic/chart/bar-chart-basic-config.component.ts
@@ -29,7 +29,6 @@ import {
 import {
   LatestChartBasicConfigComponent
 } from '@home/components/widget/config/basic/chart/latest-chart-basic-config.component';
-import { chartFillTypes } from '@home/components/widget/lib/chart/chart.models';
 
 @Component({
   selector: 'tb-bar-chart-basic-config',
@@ -78,6 +77,4 @@ export class BarChartBasicConfigComponent extends LatestChartBasicConfigComponen
     this.widgetConfig.config.settings.axisTickLabelFont = config.axisTickLabelFont;
     this.widgetConfig.config.settings.axisTickLabelColor = config.axisTickLabelColor;
   }
-
-  protected readonly chartFillTypes = chartFillTypes;
 }
