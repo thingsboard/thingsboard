00bc14ab61b53430665d7c581c8b9e3f29321fbc
dashevchenko <dshevchenko@thingsboard.io>
2025-07-30 18:07:05 +0300

    fixed tenant attribute save by system admin

--- dao/src/main/java/org/thingsboard/server/dao/timeseries/BaseTimeseriesService.java
+++ dao/src/main/java/org/thingsboard/server/dao/timeseries/BaseTimeseriesService.java
@@ -277,7 +277,8 @@ public class BaseTimeseriesService implements TimeseriesService {
         return Futures.transform(timeseriesLatestDao.removeLatest(tenantId, entityId, query), result -> {
             if (result.isRemoved()) {
                 Long version = result.getVersion();
-                edqsService.onDelete(tenantId, ObjectType.LATEST_TS_KV, new LatestTsKv(entityId, query.getKey(), version));
+                TenantId edqsTenantId = entityId.getEntityType() == EntityType.TENANT ? (TenantId) entityId : tenantId;
+                edqsService.onDelete(edqsTenantId, ObjectType.LATEST_TS_KV, new LatestTsKv(entityId, query.getKey(), version));
             }
             return result;
         }, MoreExecutors.directExecutor());
