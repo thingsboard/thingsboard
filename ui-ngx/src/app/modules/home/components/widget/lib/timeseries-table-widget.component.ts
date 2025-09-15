41d90b8e1bde908f16357f9b1b7e292f7fe9fe33
Maksym Tsymbarov <mtsymbarov@thingsboard.io>
2025-09-15 15:06:06 +0300

    edit translation key

--- ui-ngx/src/app/modules/home/components/widget/lib/timeseries-table-widget.component.ts
+++ ui-ngx/src/app/modules/home/components/widget/lib/timeseries-table-widget.component.ts
@@ -513,7 +513,7 @@ export class TimeseriesTableWidgetComponent extends PageComponent implements OnI
         let title = '';
         const header = this.sources[index].header.find(column => column.index.toString() === value);
         if (value === '0') {
-          title = this.translate.instant('audit-log.timestamp');
+          title = this.translate.instant('widgets.table.display-timestamp');
         } else if (value === 'actions') {
           title = 'Actions';
         } else {
