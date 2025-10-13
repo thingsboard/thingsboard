374b6cf22f427f76080cf50e112b925bd865589f
dashevchenko <dshevchenko@thingsboard.io>
2025-10-13 12:57:21 +0300

    import cleanup

--- application/src/main/java/org/thingsboard/server/service/device/DeviceBulkImportService.java
+++ application/src/main/java/org/thingsboard/server/service/device/DeviceBulkImportService.java
@@ -29,8 +29,6 @@ import org.thingsboard.server.common.data.DeviceProfileProvisionType;
 import org.thingsboard.server.common.data.DeviceProfileType;
 import org.thingsboard.server.common.data.DeviceTransportType;
 import org.thingsboard.server.common.data.EntityType;
-import org.thingsboard.server.common.data.NameConflictPolicy;
-import org.thingsboard.server.common.data.NameConflictStrategy;
 import org.thingsboard.server.common.data.StringUtils;
 import org.thingsboard.server.common.data.device.credentials.BasicMqttCredentials;
 import org.thingsboard.server.common.data.device.credentials.lwm2m.LwM2MClientCredential;
