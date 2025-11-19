c30400e2a0e6e402e895ea0faa4ca5a122d30cf7
Viacheslav Klimov <viacheslavklimov11@gmail.com>
2025-11-19 12:47:17 +0200

    Limit sysadmin alarm permissions to read-only

--- application/src/main/java/org/thingsboard/server/service/security/permission/SysAdminPermissions.java
+++ application/src/main/java/org/thingsboard/server/service/security/permission/SysAdminPermissions.java
@@ -31,7 +31,7 @@ public class SysAdminPermissions extends AbstractPermissions {
     public SysAdminPermissions() {
         super();
         put(Resource.ADMIN_SETTINGS, PermissionChecker.allowAllPermissionChecker);
-        put(Resource.ALARM, PermissionChecker.allowAllPermissionChecker);
+        put(Resource.ALARM, new PermissionChecker.GenericPermissionChecker(Operation.READ));
         put(Resource.DASHBOARD, new PermissionChecker.GenericPermissionChecker(Operation.READ));
         put(Resource.TENANT, PermissionChecker.allowAllPermissionChecker);
         put(Resource.RULE_CHAIN, systemEntityPermissionChecker);
