b325731ffdef1ad7ece0ffd79f620a3625103b54
Nikita Mazurenko <nikitamazurenko42@gmail.com>
2025-12-01 17:43:06 +0200

    Fix customer unassignments in the dashboard during edge event processing

--- application/src/main/java/org/thingsboard/server/service/edge/rpc/processor/dashboard/BaseDashboardProcessor.java
+++ application/src/main/java/org/thingsboard/server/service/edge/rpc/processor/dashboard/BaseDashboardProcessor.java
@@ -65,12 +65,12 @@ public abstract class BaseDashboardProcessor extends BaseEdgeProcessor {
 
         Dashboard savedDashboard = edgeCtx.getDashboardService().saveDashboard(dashboard, false);
 
-        updateDashboardAssignments(tenantId, dashboardById, savedDashboard, newAssignedCustomers);
+        updateDashboardAssignments(tenantId, customerId, dashboardById, savedDashboard, newAssignedCustomers);
 
         return created;
     }
 
-    private void updateDashboardAssignments(TenantId tenantId, Dashboard dashboardById, Dashboard savedDashboard, Set<ShortCustomerInfo> newAssignedCustomers) {
+    private void updateDashboardAssignments(TenantId tenantId, CustomerId edgeCustomerId, Dashboard dashboardById, Dashboard savedDashboard, Set<ShortCustomerInfo> newAssignedCustomers) {
         Set<ShortCustomerInfo> currentAssignedCustomers = new HashSet<>();
         if (dashboardById != null) {
             if (dashboardById.getAssignedCustomers() != null) {
@@ -78,7 +78,7 @@ public abstract class BaseDashboardProcessor extends BaseEdgeProcessor {
             }
         }
 
-        newAssignedCustomers = filterNonExistingCustomers(tenantId, currentAssignedCustomers, newAssignedCustomers);
+        newAssignedCustomers = filterNonExistingCustomers(tenantId, edgeCustomerId, currentAssignedCustomers, newAssignedCustomers);
 
         Set<CustomerId> addedCustomerIds = new HashSet<>();
         Set<CustomerId> removedCustomerIds = new HashSet<>();
@@ -114,6 +114,6 @@ public abstract class BaseDashboardProcessor extends BaseEdgeProcessor {
         }
     }
 
-    protected abstract Set<ShortCustomerInfo> filterNonExistingCustomers(TenantId tenantId, Set<ShortCustomerInfo> currentAssignedCustomers, Set<ShortCustomerInfo> newAssignedCustomers);
+    protected abstract Set<ShortCustomerInfo> filterNonExistingCustomers(TenantId tenantId, CustomerId customerId, Set<ShortCustomerInfo> currentAssignedCustomers, Set<ShortCustomerInfo> newAssignedCustomers);
 
 }
