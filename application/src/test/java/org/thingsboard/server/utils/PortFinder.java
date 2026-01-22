9577cb0aeed2127d6911109b5084e8bd1c27135a
Sergey Matvienko <smatvienko@thingsboard.io>
2026-01-22 03:17:00 +0100

    tests: PortFinder.findAvailableUdpPort added

---
+++ application/src/test/java/org/thingsboard/server/utils/PortFinder.java
@@ -0,0 +1,29 @@
+/**
+ * Copyright © 2016-2026 The Thingsboard Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.thingsboard.server.utils;
+
+import java.net.DatagramSocket;
+import java.net.SocketException;
+
+public class PortFinder {
+    public static int findAvailableUdpPort() {
+        try (DatagramSocket socket = new DatagramSocket(0)) {
+            return socket.getLocalPort();
+        } catch (SocketException e) {
+            throw new IllegalStateException("No available UDP ports found", e);
+        }
+    }
+}
