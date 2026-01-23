ac0c8eba31c79f233dfce620ca5ec5c7fd4eacc5
Nikita Mazurenko <nikitamazurenko42@gmail.com>
2026-01-23 13:00:53 +0200

    Add test to verify that enum proto EdgeVersion is synchronized with maven project.version

---
+++ application/src/test/java/org/thingsboard/server/edge/EdgeLatestVersionTest.java
@@ -0,0 +1,31 @@
+package org.thingsboard.server.edge;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.thingsboard.server.gen.edge.v1.EdgeVersion;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class EdgeLatestVersionTest {
+
+    @Test
+    public void edgeLatestVersionIsSynchronizedTest() {
+        EdgeVersion currentHighestEdgeVersion = Arrays.stream(EdgeVersion.values())
+                .filter(v -> v != EdgeVersion.UNRECOGNIZED)
+                .filter(v -> v != EdgeVersion.V_LATEST)
+                .max(Comparator.comparingInt(EdgeVersion::getNumber))
+                .orElseThrow();
+
+        String projectVersion = EdgeLatestVersionTest.class.getPackage().getImplementationVersion();
+        if (projectVersion == null || projectVersion.isBlank()) {
+            projectVersion = System.getProperty("project.version", "UNKNOWN");
+        }
+
+        String projectVersionDigits = projectVersion.replaceAll("\\D", "");
+        String currentHighestEdgeVersionDigits = currentHighestEdgeVersion.name().replaceAll("\\D", "");
+
+        Assert.assertEquals(projectVersionDigits, currentHighestEdgeVersionDigits);
+    }
+
+}
