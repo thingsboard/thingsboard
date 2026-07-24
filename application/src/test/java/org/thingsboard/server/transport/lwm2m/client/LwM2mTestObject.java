8f645671dc7038e66cf0a90b7c70af572ea05c00
nickAS21 <nick@thingsboard.io>
2026-07-24 09:14:08 +0300

    lwm2m_fix_buf_Negative_Integer_values: license

--- application/src/test/java/org/thingsboard/server/transport/lwm2m/client/LwM2mTestObject.java
+++ application/src/test/java/org/thingsboard/server/transport/lwm2m/client/LwM2mTestObject.java
@@ -1,18 +1,18 @@
-/*******************************************************************************
- * Copyright (c) 2021 Sierra Wireless and others.
+/**
+ * Copyright © 2016-2026 The Thingsboard Authors
  *
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v2.0
- * and Eclipse Distribution License v1.0 which accompany this distribution.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * The Eclipse Public License is available at
- *    http://www.eclipse.org/legal/epl-v20.html
- * and the Eclipse Distribution License is available at
- *    http://www.eclipse.org/org/documents/edl-v10.html.
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Contributors:
- *     Sierra Wireless - initial API and implementation
- *******************************************************************************/
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.thingsboard.server.transport.lwm2m.client;
 
 import org.eclipse.leshan.client.resource.SimpleInstanceEnabler;
