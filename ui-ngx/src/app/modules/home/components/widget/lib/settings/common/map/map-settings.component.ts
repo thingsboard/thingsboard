498763bfcc2ac5e35fded7fabd6cb6a9379c1c34
Dmytro Khylko <dmytro.khylko@gmail.com>
2026-08-07 13:59:42 +0300

    Made gestures mode dependant on the Zoom actions:scroll (visible only when scroll is enabled), changed from simple slider to a more styled pill slider toggle, added a tooltip where put configuration explanation

--- ui-ngx/src/app/modules/home/components/widget/lib/settings/common/map/map-settings.component.ts
+++ ui-ngx/src/app/modules/home/components/widget/lib/settings/common/map/map-settings.component.ts
@@ -37,6 +37,7 @@ import {
   MapSetting,
   MapType,
   mapZoomActions,
+  MapZoomAction,
   mapZoomActionTranslationMap
 } from '@shared/models/widget/maps/map.models';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
@@ -86,6 +87,8 @@ export class MapSettingsComponent implements OnInit, ControlValueAccessor, Valid
   mapScaleTranslationMap = mapScaleTranslationMap;
 
   MapType = MapType;
+
+  MapZoomAction = MapZoomAction;
 
   @Input()
   @coerceBoolean()
@@ -303,6 +306,9 @@ export class MapSettingsComponent implements OnInit, ControlValueAccessor, Valid
 
   private updateModel() {
     this.modelValue = this.mapSettingsFormGroup.getRawValue();
+    if (!this.modelValue.zoomActions?.includes(MapZoomAction.scroll)) {
+      this.modelValue.useGestureHandling = false;
+    }
     this.propagateChange(this.modelValue);
   }
 
